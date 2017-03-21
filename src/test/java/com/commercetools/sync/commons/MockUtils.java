f4243b7e4987742bc8db75f8c37bf6d946cf4f56
Hesham Massoud <hesham.massoud@commercetools.de>
2017-03-21 19:17:19 +0100

    CTT-192: Refactor MockUtils.

--- src/test/java/com/commercetools/sync/commons/MockUtils.java
+++ src/test/java/com/commercetools/sync/commons/MockUtils.java
@@ -2,13 +2,16 @@ package com.commercetools.sync.commons;
 
 import com.commercetools.sync.categories.CategorySyncOptions;
 import io.sphere.sdk.categories.CategoryDraft;
-import io.sphere.sdk.categories.CategoryDraftBuilder;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.models.LocalizedString;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MockUtils {
     private static BlockingSphereClient ctpClient;
@@ -21,19 +24,21 @@ public class MockUtils {
 
     public static List<CategoryDraft> getMockCategoryDrafts() {
         List<CategoryDraft> categoryDrafts = new ArrayList<>();
-        CategoryDraft categoryDraft1 = getMockCategoryDraft("draft1", "slug1", Locale.GERMAN, "SH663881");
-        CategoryDraft categoryDraft2 = getMockCategoryDraft("draft2", "slug2", Locale.GERMAN, "nothing");
+        CategoryDraft categoryDraft1 = getMockCategoryDraft(Locale.GERMAN,"draft1", "slug1", "SH663881");
+        CategoryDraft categoryDraft2 = getMockCategoryDraft(Locale.GERMAN,"draft2", "slug2", "nothing");
         categoryDrafts.add(categoryDraft1);
         categoryDrafts.add(categoryDraft2);
         return categoryDrafts;
     }
 
-    public static CategoryDraft getMockCategoryDraft(String name, String slug, Locale locale, String externalId) {
-        CategoryDraft categoryDraft = CategoryDraftBuilder.of(
-                LocalizedString.of(locale, name),
-                LocalizedString.of(locale, slug))
-                .externalId(externalId)
-                .build();
-        return categoryDraft;
+    public static CategoryDraft getMockCategoryDraft(@Nonnull final Locale locale,
+                                                     @Nonnull final String name,
+                                                     @Nonnull final String slug,
+                                                     @Nonnull final String externalId) {
+        CategoryDraft mockCategoryDraft = mock(CategoryDraft.class);
+        when(mockCategoryDraft.getName()).thenReturn(LocalizedString.of(locale, name));
+        when(mockCategoryDraft.getSlug()).thenReturn(LocalizedString.of(locale, slug));
+        when(mockCategoryDraft.getExternalId()).thenReturn(externalId);
+        return mockCategoryDraft;
     }
 }
