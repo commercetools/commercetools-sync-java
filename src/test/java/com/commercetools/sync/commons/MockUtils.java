06f83d9c988e5d5353e8acbc3d3d6b9458f388a3
Hesham Massoud <hesham.massoud@commercetools.de>
2017-03-22 16:15:54 +0100

    CTT-192: Mock customFields for a mockCategoryDraft in MockUtils.

--- src/test/java/com/commercetools/sync/commons/MockUtils.java
+++ src/test/java/com/commercetools/sync/commons/MockUtils.java
@@ -1,14 +1,15 @@
 package com.commercetools.sync.commons;
 
 import com.commercetools.sync.categories.CategorySyncOptions;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import io.sphere.sdk.categories.CategoryDraft;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.types.CustomFieldsDraft;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -25,7 +26,7 @@ public class MockUtils {
     public static List<CategoryDraft> getMockCategoryDrafts() {
         List<CategoryDraft> categoryDrafts = new ArrayList<>();
         CategoryDraft categoryDraft1 = getMockCategoryDraft(Locale.GERMAN,"draft1", "slug1", "SH663881");
-        CategoryDraft categoryDraft2 = getMockCategoryDraft(Locale.GERMAN,"draft2", "slug2", "nothing");
+        CategoryDraft categoryDraft2 = getMockCategoryDraft(Locale.GERMAN,"draft2", "slug2", "SH604972");
         categoryDrafts.add(categoryDraft1);
         categoryDrafts.add(categoryDraft2);
         return categoryDrafts;
@@ -39,6 +40,14 @@ public class MockUtils {
         when(mockCategoryDraft.getName()).thenReturn(LocalizedString.of(locale, name));
         when(mockCategoryDraft.getSlug()).thenReturn(LocalizedString.of(locale, slug));
         when(mockCategoryDraft.getExternalId()).thenReturn(externalId);
+        when(mockCategoryDraft.getCustom()).thenReturn(getMockCustomFieldsDraft());
         return mockCategoryDraft;
+    }
+
+    public static CustomFieldsDraft getMockCustomFieldsDraft(){
+        Map<String, JsonNode> customFieldsJsons = new HashMap<>();
+        customFieldsJsons.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(false));
+        customFieldsJsons.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("de","rot").put("en", "red"));
+        return CustomFieldsDraft.ofTypeKeyAndJson("StepCategoryTypeKey", customFieldsJsons);
     }
 }
