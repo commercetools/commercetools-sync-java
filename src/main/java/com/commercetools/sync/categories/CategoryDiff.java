12c8775bd5792cfd5fede581ab25373f0f9f22af
Hesham Massoud <hesham.massoud@commercetools.de>
2017-03-28 13:57:40 +0200

    Add JavaDocs for CategoryDiff methods.

--- src/main/java/com/commercetools/sync/categories/CategoryDiff.java
+++ src/main/java/com/commercetools/sync/categories/CategoryDiff.java
@@ -15,7 +15,16 @@ import java.util.Optional;
 
 public class CategoryDiff {
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} names of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same name, an empty {@link Optional}
+     * is returned.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new name.
+     * @return A filled optional with the update action or an empty optional if the names are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildChangeNameUpdateAction(
             @Nonnull final Category existingCategory,
@@ -25,7 +34,16 @@ public class CategoryDiff {
                 ChangeName.of(newCategory.getName()));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} slugs of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same slugs, an empty {@link Optional}
+     * is returned.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new slug.
+     * @return A filled optional with the update action or an empty optional if the slugs are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildChangeSlugUpdateAction(
             @Nonnull final Category existingCategory,
@@ -35,7 +53,19 @@ public class CategoryDiff {
                 ChangeSlug.of(newCategory.getSlug()));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} descriptions of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same description values, an empty
+     * {@link Optional} is returned.
+     * <p>
+     * Note: If the description of the new {@link CategoryDraft} is null, an empty {@link Optional} is returned with
+     * no update actions.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new description.
+     * @return A filled optional with the update action or an empty optional if the descriptions are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildSetDescriptionUpdateAction(
             @Nonnull final Category existingCategory,
@@ -51,7 +81,19 @@ public class CategoryDiff {
                 SetDescription.of(newCategoryDescription));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the parents {@link Reference<Category>} of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same parents, an empty
+     * {@link Optional} is returned.
+     * <p>
+     * Note: If the parent {@link Reference<Category>} of the new {@link CategoryDraft} is null, an empty
+     * {@link Optional} is returned with no update actions.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new parent.
+     * @return A filled optional with the update action or an empty optional if the parent references are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildChangeParentUpdateAction(
             @Nonnull final Category existingCategory,
@@ -67,7 +109,19 @@ public class CategoryDiff {
                 ChangeParent.of(parentCategoryReference));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the orderHint values of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same orderHint values, an empty
+     * {@link Optional} is returned.
+     * <p>
+     * Note: If the orderHint of the new {@link CategoryDraft} is null, an empty {@link Optional} is returned with
+     * no update actions.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new orderHint.
+     * @return A filled optional with the update action or an empty optional if the orderHint values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildChangeOrderHintUpdateAction(
             @Nonnull final Category existingCategory,
@@ -83,7 +137,16 @@ public class CategoryDiff {
                 ChangeOrderHint.of(orderHint));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} meta title of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same meta title values, an empty
+     * {@link Optional} is returned.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new meta title.
+     * @return A filled optional with the update action or an empty optional if the meta titles values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildSetMetaTitleUpdateAction(
             @Nonnull final Category existingCategory,
@@ -94,7 +157,16 @@ public class CategoryDiff {
                 SetMetaTitle.of(newCategoryMetaTitle));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} meta keywords of a {@link Category} and a {@link CategoryDraft} and returns an
+     * {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed, for example in
+     * case where both the {@link Category} and the {@link CategoryDraft} have the same meta keywords values, an empty
+     * {@link Optional} is returned.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new meta keywords.
+     * @return A filled optional with the update action or an empty optional if the meta keywords values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildSetMetaKeywordsUpdateAction(
             @Nonnull final Category existingCategory,
@@ -105,7 +177,16 @@ public class CategoryDiff {
                 SetMetaKeywords.of(newCategoryMetaKeywords));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares the {@link LocalizedString} meta description of a {@link Category} and a {@link CategoryDraft} and
+     * returns an {@link UpdateAction<Category>} as a result in an {@link Optional}. If no update action is needed,
+     * for example in case where both the {@link Category} and the {@link CategoryDraft} have the same meta description
+     * values, an empty {@link Optional} is returned.
+     *
+     * @param existingCategory the category which should be updated.
+     * @param newCategory      the category draft where we get the new meta description.
+     * @return A filled optional with the update action or an empty optional if the meta description values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildSetMetaDescriptionUpdateAction(
             @Nonnull final Category existingCategory,
@@ -116,7 +197,16 @@ public class CategoryDiff {
                 SetMetaDescription.of(newCategoryMetaDescription));
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares two {@link LocalizedString} and returns a supplied {@link UpdateAction<Category>} as a result in an
+     * {@link Optional}. If no update action is needed, for example in case where both the {@link LocalizedString}
+     * have the same fields and values, an empty {@link Optional} is returned.
+     *
+     * @param existingLocalisedString the localised string which should be updated.
+     * @param newLocalisedString      the localised string with the new information.
+     * @param updateAction            the update action to return in the optional.
+     * @return A filled optional with the update action or an empty optional if the localised string values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildUpdateActionForLocalizedStrings(
             @Nullable final LocalizedString existingLocalisedString,
@@ -125,7 +215,17 @@ public class CategoryDiff {
         return buildUpdateActionForObjects(existingLocalisedString, newLocalisedString, updateAction);
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares two {@link Reference<Category>} and returns a supplied {@link UpdateAction<Category>} as a result in an
+     * {@link Optional}. If no update action is needed, for example in case where both the {@link Reference<Category>}
+     * have the same values, an empty {@link Optional} is returned.
+     *
+     * @param existingCategoryReference the category reference which should be updated.
+     * @param newCategoryReference      the category reference with the new information.
+     * @param updateAction              the update action to return in the optional.
+     * @return A filled optional with the update action or an empty optional if the category reference
+     * values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildUpdateActionForReferences(
             @Nullable final Reference<Category> existingCategoryReference,
@@ -134,7 +234,16 @@ public class CategoryDiff {
         return buildUpdateActionForObjects(existingCategoryReference, newCategoryReference, updateAction);
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares two {@link String} and returns a supplied {@link UpdateAction<Category>} as a result in an
+     * {@link Optional}. If no update action is needed, for example in case where both the {@link String}
+     * have the same values, an empty {@link Optional} is returned.
+     *
+     * @param existingString the string which should be updated.
+     * @param newString      the string with the new information.
+     * @param updateAction   the update action to return in the optional.
+     * @return A filled optional with the update action or an empty optional if the string values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildUpdateActionForStrings(@Nullable final String existingString,
                                                                         @Nullable final String newString,
@@ -142,14 +251,20 @@ public class CategoryDiff {
         return buildUpdateActionForObjects(existingString, newString, updateAction);
     }
 
-    //TODO: JAVADOC
+    /**
+     * Compares two {@link Object} and returns a supplied {@link UpdateAction<Category>} as a result in an
+     * {@link Optional}. If no update action is needed, for example in case where both the {@link Object}
+     * have the same values, an empty {@link Optional} is returned.
+     *
+     * @param existingObject the object which should be updated.
+     * @param newObject      the object with the new information.
+     * @param updateAction   the update action to return in the optional.
+     * @return A filled optional with the update action or an empty optional if the object values are identical.
+     */
     @Nonnull
     static Optional<UpdateAction<Category>> buildUpdateActionForObjects(@Nullable final Object existingObject,
                                                                         @Nullable final Object newObject,
                                                                         @Nonnull final UpdateAction<Category> updateAction) {
-        if (!Objects.equals(existingObject, newObject)) {
-            return Optional.of(updateAction);
-        }
-        return Optional.empty();
+        return !Objects.equals(existingObject, newObject) ? Optional.of(updateAction) : Optional.empty();
     }
 }
