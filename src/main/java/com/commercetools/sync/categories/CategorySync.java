b83657d4f090729a0e821d735d3322a0472afd4f
Hesham Massoud <hesham.massoud@commercetools.de>
2017-04-04 18:24:59 +0200

    Add SyncResult to CategorySync and impl.

--- src/main/java/com/commercetools/sync/categories/CategorySync.java
+++ src/main/java/com/commercetools/sync/categories/CategorySync.java
@@ -1,5 +1,6 @@
 package com.commercetools.sync.categories;
 
+import com.commercetools.sync.commons.helpers.SyncResult;
 import io.sphere.sdk.categories.Category;
 import io.sphere.sdk.categories.CategoryDraft;
 
@@ -9,5 +10,5 @@ import java.util.List;
 public interface CategorySync {
     void syncCategoryDrafts(@Nonnull final List<CategoryDraft> categories);
     void syncCategories(@Nonnull final List<Category> categories);
-    String getSummary();
+    String getSummary(@Nonnull final SyncResult<Category> syncResult);
 }
--- src/main/java/com/commercetools/sync/categories/impl/CategorySyncImpl.java
+++ src/main/java/com/commercetools/sync/categories/impl/CategorySyncImpl.java
@@ -3,6 +3,8 @@ package com.commercetools.sync.categories.impl;
 import com.commercetools.sync.categories.CategorySync;
 import com.commercetools.sync.categories.CategorySyncOptions;
 import com.commercetools.sync.categories.utils.CategorySyncUtils;
+import com.commercetools.sync.commons.helpers.SyncError;
+import com.commercetools.sync.commons.helpers.SyncResult;
 import com.commercetools.sync.services.CategoryService;
 import com.commercetools.sync.services.TypeService;
 import io.sphere.sdk.categories.Category;
@@ -15,6 +17,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
+import static com.commercetools.sync.commons.utils.StatisticsUtils.getHorizontalLine;
 import static java.lang.String.format;
 
 public class CategorySyncImpl implements CategorySync {
@@ -48,66 +51,79 @@ public class CategorySyncImpl implements CategorySync {
         processedCategories = categoryDrafts.size();
         LOGGER.info(format("About to sync %d category drafts into CTP project with key '%s'."
                 , categoryDrafts.size(), options.getCtpProjectKey()));
-        for (int i = 0; i < categoryDrafts.size(); i++) {
+        final SyncResult<Category> syncResult = SyncResult.emptyResult();
+
+
+        for (int i = 0; i < categoryDrafts.size(); i++) { // TODO: HANDLE IF THERE IS A NULL OBJECT IN THE LIST!
+            SyncResult<Category> currentSyncResult = SyncResult.emptyResult();
             CategoryDraft newCategoryDraft = categoryDrafts.get(i);
             String externalId = newCategoryDraft != null ? newCategoryDraft.getExternalId() : null;
             if (externalId != null) { // TODO NEED TO PARALLELISE!
                 Category oldCategory = categoryService.fetchCategoryByExternalId(externalId);
                 if (oldCategory == null) {
-                    createCategory(newCategoryDraft);
+                    currentSyncResult = createCategory(newCategoryDraft);
                 } else {
-                    List<UpdateAction<Category>> updateActions = CategorySyncUtils.buildActions(oldCategory, newCategoryDraft, typeService);
+                    currentSyncResult = CategorySyncUtils.buildActions(oldCategory, newCategoryDraft, typeService);
+                    final List<UpdateAction<Category>> updateActions = currentSyncResult.getUpdateActions();
                     if (!updateActions.isEmpty()) {
-                        updateCategory(oldCategory, updateActions);
+                        updateCategory(oldCategory, currentSyncResult);
                     }
                 }
             }
+            syncResult.mergeStatistics(currentSyncResult);
         }
-        LOGGER.info(getSummary());
+        LOGGER.info(getSummary(syncResult));
     }
 
     @Nullable
-    private Category createCategory(@Nonnull final CategoryDraft newCategory) {
-        Category category = null;
+    private SyncResult<Category> createCategory(@Nonnull final CategoryDraft newCategory) {
+        SyncResult<Category> syncResult = SyncResult.emptyResult();
         try {
-            category = categoryService.createCategory(newCategory);
+            categoryService.createCategory(newCategory);
             createdCategories++;
         } catch (Exception e) {
-            LOGGER.error(format("Failed to create category with external id" +
-                            " '%s' in CTP project with key '%s",
-                    newCategory.getExternalId(), options.getCtpProjectKey()), e);
+            syncResult = SyncResult.ofError(SyncError.of(null, format("Failed to create category with external id" +
+                                    " '%s' in CTP project with key '%s",
+                            newCategory.getExternalId(), options.getCtpProjectKey()), e));
             failedCategories++;
         }
-        return category;
+        return syncResult;
     }
 
     @Nullable
-    private Category updateCategory(@Nonnull final Category category, List<UpdateAction<Category>> updateActions) {
-        Category updatedCategory = null;
+    private SyncResult<Category> updateCategory(@Nonnull final Category category,
+                                                @Nonnull final SyncResult<Category> syncResult) {
         try {
-            updatedCategory = categoryService.updateCategory(category, updateActions);
+            categoryService.updateCategory(category, syncResult.getUpdateActions());
             updatedCategories++;
         } catch (Exception e) {
-            LOGGER.error(format("Failed to update category with id" +
-                            " '%s' in CTP project with key '%s",
-                    category.getId(), options.getCtpProjectKey()), e);
+            syncResult.addError(
+                    SyncError.of(category.getId(), format("Failed to update category with id" +
+                                    " '%s' in CTP project with key '%s",
+                            category.getId(), options.getCtpProjectKey()), e));
             failedCategories++;
         }
-        return updatedCategory;
+        return syncResult;
     }
 
 
     @Override
-    public String getSummary() {
+    public String getSummary(@Nonnull final SyncResult<Category> syncResult) {
         return format("Category Sync completed successfully!\n" +
-                        "Summary: (%s) categories were processed in total." +
+                        "Sync Summary:\n%s(%s) categories were processed in total." +
                         "\n\t+ (%s) categories created." +
                         "\n\t+ (%s) categories updated." +
-                        "\n\t+ (%s) categories failed to sync.",
+                        "\n\t+ (%s) categories failed to sync." +
+                        "\n\n" +
+                        "Detailed Report of Sync Statistics:\n" +
+                        "%s\n%s",
+                getHorizontalLine(15),
                 processedCategories,
                 createdCategories,
                 updatedCategories,
-                failedCategories);
+                failedCategories,
+                getHorizontalLine(35),
+                syncResult.getResultStatistics());
     }
 
 }
