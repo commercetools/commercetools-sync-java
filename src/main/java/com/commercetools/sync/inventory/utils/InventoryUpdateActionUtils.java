aaa6d96bca006d8a56c34cefa22cb9680f8b65d9
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-04-11 15:36:26 +0200

    CTPI-2: added tests and documentation to InventoryUpdateActionUtils

--- src/main/java/com/commercetools/sync/inventory/utils/InventoryUpdateActionUtils.java
+++ src/main/java/com/commercetools/sync/inventory/utils/InventoryUpdateActionUtils.java
@@ -15,48 +15,95 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 
+/**
+ * This class provides static utility methods for building update actions related to inventories.
+ */
 public final class InventoryUpdateActionUtils {
 
     private InventoryUpdateActionUtils() {
         throw new AssertionError();
     }
 
+    /**
+     * Compares the {@code quantityOnStock} values of a {@link InventoryEntry} and {@link InventoryEntryDraft}
+     * and returns a {@link List} of update actions, which would contain the {@code "changeQuantity"}
+     * {@link UpdateAction}. If quantity on stock from {@code newEntry} is {@code null} or no update action is needed,
+     * for example in case where both the {@link InventoryEntry} and the {@link InventoryEntryDraft} have the same
+     * {@code quantityOnStock} values, the empty list would be returned.
+     *
+     * @param oldEntry the inventory entry that should be updated
+     * @param newEntry the inventory entry draft which contains new quantity on stock.
+     * @return list containing update action or empty list if quantities on stock are indentical or quantity on stock
+     * from {@code newEntry} is null.
+     */
     @Nonnull
     public static List<UpdateAction<InventoryEntry>> buildChangeQuantityAction(@Nonnull final InventoryEntry oldEntry,
                                                                                @Nonnull final InventoryEntryDraft newEntry) {
         final Long oldValue = oldEntry.getQuantityOnStock();
         final Long newValue = newEntry.getQuantityOnStock();
-        return Objects.equals(oldValue, newValue)
-                ? emptyList() : singletonList(ChangeQuantity.of(newValue));
+        return newValue == null || Objects.equals(oldValue, newValue)
+                ? emptyList() : asList(ChangeQuantity.of(newValue));
     }
 
+    /**
+     * Compares the {@code restockableInDays} values of a {@link InventoryEntry} and {@link InventoryEntryDraft}
+     * and returns a {@link List} of update actions, which would contain the {@code "setRestockableInDays"}
+     * {@link UpdateAction}. If no update action is needed, for example in case where both the
+     * {@link InventoryEntry} and the {@link InventoryEntryDraft} have the same {@code restockableInDays} values,
+     * the empty list would be returned.
+     *
+     * @param oldEntry the inventory entry that should be updated
+     * @param newEntry the inventory entry draft which contains new restockable in days.
+     * @return list containing update action or empty list if restockable in days are indentical.
+     */
     @Nonnull
     public static List<UpdateAction<InventoryEntry>> buildSetRestockableInDaysAction(@Nonnull final InventoryEntry oldEntry,
                                                                                      @Nonnull final InventoryEntryDraft newEntry) {
         final Integer oldValue = oldEntry.getRestockableInDays();
         final Integer newValue = newEntry.getRestockableInDays();
         return Objects.equals(oldValue, newValue)
-                ? emptyList() : singletonList(SetRestockableInDays.of(newValue));
+                ? emptyList() : asList(SetRestockableInDays.of(newValue));
     }
 
+    /**
+     * Compares the {@code expectedDelivery} values of a {@link InventoryEntry} and {@link InventoryEntryDraft}
+     * and returns a {@link List} of update actions, which would contain the {@code "setExpectedDelivery"}
+     * {@link UpdateAction}. If no update action is needed, for example in case where both the
+     * {@link InventoryEntry} and the {@link InventoryEntryDraft} have the same {@code expectedDelivery} values,
+     * the empty list would be returned.
+     *
+     * @param oldEntry the inventory entry that should be updated
+     * @param newEntry the inventory entry draft which contains new expected delivery.
+     * @return list containing update action or empty list if expected deliveries are indentical.
+     */
     @Nonnull
     public static List<UpdateAction<InventoryEntry>> buildSetExpectedDeliveryAction(@Nonnull final InventoryEntry oldEntry,
                                                                                     @Nonnull final InventoryEntryDraft newEntry) {
         final ZonedDateTime oldValue = oldEntry.getExpectedDelivery();
         final ZonedDateTime newValue = newEntry.getExpectedDelivery();
         return Objects.equals(oldValue, newValue)
-                ? emptyList() : singletonList(SetExpectedDelivery.of(newValue));
+                ? emptyList() : asList(SetExpectedDelivery.of(newValue));
     }
 
+    /**
+     * Compares the {@code supplyChannel} references of a {@link InventoryEntry} and {@link InventoryEntryDraft}
+     * and returns a {@link List} of update actions, which would contain the {@code "setSupplyChannel"}
+     * {@link UpdateAction}. If no update action is needed, for example in case where both the {@link InventoryEntry}
+     * and the {@link InventoryEntryDraft} have the same supply channel, the empty list would be returned.
+     *
+     * @param oldEntry the inventory entry that should be updated
+     * @param newEntry the inventory entry draft which contains new supply channel.
+     * @return list containing update action or empty list if supply channels are indentical.
+     */
     @Nonnull
     public static List<UpdateAction<InventoryEntry>> buildSetSupplyChannelAction(@Nonnull final InventoryEntry oldEntry,
                                                                                  @Nonnull final InventoryEntryDraft newEntry) {
         final Reference<Channel> oldValue = oldEntry.getSupplyChannel();
         final Reference<Channel> newValue = newEntry.getSupplyChannel();
         return Objects.equals(oldValue, newValue)
-                ? emptyList() : singletonList(SetSupplyChannel.of(newEntry.getSupplyChannel()));
+                ? emptyList() : asList(SetSupplyChannel.of(newEntry.getSupplyChannel()));
     }
 }
---
+++ src/test/java/com/commercetools/sync/inventory/utils/InventoryUpdateActionUtilsTest.java
@@ -0,0 +1,153 @@
+package com.commercetools.sync.inventory.utils;
+
+import io.sphere.sdk.channels.Channel;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.inventory.InventoryEntry;
+import io.sphere.sdk.inventory.InventoryEntryDraft;
+import io.sphere.sdk.inventory.commands.updateactions.ChangeQuantity;
+import io.sphere.sdk.inventory.commands.updateactions.SetExpectedDelivery;
+import io.sphere.sdk.inventory.commands.updateactions.SetRestockableInDays;
+import io.sphere.sdk.inventory.commands.updateactions.SetSupplyChannel;
+import io.sphere.sdk.models.Reference;
+import org.junit.Test;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import static com.commercetools.sync.inventory.utils.InventoryUpdateActionUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class InventoryUpdateActionUtilsTest {
+
+    private InventoryEntry old;
+    private InventoryEntryDraft newSame;
+    private InventoryEntryDraft newDifferent;
+    private InventoryEntryDraft newWithNullValues;
+
+    {
+        final ZonedDateTime date1 = ZonedDateTime.of(2017, 5, 1, 10, 0, 0, 0, ZoneId.of("UTC"));
+        final ZonedDateTime date2 = ZonedDateTime.of(2017, 4, 1, 12, 0, 0, 0, ZoneId.of("UTC"));
+        
+        final Channel channel = mock(Channel.class);
+        final Reference<Channel> supplyChannel1 = Channel.referenceOfId("456");
+        final Reference<Channel> supplyChannel1WithObject = Channel.referenceOfId("456").filled(channel);
+        final Reference<Channel> supplyChannel2 = Channel.referenceOfId("789");
+
+        old = mock(InventoryEntry.class);
+        when(old.getSku()).thenReturn("123");
+        when(old.getQuantityOnStock()).thenReturn(10l);
+        when(old.getRestockableInDays()).thenReturn(10);
+        when(old.getExpectedDelivery()).thenReturn(date1);
+        when(old.getSupplyChannel()).thenReturn(supplyChannel1WithObject);
+
+        newSame = InventoryEntryDraft.of("123", 10l, date1, 10, supplyChannel1);
+        newDifferent = InventoryEntryDraft.of("123", 20l, date2, 20, supplyChannel2);
+        newWithNullValues = InventoryEntryDraft.of("123", 20l, null, null, null);
+    }
+
+    @Test
+    public void buildChangeQuantityActionReturnsActionForDifferentValues() {
+        final List<UpdateAction<InventoryEntry>> result = buildChangeQuantityAction(old, newDifferent);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(ChangeQuantity.class);
+        assertThat(((ChangeQuantity) result.get(0)).getQuantity()).isEqualTo(newDifferent.getQuantityOnStock());
+    }
+
+    @Test
+    public void buildChangeQuantityActionReturnsEmptyListForNewNullValue() {
+        final InventoryEntryDraft draft = mock(InventoryEntryDraft.class);
+        when(draft.getQuantityOnStock()).thenReturn(null);
+        final List<UpdateAction<InventoryEntry>> result = buildChangeQuantityAction(old, draft);
+        assertThat(result).isNotNull();
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void buildChangeQuantityActionReturnsEmptyListForSameValues() {
+        assertNoUpdatesForSameValues(InventoryUpdateActionUtils::buildChangeQuantityAction);
+    }
+
+    @Test
+    public void buildSetRestockableInDaysActionReturnsActionForDifferentValues() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetRestockableInDaysAction(old, newDifferent);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetRestockableInDays.class);
+        assertThat(((SetRestockableInDays) result.get(0)).getRestockableInDays())
+                .isEqualTo(newDifferent.getRestockableInDays());
+    }
+
+    @Test
+    public void buildSetRestockableInDaysActionReturnsActionForNewNullValue() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetRestockableInDaysAction(old, newWithNullValues);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetRestockableInDays.class);
+        assertThat(((SetRestockableInDays) result.get(0)).getRestockableInDays()).isNull();
+    }
+
+    @Test
+    public void buildSetRestockableInDaysActionReturnsEmptyListForSameValues() {
+        assertNoUpdatesForSameValues(InventoryUpdateActionUtils::buildSetRestockableInDaysAction);
+    }
+
+    @Test
+    public void buildSetExpectedDeliveryActionReturnsActionForDifferentValues() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetExpectedDeliveryAction(old, newDifferent);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetExpectedDelivery.class);
+        assertThat(((SetExpectedDelivery) result.get(0)).getExpectedDelivery())
+                .isEqualTo(newDifferent.getExpectedDelivery());
+    }
+
+    @Test
+    public void buildSetExpectedDeliveryActionReturnsActionForNewNullValue() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetExpectedDeliveryAction(old, newWithNullValues);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetExpectedDelivery.class);
+        assertThat(((SetExpectedDelivery) result.get(0)).getExpectedDelivery()).isNull();
+    }
+
+    @Test
+    public void buildSetExpectedDeliveryActionReturnsEmptyListForSameValues() {
+        assertNoUpdatesForSameValues(InventoryUpdateActionUtils::buildSetExpectedDeliveryAction);
+    }
+
+    @Test
+    public void buildSetSupplyChannelActionReturnsActionForDifferentValues() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetSupplyChannelAction(old, newDifferent);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetSupplyChannel.class);
+        assertThat(((SetSupplyChannel) result.get(0)).getSupplyChannel())
+                .isEqualTo(newDifferent.getSupplyChannel());
+    }
+
+    @Test
+    public void buildSetSupplyChannelActionReturnsActionForNewNullValue() {
+        final List<UpdateAction<InventoryEntry>> result = buildSetSupplyChannelAction(old, newWithNullValues);
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isExactlyInstanceOf(SetSupplyChannel.class);
+        assertThat(((SetSupplyChannel) result.get(0)).getSupplyChannel()).isNull();
+    }
+
+    @Test
+    public void buildSetSupplyChannelActionReturnsEmptyListForSameValues() {
+        assertNoUpdatesForSameValues(InventoryUpdateActionUtils::buildSetSupplyChannelAction);
+    }
+
+    private void assertNoUpdatesForSameValues
+            (BiFunction<InventoryEntry, InventoryEntryDraft, List<UpdateAction<InventoryEntry>>> buildFunction) {
+        final List<UpdateAction<InventoryEntry>> result = buildFunction.apply(old, newSame);
+        assertThat(result).isNotNull();
+        assertThat(result).isEmpty();
+    }
+}
