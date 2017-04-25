b7bea008baef2f5977cddae29a3a9b580e9a19f9
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-04-25 18:35:07 +0200

    add implemenation of abstract method in InventoryStatistics

--- src/main/java/com/commercetools/sync/inventory/helpers/InventorySyncStatistics.java
+++ src/main/java/com/commercetools/sync/inventory/helpers/InventorySyncStatistics.java
@@ -2,6 +2,8 @@ package com.commercetools.sync.inventory.helpers;
 
 import com.commercetools.sync.commons.helpers.BaseSyncStatistics;
 
+import static java.lang.String.format;
+
 /**
  * Statistics of inventory synchronisation process
  */
@@ -27,5 +29,27 @@ public class InventorySyncStatistics extends BaseSyncStatistics {
      */
     public void incrementUnprocessedDueToEmptySku() {
         this.unprocessedDueToEmptySku++;
+    }
+
+    /**
+     * Builds a summary of the inventory sync statistics instance that looks like the following example:
+     * <pre>
+     *     Summary of inventory synchronisation:
+     *     2 inventory entries weren't processed due to empty sku.
+     *     25 inventory entries were processed in total (9 created, 5 updated, 2 failed to sync).
+     * </pre>
+     * <strong>Note:</strong> second line is present only when there are any unprocessed entries.
+     *
+     * @return summary message
+     */
+    @Override
+    public String getReportMessage() {
+        String report = "\nSummary of inventory synchronisation:\n";
+        if (unprocessedDueToEmptySku > 0) {
+            report += format("%d inventory entries weren't processed due to empty sku\n", unprocessedDueToEmptySku);
+        }
+        report += format("%d inventory entries were processed in total (%d created, %d updated, %d failed to sync\n\n",
+                getProcessed(), getCreated(), getUpdated(), getFailed());
+        return report;
     }
 }
