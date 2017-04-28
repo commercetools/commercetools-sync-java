70bf98ec9bd8503af3ae5aacb160edac58ab1af3
Hesham Massoud <hesham.massoud@commercetools.de>
2017-04-28 12:27:37 +0200

    Make mutable objects non static.

--- src/test/java/com/commercetools/sync/commons/helpers/BaseSyncStatisticsTest.java
+++ src/test/java/com/commercetools/sync/commons/helpers/BaseSyncStatisticsTest.java
@@ -12,89 +12,89 @@ import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseSyncStatisticsTest {
-    private static BaseSyncStatistics BASE_SYNC_STATISTICS;
+    private BaseSyncStatistics baseSyncStatistics;
 
     @Before
     public void setup() {
-        BASE_SYNC_STATISTICS = new CategorySyncStatistics();
+        baseSyncStatistics = new CategorySyncStatistics();
     }
 
     @Test
     public void getUpdated_WithNoUpdated_ShouldReturnZero() {
-        assertThat(BASE_SYNC_STATISTICS.getUpdated()).isEqualTo(0);
+        assertThat(baseSyncStatistics.getUpdated()).isEqualTo(0);
     }
 
     @Test
     public void incrementUpdated_ShouldIncrementUpdatedValue() {
-        BASE_SYNC_STATISTICS.incrementUpdated();
-        assertThat(BASE_SYNC_STATISTICS.getUpdated()).isEqualTo(1);
+        baseSyncStatistics.incrementUpdated();
+        assertThat(baseSyncStatistics.getUpdated()).isEqualTo(1);
     }
 
     @Test
     public void getCreated_WithNoCreated_ShouldReturnZero() {
-        assertThat(BASE_SYNC_STATISTICS.getCreated()).isEqualTo(0);
+        assertThat(baseSyncStatistics.getCreated()).isEqualTo(0);
     }
 
     @Test
     public void incrementCreated_ShouldIncrementCreatedValue() {
-        BASE_SYNC_STATISTICS.incrementCreated();
-        assertThat(BASE_SYNC_STATISTICS.getCreated()).isEqualTo(1);
+        baseSyncStatistics.incrementCreated();
+        assertThat(baseSyncStatistics.getCreated()).isEqualTo(1);
     }
 
     @Test
     public void getProcessed_WithNoProcessed_ShouldReturnZero() {
-        assertThat(BASE_SYNC_STATISTICS.getProcessed()).isEqualTo(0);
+        assertThat(baseSyncStatistics.getProcessed()).isEqualTo(0);
     }
 
     @Test
     public void incrementProcessed_ShouldIncrementProcessedValue() {
-        BASE_SYNC_STATISTICS.incrementProcessed();
-        assertThat(BASE_SYNC_STATISTICS.getProcessed()).isEqualTo(1);
+        baseSyncStatistics.incrementProcessed();
+        assertThat(baseSyncStatistics.getProcessed()).isEqualTo(1);
     }
 
     @Test
     public void getFailed_WithNoFailed_ShouldReturnZero() {
-        assertThat(BASE_SYNC_STATISTICS.getFailed()).isEqualTo(0);
+        assertThat(baseSyncStatistics.getFailed()).isEqualTo(0);
     }
 
     @Test
     public void incrementFailed_ShouldIncrementFailedValue() {
-        BASE_SYNC_STATISTICS.incrementFailed();
-        assertThat(BASE_SYNC_STATISTICS.getFailed()).isEqualTo(1);
+        baseSyncStatistics.incrementFailed();
+        assertThat(baseSyncStatistics.getFailed()).isEqualTo(1);
     }
 
     @Test
     public void calculateProcessingTime_ShouldSetProcessingTimeInAllUnitsAndHumanReadableString() throws InterruptedException {
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInMillis()).isEqualTo(0);
-        assertThat(BASE_SYNC_STATISTICS.getHumanReadableProcessingTime()).isEqualTo(StringUtil.EMPTY_STRING);
+        assertThat(baseSyncStatistics.getProcessingTimeInMillis()).isEqualTo(0);
+        assertThat(baseSyncStatistics.getHumanReadableProcessingTime()).isEqualTo(StringUtil.EMPTY_STRING);
 
         final int waitingTimeInMillis = 100;
         Thread.sleep(waitingTimeInMillis);
-        BASE_SYNC_STATISTICS.calculateProcessingTime();
+        baseSyncStatistics.calculateProcessingTime();
 
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInDays()).isGreaterThanOrEqualTo(0);
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInHours()).isGreaterThanOrEqualTo(0);
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInMinutes()).isGreaterThanOrEqualTo(0);
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInSeconds()).isGreaterThanOrEqualTo(waitingTimeInMillis / 1000);
-        assertThat(BASE_SYNC_STATISTICS.getProcessingTimeInMillis()).isGreaterThanOrEqualTo(waitingTimeInMillis);
+        assertThat(baseSyncStatistics.getProcessingTimeInDays()).isGreaterThanOrEqualTo(0);
+        assertThat(baseSyncStatistics.getProcessingTimeInHours()).isGreaterThanOrEqualTo(0);
+        assertThat(baseSyncStatistics.getProcessingTimeInMinutes()).isGreaterThanOrEqualTo(0);
+        assertThat(baseSyncStatistics.getProcessingTimeInSeconds()).isGreaterThanOrEqualTo(waitingTimeInMillis / 1000);
+        assertThat(baseSyncStatistics.getProcessingTimeInMillis()).isGreaterThanOrEqualTo(waitingTimeInMillis);
 
-        final long remainingMillis = BASE_SYNC_STATISTICS.getProcessingTimeInMillis() -
-                TimeUnit.SECONDS.toMillis(BASE_SYNC_STATISTICS.getProcessingTimeInSeconds());
-        assertThat(BASE_SYNC_STATISTICS.getHumanReadableProcessingTime()).contains(format(", %dms", remainingMillis));
+        final long remainingMillis = baseSyncStatistics.getProcessingTimeInMillis() -
+                TimeUnit.SECONDS.toMillis(baseSyncStatistics.getProcessingTimeInSeconds());
+        assertThat(baseSyncStatistics.getHumanReadableProcessingTime()).contains(format(", %dms", remainingMillis));
     }
 
     @Test
     public void getStatisticsAsJSONString_WithoutCalculatingProcessingTime_ShouldGetCorrectJSONString() {
-        BASE_SYNC_STATISTICS.incrementCreated();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementCreated();
+        baseSyncStatistics.incrementProcessed();
 
-        BASE_SYNC_STATISTICS.incrementFailed();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementFailed();
+        baseSyncStatistics.incrementProcessed();
 
-        BASE_SYNC_STATISTICS.incrementUpdated();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementUpdated();
+        baseSyncStatistics.incrementProcessed();
 
-        final String statisticsAsJsonString = getStatisticsAsJSONString(BASE_SYNC_STATISTICS);
+        final String statisticsAsJsonString = getStatisticsAsJSONString(baseSyncStatistics);
         assertThat(statisticsAsJsonString)
                 .isEqualTo("{\"reportMessage\":\"Summary: 3 categories were processed in total (1 created, 1 updated and" +
                         " 1 categories failed to sync).\",\"" +
@@ -102,28 +102,28 @@ public class BaseSyncStatisticsTest {
                         "created\":1,\"" +
                         "failed\":1,\"" +
                         "processed\":3,\"" +
-                        "processingTimeInDays\":" + BASE_SYNC_STATISTICS.getProcessingTimeInDays() + ",\"" +
-                        "processingTimeInHours\":" + BASE_SYNC_STATISTICS.getProcessingTimeInHours() + ",\"" +
-                        "processingTimeInMinutes\":" + BASE_SYNC_STATISTICS.getProcessingTimeInMinutes() + ",\"" +
-                        "processingTimeInSeconds\":" + BASE_SYNC_STATISTICS.getProcessingTimeInSeconds() + ",\"" +
-                        "processingTimeInMillis\":" + BASE_SYNC_STATISTICS.getProcessingTimeInMillis() + ",\"" +
-                        "humanReadableProcessingTime\":\"" + BASE_SYNC_STATISTICS.getHumanReadableProcessingTime() + "\"}");
+                        "processingTimeInDays\":" + baseSyncStatistics.getProcessingTimeInDays() + ",\"" +
+                        "processingTimeInHours\":" + baseSyncStatistics.getProcessingTimeInHours() + ",\"" +
+                        "processingTimeInMinutes\":" + baseSyncStatistics.getProcessingTimeInMinutes() + ",\"" +
+                        "processingTimeInSeconds\":" + baseSyncStatistics.getProcessingTimeInSeconds() + ",\"" +
+                        "processingTimeInMillis\":" + baseSyncStatistics.getProcessingTimeInMillis() + ",\"" +
+                        "humanReadableProcessingTime\":\"" + baseSyncStatistics.getHumanReadableProcessingTime() + "\"}");
     }
 
     @Test
     public void getStatisticsAsJSONString_WithCalculatingProcessingTime_ShouldGetCorrectJSONString() {
-        BASE_SYNC_STATISTICS.incrementCreated();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementCreated();
+        baseSyncStatistics.incrementProcessed();
 
-        BASE_SYNC_STATISTICS.incrementFailed();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementFailed();
+        baseSyncStatistics.incrementProcessed();
 
-        BASE_SYNC_STATISTICS.incrementUpdated();
-        BASE_SYNC_STATISTICS.incrementProcessed();
+        baseSyncStatistics.incrementUpdated();
+        baseSyncStatistics.incrementProcessed();
 
-        BASE_SYNC_STATISTICS.calculateProcessingTime();
+        baseSyncStatistics.calculateProcessingTime();
 
-        final String statisticsAsJsonString = getStatisticsAsJSONString(BASE_SYNC_STATISTICS);
+        final String statisticsAsJsonString = getStatisticsAsJSONString(baseSyncStatistics);
         assertThat(statisticsAsJsonString)
                 .isEqualTo("{\"reportMessage\":\"Summary: 3 categories were processed in total (1 created, 1 updated and" +
                         " 1 categories failed to sync).\",\"" +
@@ -131,11 +131,11 @@ public class BaseSyncStatisticsTest {
                         "created\":1,\"" +
                         "failed\":1,\"" +
                         "processed\":3,\"" +
-                        "processingTimeInDays\":" + BASE_SYNC_STATISTICS.getProcessingTimeInDays() + ",\"" +
-                        "processingTimeInHours\":" + BASE_SYNC_STATISTICS.getProcessingTimeInHours() + ",\"" +
-                        "processingTimeInMinutes\":" + BASE_SYNC_STATISTICS.getProcessingTimeInMinutes() + ",\"" +
-                        "processingTimeInSeconds\":" + BASE_SYNC_STATISTICS.getProcessingTimeInSeconds() + ",\"" +
-                        "processingTimeInMillis\":" + BASE_SYNC_STATISTICS.getProcessingTimeInMillis() + ",\"" +
-                        "humanReadableProcessingTime\":\"" + BASE_SYNC_STATISTICS.getHumanReadableProcessingTime() + "\"}");
+                        "processingTimeInDays\":" + baseSyncStatistics.getProcessingTimeInDays() + ",\"" +
+                        "processingTimeInHours\":" + baseSyncStatistics.getProcessingTimeInHours() + ",\"" +
+                        "processingTimeInMinutes\":" + baseSyncStatistics.getProcessingTimeInMinutes() + ",\"" +
+                        "processingTimeInSeconds\":" + baseSyncStatistics.getProcessingTimeInSeconds() + ",\"" +
+                        "processingTimeInMillis\":" + baseSyncStatistics.getProcessingTimeInMillis() + ",\"" +
+                        "humanReadableProcessingTime\":\"" + baseSyncStatistics.getHumanReadableProcessingTime() + "\"}");
     }
 }
