0284221d973ca85eadd4a8e166ba042bcc6e1c6f
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-05-09 14:57:34 +0200

    update SkuKeyTuple description, annotations and hashCode

--- src/main/java/com/commercetools/sync/inventory/SkuKeyTuple.java
+++ src/main/java/com/commercetools/sync/inventory/SkuKeyTuple.java
@@ -4,15 +4,22 @@ import io.sphere.sdk.inventory.InventoryEntry;
 import io.sphere.sdk.inventory.InventoryEntryDraft;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 /**
  * Helper class that holds sku and supply channel key - values used for {@link io.sphere.sdk.inventory.InventoryEntry}
- * distinction. Can be used as key in Maps.
+ * distinction. Presence of sku and key in created instance is similar as for inventory entry resource: supply channel
+ * key can be {@code null} value since it is optional in inventory entry, but sku has to be not null neither not empty.
+ * Can be used as key in Maps.
  */
 final class SkuKeyTuple {
 
-    private String sku;
-    private String key;
+    private final String sku;
+
+    @Nullable
+    private final String key;
 
     private SkuKeyTuple(String sku, String key) {
         this.sku = sku;
@@ -23,11 +30,15 @@ final class SkuKeyTuple {
      * Returns new {@link SkuKeyTuple} of given {@link InventoryEntry}. If contains
      * {@link io.sphere.sdk.models.Reference} to supply channel, then reference should be expanded, so that key is
      * taken from it. If reference is null or is not expanded it's result in {@code null} key value.
-     * @param existingEntry must have sku set
+     * @param existingEntry must have sku set and not empty
      * @return new instance of {@link SkuKeyTuple}
+     * @throws IllegalArgumentException when sku of {@code existingEntry} is null or empty string
      */
     static SkuKeyTuple of(@Nonnull InventoryEntry existingEntry) {
         final String sku = existingEntry.getSku();
+        if (isEmpty(sku)) {
+            throw new IllegalArgumentException("Sku is not set");
+        }
         if (existingEntry.getSupplyChannel() != null && existingEntry.getSupplyChannel().getObj() != null) {
             return new SkuKeyTuple(sku, existingEntry.getSupplyChannel().getObj().getKey());
         } else {
@@ -42,11 +53,15 @@ final class SkuKeyTuple {
      *     <li>referenced object's key - when supply channel {@link io.sphere.sdk.models.Reference} is present and expanded</li>
      *     <li>{@code null} - otherwise</li>
      * </ul>
-     * @param newEntryDraft must have sku set
+     * @param newEntryDraft must have sku set and not empty
      * @return new instance of {@link SkuKeyTuple}
+     * @throws IllegalArgumentException when sku of {@code existingEntry} is null or empty string
      */
     static SkuKeyTuple of(@Nonnull InventoryEntryDraft newEntryDraft) {
         final String sku = newEntryDraft.getSku();
+        if (isEmpty(sku)) {
+            throw new IllegalArgumentException("Sku is not set");
+        }
         String key = null;
         if (newEntryDraft.getSupplyChannel() != null) {
             if (newEntryDraft.getSupplyChannel().getObj() != null) {
@@ -58,10 +73,12 @@ final class SkuKeyTuple {
         return new SkuKeyTuple(sku, key);
     }
 
+    @Nonnull
     String getSku() {
         return sku;
     }
 
+    @Nullable
     String getKey() {
         return key;
     }
@@ -80,7 +97,7 @@ final class SkuKeyTuple {
     @Override
     public int hashCode() {
         int result = getSku().hashCode();
-        result = 31 * result + (getKey() != null ? getKey().hashCode() : 0);
+        result = 31 * result ^ (getKey() != null ? getKey().hashCode() : 0);
         return result;
     }
 }
