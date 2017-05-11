c60173d67dd4834b2890c178ab4582828e8145e1
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-05-11 15:12:27 +0200

    remove unused statistic from InvenotrySyncStatistics, update report message

--- src/main/java/com/commercetools/sync/inventories/helpers/InventorySyncStatistics.java
+++ src/main/java/com/commercetools/sync/inventories/helpers/InventorySyncStatistics.java
@@ -9,33 +9,14 @@ import static java.lang.String.format;
  */
 public class InventorySyncStatistics extends BaseSyncStatistics {
 
-    private int unprocessedDueToEmptySku;
-
     public InventorySyncStatistics() {
         super();
-    }
-
-    /**
-     * Get the total number of resources that weren't processed because they had empty sku.
-     *
-     * @return total number of resources unprocessed due to empty sku.
-     */
-    public int getUnprocessedDueToEmptySku() {
-        return unprocessedDueToEmptySku;
-    }
-
-    /**
-     * Increment the total number of resources that weren't processed because they had empty sku.
-     */
-    public void incrementUnprocessedDueToEmptySku() {
-        this.unprocessedDueToEmptySku++;
     }
 
     /**
      * Builds a summary of the inventory sync statistics instance that looks like the following example:
      * <pre>
      *     Summary of inventory synchronisation:
-     *     2 inventory entries weren't processed due to empty sku.
      *     25 inventory entries were processed in total (9 created, 5 updated, 2 failed to sync).
      * </pre>
      * <strong>Note:</strong> second line is present only when there are any unprocessed entries.
@@ -44,12 +25,8 @@ public class InventorySyncStatistics extends BaseSyncStatistics {
      */
     @Override
     public String getReportMessage() {
-        String unprocessedReport =  unprocessedDueToEmptySku > 0
-                ? format("%d inventory entries weren't processed due to empty sku%n", unprocessedDueToEmptySku) : "";
-
-        String processedReport = format("%d inventory entries were processed in total (%d created, %d updated, %d failed to sync%n",
+        return format("%nSummary of inventory synchronisation:%n%d inventory entries were processed in total "
+                        + "(%d created, %d updated, %d failed to sync)%n",
                 getProcessed(), getCreated(), getUpdated(), getFailed());
-
-        return format("%nSummary of inventory synchronisation:%n%s%s%n", unprocessedReport, processedReport);
     }
 }
