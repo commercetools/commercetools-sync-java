895efd36f1fac110d07a5af2f119704dd0f83746
Hesham Massoud <hesham.massoud@commercetools.de>
2017-05-12 16:44:40 +0200

    #39: unit tests null categories.

--- src/test/java/com/commercetools/sync/categories/CategorySyncTest.java
+++ src/test/java/com/commercetools/sync/categories/CategorySyncTest.java
@@ -54,18 +54,18 @@ public class CategorySyncTest {
     }
 
     @Test
-    public void syncDrafts_WithANullDraft_ShouldSkipIt() {
+    public void syncDrafts_WithANullDraft_ShouldBeCountedAsFailed() {
         final ArrayList<CategoryDraft> categoryDrafts = new ArrayList<>();
         categoryDrafts.add(null);
 
         categorySync.syncDrafts(categoryDrafts);
         assertThat(categorySync.getStatistics().getCreated()).isEqualTo(0);
-        assertThat(categorySync.getStatistics().getFailed()).isEqualTo(0);
+        assertThat(categorySync.getStatistics().getFailed()).isEqualTo(1);
         assertThat(categorySync.getStatistics().getUpdated()).isEqualTo(0);
-        assertThat(categorySync.getStatistics().getProcessed()).isEqualTo(0);
+        assertThat(categorySync.getStatistics().getProcessed()).isEqualTo(1);
         assertThat(categorySync.getStatistics().getReportMessage()).isEqualTo(
-            "Summary: 0 categories were processed in total "
-                + "(0 created, 0 updated and 0 categories failed to sync).");
+            "Summary: 1 categories were processed in total "
+                + "(0 created, 0 updated and 1 categories failed to sync).");
     }
 
     @Test
