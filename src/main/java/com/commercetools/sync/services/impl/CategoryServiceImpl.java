cc7b61498a9d0f19901716c55c755897a129d391
Hesham Massoud <hesham.massoud@commercetools.de>
2017-05-12 14:25:37 +0200

    #22: Remove wrong throwing declaration in category service.

--- src/main/java/com/commercetools/sync/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/commercetools/sync/services/impl/CategoryServiceImpl.java
@@ -9,7 +9,6 @@ import io.sphere.sdk.categories.commands.CategoryUpdateCommand;
 import io.sphere.sdk.categories.queries.CategoryQuery;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.commands.UpdateAction;
-import io.sphere.sdk.models.SphereException;
 import io.sphere.sdk.queries.PagedResult;
 
 import javax.annotation.Nonnull;
@@ -27,15 +26,14 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Nonnull
     @Override
-    public CompletionStage<Optional<Category>> fetchCategoryByExternalId(@Nullable final String externalId)
-            throws SphereException {
+    public CompletionStage<Optional<Category>> fetchCategoryByExternalId(@Nullable final String externalId) {
         final CategoryQuery categoryQuery = CategoryQuery.of().byExternalId(externalId);
         return ctpClient.execute(categoryQuery).thenApply(PagedResult::head);
     }
 
     @Nonnull
     @Override
-    public CompletionStage<Category> createCategory(@Nonnull final CategoryDraft categoryDraft) throws SphereException {
+    public CompletionStage<Category> createCategory(@Nonnull final CategoryDraft categoryDraft) {
         final CategoryCreateCommand categoryCreateCommand = CategoryCreateCommand.of(categoryDraft);
         return ctpClient.execute(categoryCreateCommand);
 
@@ -44,8 +42,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Nonnull
     @Override
     public CompletionStage<Category> updateCategory(@Nonnull final Category category,
-                                   @Nonnull final List<UpdateAction<Category>> updateActions)
-            throws SphereException {
+                                   @Nonnull final List<UpdateAction<Category>> updateActions) {
         final CategoryUpdateCommand categoryUpdateCommand = CategoryUpdateCommand.of(category, updateActions);
         return ctpClient.execute(categoryUpdateCommand);
     }
