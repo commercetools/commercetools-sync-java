70e2b41982ebb026bdc20deb07ef45c96cb37855
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-05-26 10:37:55 +0200

    add draft of UpdateActions It tests

---
+++ src/integration-test/java/com/commercetools/sync/inventories/utils/InventoryUpdateActionUtilsItTest.java
@@ -0,0 +1,41 @@
+package com.commercetools.sync.inventories.utils;
+
+import org.junit.Test;
+
+public class InventoryUpdateActionUtilsItTest {
+
+    @Test
+    public void buildChangeQuantityAction_ShouldBuildActionThatUpdatesQuantity() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildSetRestockableInDaysAction_ShouldBuildActionThatUpdatesRestockableInDays() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildSetExpectedDeliveryAction_ShouldBuildActionThatUpdatesExpectedDelivery() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildSetSupplyChannelAction_ShouldBuildActionThatUpdatesSupplyChannel() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildCustomUpdateActions_WithDeletedCustom_ShouldBuildActionThatDeletesCustom() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildCustomUpdateActions_WithNewCustom_ShouldBuildActionThatAddsCustom() {
+        // TODO implement
+    }
+
+    @Test
+    public void buildCustomUpdateActions_WithUpdatedCustom_ShouldBuildActionThatUpdatesCustom() {
+        // TODO implement
+    }
+}
