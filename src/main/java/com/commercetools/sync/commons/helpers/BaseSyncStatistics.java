60108be259d6f4691f05c439e90399fe462d5b99
Jakub Chrobak <jakub.chrobak@futurum.tech>
2017-06-02 18:29:02 +0200

    delete unused import

--- src/main/java/com/commercetools/sync/commons/helpers/BaseSyncStatistics.java
+++ src/main/java/com/commercetools/sync/commons/helpers/BaseSyncStatistics.java
@@ -2,8 +2,6 @@ package com.commercetools.sync.commons.helpers;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.commons.lang3.time.DurationFormatUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 
