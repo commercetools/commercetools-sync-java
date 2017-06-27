0be1124e2932c81f62b60f8288afcd29f928ce05
Hesham Massoud <hesham.massoud@commercetools.de>
2017-06-27 15:08:22 +0200

    #45: Replace all 'externalId' usages in Category sync with 'key'.

--- src/main/java/com/commercetools/sync/services/CategoryService.java
+++ src/main/java/com/commercetools/sync/services/CategoryService.java
@@ -11,36 +11,36 @@ import java.util.concurrent.CompletionStage;
 
 public interface CategoryService {
     /**
-     * Given an {@code externalId}, this method first checks if cached map of category external ids -> ids is not empty.
-     * If not, it returns a completed future that contains an optional that contains what this external id maps to in
-     * the cache. If the cache is empty, the method populates the cache with the mapping of all categories' external ids
+     * Given a {@code key}, this method first checks if cached map of category keys -> ids is not empty.
+     * If not, it returns a completed future that contains an optional that contains what this key maps to in
+     * the cache. If the cache is empty, the method populates the cache with the mapping of all categories' keys
      * to ids in the CTP project. After that, the method returns a
      * {@link CompletionStage&lt;Optional&lt;String&gt;&gt;} in which the result of it's completion could contain an
      * {@link Optional} with the id inside of it or an empty {@link Optional} if no {@link Category} was
-     * found in the CTP project with this external id.
+     * found in the CTP project with this key.
      *
-     * @param externalId the externalId by which a {@link Category} id should be fetched from the CTP project.
+     * @param key the key by which a {@link Category} id should be fetched from the CTP project.
      * @return {@link CompletionStage&lt;Optional&lt;String&gt;&gt;} in which the result of it's completion could
      *      contain an {@link Optional} with the id inside of it or an empty {@link Optional} if no {@link Category} was
-     *      found in the CTP project with this external id.
+     *      found in the CTP project with this key.
      */
     @Nonnull
-    CompletionStage<Optional<String>> fetchCachedCategoryId(@Nonnull final String externalId);
+    CompletionStage<Optional<String>> fetchCachedCategoryId(@Nonnull final String key);
 
     /**
-     * Given an {@code externalId}, this method tries to fetch a {@link Category} with this {@code externalId} from the
+     * Given an {@code key}, this method tries to fetch a {@link Category} with this {@code key} from the
      * CTP project defined in a potentially injected {@link io.sphere.sdk.client.SphereClient}. This method returns
      * {@link CompletionStage&lt;Optional&lt;Category&gt;&gt;} in which the result of it's completion could contain an
      * {@link Optional} with the {@link Category} inside of it or an empty {@link Optional} if no {@link Category} was
      * found in the CTP project.
      *
-     * @param externalId the externalId by which a {@link Category} should be fetched from the CTP project.
+     * @param key the key by which a {@link Category} should be fetched from the CTP project.
      * @return {@link CompletionStage&lt;Optional&lt;Category&gt;&gt;} containing as a result of it's completion an
      *      {@link Optional} with the {@link Category} inside of it or an empty {@link Optional} if no {@link Category}
      *      was found in the CTP project, or a {@link io.sphere.sdk.models.SphereException}.
      */
     @Nonnull
-    CompletionStage<Optional<Category>> fetchCategoryByExternalId(@Nonnull final String externalId);
+    CompletionStage<Optional<Category>> fetchCategoryByKey(@Nonnull final String key);
 
     /**
      * Given a {@link CategoryDraft}, this method creates a {@link Category} based on it in the CTP project defined in
