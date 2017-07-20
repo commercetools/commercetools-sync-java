5a00c04bc964eeb204fd1be6c8e4093b482212d2
Hesham Massoud <hesham.massoud@commercetools.de>
2017-07-20 09:40:18 +0200

    #75: Expose utils for replacing reference ids with keys.

--- src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
@@ -1,12 +1,25 @@
 package com.commercetools.sync.commons.utils;
 
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.categories.CategoryDraft;
+import io.sphere.sdk.categories.CategoryDraftBuilder;
+import io.sphere.sdk.inventory.InventoryEntry;
+import io.sphere.sdk.inventory.InventoryEntryDraft;
+import io.sphere.sdk.inventory.InventoryEntryDraftBuilder;
+import io.sphere.sdk.models.Reference;
+import io.sphere.sdk.types.Custom;
 import io.sphere.sdk.types.CustomDraft;
+import io.sphere.sdk.types.CustomFields;
+import io.sphere.sdk.types.CustomFieldsDraft;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
-public final class BaseSyncUtils {
+public final class SyncUtils {
     /**
      * Given a list of resource (e.g. categories, products, etc..) drafts and a {@code batchSize}, this method separates
      * the drafts into batches with the {@code batchSize}. Each batch is represented by a
@@ -24,5 +37,79 @@ public final class BaseSyncUtils {
             batches.add(drafts.subList(i, Math.min(i + batchSize, drafts.size())));
         }
         return batches;
+    }
+
+    @Nullable
+    static <T extends Custom> CustomFieldsDraft replaceCustomTypeIdWithKeys(@Nonnull final T resource) {
+        final CustomFields custom = resource.getCustom();
+        if (custom != null) {
+            if (custom.getType().getObj() != null) {
+                return CustomFieldsDraft.ofTypeIdAndJson(custom.getType().getObj().getKey(),
+                    custom.getFieldsJsonMap());
+            }
+            return CustomFieldsDraft.ofCustomFields(custom);
+        }
+        return null;
+    }
+
+    @Nullable
+    static <T> Reference<T> replaceReferenceIdWithKey(@Nullable final Reference<T> reference,
+                                                      @Nonnull
+                                                      final Supplier<Reference<T>> keyInReferenceSupplier) {
+        if (reference != null && reference.getObj() != null) {
+            return keyInReferenceSupplier.get();
+        }
+        return reference;
+    }
+
+
+    /**
+     * Takes a list of Categories that are supposed to have their custom type and parent category reference expanded
+     * in order to be able to fetch the keys and replace the reference ids with the corresponding keys and then return
+     * a new list of category drafts with their references containing keys instead of the ids. Note that if the
+     * references are not expanded for a category, the reference ids will not be replaced with keys and will still have
+     * their ids in place.
+     *
+     * @param categories the categories to replace their reference ids with keys
+     * @return a list of category drafts with keys instead of ids for references.
+     */
+    @Nonnull
+    public static List<CategoryDraft> replaceCategoryReferenceIdsWithKeys(@Nonnull final List<Category> categories) {
+        return categories
+            .stream()
+            .map(category -> {
+                final CustomFieldsDraft customTypeWithKeysInReference = replaceCustomTypeIdWithKeys(category);
+                final Reference<Category> parentWithKeyInReference = replaceReferenceIdWithKey(category.getParent(),
+                    () -> Category.referenceOfId(category.getParent().getObj().getKey()));
+                return CategoryDraftBuilder.of(category)
+                                           .custom(customTypeWithKeysInReference)
+                                           .parent(parentWithKeyInReference)
+                                           .build();
+            })
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Takes a list of inventoryEntries that are supposed to have their custom type reference expanded
+     * in order to be able to fetch the keys and replace the reference ids with the corresponding keys and then return
+     * a new list of inventory entry drafts with their references containing keys instead of the ids.  Note that if the
+     * references are not expanded for an inventory entry, the reference ids will not be replaced with keys and will
+     * still have their ids in place.
+     *
+     * @param inventoryEntries the inventory entries to replace their reference ids with keys
+     * @return a list of inventoryEntry drafts with keys instead of ids for references.
+     */
+    @Nonnull
+    public static List<InventoryEntryDraft> replaceInventoriesReferenceIdsWithKeys(@Nonnull final List<InventoryEntry>
+                                                                                       inventoryEntries) {
+        return inventoryEntries
+            .stream()
+            .map(inventoryEntry -> {
+                final CustomFieldsDraft customTypeWithKeysInReference = replaceCustomTypeIdWithKeys(inventoryEntry);
+                return InventoryEntryDraftBuilder.of(inventoryEntry)
+                                                 .custom(customTypeWithKeysInReference)
+                                                 .build();
+            })
+            .collect(Collectors.toList());
     }
 }
