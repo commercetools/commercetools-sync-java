ba08c6a06451898e59f0fcd022a9d2cde8040cdf
Hesham Massoud <hesham.massoud@commercetools.de>
2017-07-21 16:19:01 +0200

    #31: Add repeater on 5xx http error codes.

--- src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
@@ -2,19 +2,30 @@ package com.commercetools.sync.commons.utils;
 
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.QueueSphereClientDecorator;
+import io.sphere.sdk.client.RetrySphereClientDecorator;
 import io.sphere.sdk.client.SphereAccessTokenSupplier;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.http.AsyncHttpClientAdapter;
 import io.sphere.sdk.http.HttpClient;
+import io.sphere.sdk.retry.RetryAction;
+import io.sphere.sdk.retry.RetryPredicate;
+import io.sphere.sdk.retry.RetryRule;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 
 import javax.annotation.Nonnull;
+import java.time.Duration;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import static io.sphere.sdk.http.HttpStatusCode.BAD_GATEWAY_502;
+import static io.sphere.sdk.http.HttpStatusCode.GATEWAY_TIMEOUT_504;
+import static io.sphere.sdk.http.HttpStatusCode.SERVICE_UNAVAILABLE_503;
 
 public class ClientConfigurationUtils {
     private static HttpClient httpClient;
@@ -36,7 +47,8 @@ public class ClientConfigurationUtils {
                 SphereAccessTokenSupplier.ofAutoRefresh(clientConfig, httpClient, false);
             final SphereClient underlying = SphereClient.of(clientConfig, httpClient, tokenSupplier);
             final SphereClient limitedParallelRequestsClient = withLimitedParallelRequests(underlying);
-            delegatesCache.put(clientConfig, limitedParallelRequestsClient);
+            final SphereClient retryClient = withRetry(limitedParallelRequestsClient);
+            delegatesCache.put(clientConfig, retryClient);
         }
         return BlockingSphereClient.of(delegatesCache.get(clientConfig), timeout, timeUnit);
     }
@@ -66,6 +78,16 @@ public class ClientConfigurationUtils {
             httpClient = AsyncHttpClientAdapter.of(asyncHttpClient);
         }
         return httpClient;
+    }
+
+    private static SphereClient withRetry(final SphereClient delegate) {
+        final int maxAttempts = 5;
+        final RetryAction scheduledRetry = RetryAction
+            .ofScheduledRetry(maxAttempts, context -> Duration.ofSeconds(context.getAttempt() * 2));
+        final RetryPredicate http5xxMatcher = RetryPredicate
+            .ofMatchingStatusCodes(BAD_GATEWAY_502, SERVICE_UNAVAILABLE_503, GATEWAY_TIMEOUT_504);
+        final List<RetryRule> retryRules = Collections.singletonList(RetryRule.of(http5xxMatcher, scheduledRetry));
+        return RetrySphereClientDecorator.of(delegate, retryRules);
     }
 
     private static SphereClient withLimitedParallelRequests(final SphereClient delegate) {
