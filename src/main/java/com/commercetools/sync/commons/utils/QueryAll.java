106f41f6ab2161b37fbdcc177541d7d527b639a9
Hesham Massoud <hesham.massoud@commercetools.de>
2017-08-16 14:06:19 +0300

    #81: Add some javadocs for QueryAll.

--- src/main/java/com/commercetools/sync/commons/utils/QueryAll.java
+++ src/main/java/com/commercetools/sync/commons/utils/QueryAll.java
@@ -27,6 +27,16 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
         this.pageSize = pageSize;
     }
 
+    /**
+     * Given a {@code callback} {@link Function}, this method applies this callback on each page of the results from
+     * {@link this} instance's {@code baseQuery} and returns a future containing a list containing the results of this
+     * callback on each page.
+     *
+     * @param client   the CTP client that the query is run on.
+     * @param callback the callback that gets called on each page of results.
+     * @param <S>      the type of the result of the callback on each page.
+     * @return a future containing a list of results of the callback on each page.
+     */
     @Nonnull
     <S> CompletionStage<List<S>> run(final SphereClient client, final Function<List<T>, S> callback) {
         return queryPage(client, 0).thenCompose(result -> {
@@ -42,6 +52,14 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
         });
     }
 
+    /**
+     * Given a {@link Consumer}, this method applies this consumer on each page of the results from
+     * {@link this} instance's {@code baseQuery}.
+     *
+     * @param client   the CTP client that the query is run on.
+     * @param consumer the consumer that gets called on each page of results.
+     * @return an empty future.
+     */
     @Nonnull
     CompletionStage<Void> run(final SphereClient client, final Consumer<List<T>> consumer) {
         return queryPage(client, 0).thenCompose(result -> {
@@ -52,6 +70,16 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
         });
     }
 
+    /**
+     * Given a {@link Consumer}, this method first calculates the total numbr of pages resulting from the query, then
+     * it applies this consumer on each page of the results from {@link this} instance's {@code baseQuery}
+     * and returns a list of futures after applying of this consumer on each page.
+     *
+     * @param client        the CTP client that the query is run on.
+     * @param totalElements the total number of elements resulting from the query.
+     * @param consumer      the consumer to apply on each page of results.
+     * @return a list of futures of applying the consumer on each page of results.
+     */
     @Nonnull
     private List<CompletableFuture<Void>> queryNextPages(final SphereClient client, final long totalElements,
                                                          final Consumer<List<T>> consumer) {
@@ -64,6 +92,16 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
                          .collect(toList());
     }
 
+    /**
+     * Given a callback {@link Function}, this method first calculates the total number of pages resulting from the
+     * query, then it applies this callback on each page of the results from {@link this} instance's {@code baseQuery}
+     * and returns a list containing the result of this callback on each page.
+     *
+     * @param client        the CTP client that the query is run on.
+     * @param totalElements the total number of elements resulting from the query.
+     * @param callback      the callback to apply on each page of results.
+     * @return a list of futures containing the results of applying the callback on each page of results.
+     */
     @Nonnull
     private <S> List<CompletableFuture<S>> queryNextPages(final SphereClient client, final long totalElements,
                                                           final Function<List<T>, S> callback) {
@@ -76,10 +114,24 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
                          .collect(toList());
     }
 
+    /**
+     * Given a total number of elements {@code totalElements} this method calculates the number of pages needed that
+     * would be needed to cover all the elements with set {@code pageSize} of {@link this} instance.
+     *
+     * @param totalElements number of elements to get the number of pages for.
+     * @return the total number of pages.
+     */
     long getTotalNumberOfPages(final long totalElements) {
         return (long) Math.ceil((double) totalElements / pageSize);
     }
 
+    /**
+     * Gets the results of {@link this} instance's query for a specific page with {@code pageNumber}.
+     *
+     * @param client     the CTP client that the query is run on.
+     * @param pageNumber the page number to get the results for.
+     * @return a future containing the results of the requested page of applying the query.
+     */
     @Nonnull
     private CompletionStage<PagedQueryResult<T>> queryPage(final SphereClient client, final long pageNumber) {
         final QueryDsl<T, C> query = baseQuery
@@ -88,6 +140,14 @@ final class QueryAll<T, C extends QueryDsl<T, C>> {
         return client.execute(query);
     }
 
+    /**
+     * Given a list of futures, this method converts it to a future containing a list of results of these futures after
+     * executing them in parallel with {@link CompletableFuture#allOf(CompletableFuture[])}.
+     *
+     * @param futures list of futures.
+     * @param <S>     the type of the results of the futures.
+     * @return a future containing a list of the results of the input futures.
+     */
     @Nonnull
     private <S> CompletableFuture<List<S>> transformListOfFuturesToFutureOfLists(
         final List<CompletableFuture<S>> futures) {
