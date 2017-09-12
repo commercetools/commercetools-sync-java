d293f164fb2698eef5d3c30564649c92b8a93fd7
Hesham Massoud <hesham.massoud@commercetools.de>
2017-09-12 16:13:42 +0200

    #57: Add integration tests for product sync from external source.

--- src/integration-test/java/com/commercetools/sync/integration/externalsource/products/ProductSyncIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/externalsource/products/ProductSyncIT.java
@@ -5,15 +5,25 @@ import com.commercetools.sync.products.ProductSyncOptions;
 import com.commercetools.sync.products.ProductSyncOptionsBuilder;
 import com.commercetools.sync.products.helpers.ProductSyncStatistics;
 import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.client.ConcurrentModificationException;
+import io.sphere.sdk.client.ErrorResponseException;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.products.CategoryOrderHints;
 import io.sphere.sdk.products.Product;
 import io.sphere.sdk.products.ProductDraft;
+import io.sphere.sdk.products.ProductVariantDraftBuilder;
 import io.sphere.sdk.products.commands.ProductCreateCommand;
+import io.sphere.sdk.products.commands.ProductUpdateCommand;
 import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.utils.CompletableFutureUtils;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,15 +44,22 @@ import static com.commercetools.sync.products.ProductSyncMockUtils.createProduct
 import static com.commercetools.sync.products.ProductSyncMockUtils.createProductDraftBuilder;
 import static com.commercetools.sync.products.ProductSyncMockUtils.createRandomCategoryOrderHints;
 import static java.lang.String.format;
+import static java.util.Collections.EMPTY_LIST;
+import static java.util.Collections.EMPTY_MAP;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class ProductSyncIT {
-
     private static ProductType productType;
     private static List<Category> categories;
     private ProductSyncOptions syncOptions;
     private Product product;
+    private List<String> errorCallBackMessages;
+    private List<String> warningCallBackMessages;
+    private List<Throwable> errorCallBackExceptions;
 
     /**
      * Delete all product related test data from target project. Then creates custom types for target CTP project
@@ -63,13 +80,27 @@ public class ProductSyncIT {
      */
     @Before
     public void setupTest() {
+
+        errorCallBackMessages = new ArrayList<>();
+        errorCallBackExceptions = new ArrayList<>();
+        warningCallBackMessages = new ArrayList<>();
         deleteAllProducts(CTP_TARGET_CLIENT);
 
         syncOptions = ProductSyncOptionsBuilder.of(CTP_TARGET_CLIENT)
-                                        .publish(true)
-                                        .revertStagedChanges(true)
-                                        .updateStaged(false)
-                                        .build();
+                                               .publish(true)
+                                               .revertStagedChanges(true)
+                                               .updateStaged(false)
+                                               .setErrorCallBack(
+                                                   (errorMessage, exception) -> {
+                                                       errorCallBackMessages
+                                                           .add(errorMessage);
+                                                       errorCallBackExceptions
+                                                           .add(exception);
+                                                   })
+                                               .setWarningCallBack(warningMessage ->
+                                                   warningCallBackMessages
+                                                       .add(warningMessage))
+                                               .build();
 
         final ProductDraft productDraft = createProductDraft(PRODUCT_KEY_1_PUBLISHED_RESOURCE_PATH, productType,
             categories, createRandomCategoryOrderHints(categories));
@@ -93,6 +124,31 @@ public class ProductSyncIT {
         assertThat(syncStatistics.getReportMessage())
             .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
                 + " failed to sync).", 1, 1, 0, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
+    }
+
+    @Test
+    public void sync_withNewProductWithExistingSlug_shouldNotCreateProduct() {
+        @SuppressWarnings("ConstantConditions")
+        final ProductDraft productDraft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .slug(product.getMasterData().getCurrent().getSlug())
+            .build();
+
+
+        final ProductSync productSync = new ProductSync(syncOptions);
+        final ProductSyncStatistics syncStatistics = productSync.sync(singletonList(productDraft))
+                                                                .toCompletableFuture().join();
+        assertThat(syncStatistics.getReportMessage())
+            .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
+                + " failed to sync).", 1, 0, 0, 1));
+        assertThat(errorCallBackExceptions).hasSize(1);
+        assertThat(errorCallBackExceptions.get(0)).isExactlyInstanceOf(ErrorResponseException.class);
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).contains(format("A duplicate value '\\\"%s\\\"' exists for field"
+            + " 'slug.en' on", product.getMasterData().getCurrent().getSlug().get(Locale.ENGLISH)));
+        assertThat(warningCallBackMessages).isEmpty();
     }
 
     @Test
@@ -107,6 +163,9 @@ public class ProductSyncIT {
         assertThat(syncStatistics.getReportMessage())
             .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
                 + " failed to sync).", 1, 0, 0, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
     }
 
     @Test
@@ -121,5 +180,233 @@ public class ProductSyncIT {
         assertThat(syncStatistics.getReportMessage())
             .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
                 + " failed to sync).", 1, 0, 1, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
+    }
+
+    @Test// TODO handle all retry cases
+    public void sync_withChangedProductButConcurrentModificationException_shouldRetryAndUpdateProduct() {
+        // Mock sphere client to return ConcurrentModification on the first update request.
+        final SphereClient spyClient = spy(CTP_TARGET_CLIENT);
+        when(spyClient.execute(any(ProductUpdateCommand.class)))
+            .thenReturn(CompletableFutureUtils.exceptionallyCompletedFuture(new ConcurrentModificationException()))
+            .thenCallRealMethod();
+
+        final ProductSyncOptions spyOptions = ProductSyncOptionsBuilder.of(spyClient)
+                                                                .setErrorCallBack(
+                                                                    (errorMessage, exception) -> {
+                                                                        errorCallBackMessages
+                                                                            .add(errorMessage);
+                                                                        errorCallBackExceptions
+                                                                            .add(exception);
+                                                                    })
+                                                                .setWarningCallBack(warningMessage ->
+                                                                    warningCallBackMessages
+                                                                        .add(warningMessage))
+                                                                .build();
+
+        final ProductSync spyProductSync = new ProductSync(spyOptions);
+
+        @SuppressWarnings("ConstantConditions")
+        final ProductDraft productDraft = createProductDraft(PRODUCT_KEY_1_CHANGED_RESOURCE_PATH,
+            productType, categories, product.getMasterData().getCurrent().getCategoryOrderHints());
+
+        final ProductSyncStatistics syncStatistics = spyProductSync.sync(singletonList(productDraft))
+                                                                .toCompletableFuture().join();
+        assertThat(syncStatistics.getReportMessage())
+            .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
+                + " failed to sync).", 1, 0, 1, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
+    }
+
+    @Test
+    public void sync_withMultipleBatchSyncing_ShouldSync() {
+        //_-----_-----_-----_-----_-----_PREPARE EXISTING PRODUCTS (productKey1, productKey2, productKey3)------
+        //_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----
+        @SuppressWarnings("ConstantConditions")
+
+        final ProductDraft key2Draft = createProductDraft(PRODUCT_KEY_2_RESOURCE_PATH,
+            productType, categories, product.getMasterData().getCurrent().getCategoryOrderHints());
+
+        CTP_TARGET_CLIENT.execute(ProductCreateCommand.of(key2Draft))
+                         .toCompletableFuture()
+                         .join();
+
+        final ProductDraft key3Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(EMPTY_LIST)
+            .categoryOrderHints(CategoryOrderHints.of(EMPTY_MAP))
+            .key("productKey3")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug3"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        CTP_TARGET_CLIENT.execute(ProductCreateCommand.of(key3Draft))
+                         .toCompletableFuture()
+                         .join();
+
+        //_-----_-----_-----_-----_-----_PREPARE BATCHES FROM EXTERNAL SOURCE-----_-----_-----_-----_-----_-----
+        //_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----_-----
+        final ProductDraft productDraft = createProductDraft(PRODUCT_KEY_1_CHANGED_RESOURCE_PATH,
+            productType, categories, product.getMasterData().getCurrent().getCategoryOrderHints());
+
+        final List<ProductDraft> batch1 = new ArrayList<>();
+        batch1.add(productDraft);
+
+        final ProductDraft key4Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(EMPTY_LIST)
+            .categoryOrderHints(CategoryOrderHints.of(EMPTY_MAP))
+            .key("productKey4")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug4"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final List<ProductDraft> batch2 = new ArrayList<>();
+        batch2.add(key4Draft);
+
+        final ProductDraft key3DraftNewSlug = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(EMPTY_LIST)
+            .categoryOrderHints(CategoryOrderHints.of(EMPTY_MAP))
+            .key("productKey3")
+            .slug(LocalizedString.of(Locale.ENGLISH, "newSlug"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final List<ProductDraft> batch3 = new ArrayList<>();
+        batch3.add(key3DraftNewSlug);
+
+
+        final ProductSync productSync = new ProductSync(syncOptions);
+        final ProductSyncStatistics syncStatistics = productSync.sync(batch1)
+                                                                .thenCompose(result -> productSync.sync(batch2))
+                                                                .thenCompose(result -> productSync.sync(batch3))
+                                                                .toCompletableFuture()
+                                                                .join();
+        assertThat(syncStatistics.getReportMessage())
+            .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
+                + " failed to sync).", 3, 1, 2, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
+    }
+
+
+    @Test
+    public void sync_withSingleBatchSyncing_ShouldSync() {
+        @SuppressWarnings("ConstantConditions")
+        //PREPARE BATCHES FROM EXTERNAL SOURCE
+        final ProductDraft productDraft = createProductDraft(PRODUCT_KEY_1_CHANGED_RESOURCE_PATH,
+            productType, categories, product.getMasterData().getCurrent().getCategoryOrderHints());
+
+        final ProductDraft key3Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey3")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug3"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key4Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey4")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug4"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key5Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey5")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug5"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key6Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey6")
+            .slug(LocalizedString.of(Locale.ENGLISH, "slug6"))
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final List<ProductDraft> batch1 = new ArrayList<>();
+        batch1.add(productDraft);
+        batch1.add(key3Draft);
+        batch1.add(key4Draft);
+        batch1.add(key5Draft);
+        batch1.add(key6Draft);
+
+        final ProductSync productSync = new ProductSync(syncOptions);
+        final ProductSyncStatistics syncStatistics = productSync.sync(batch1)
+                                                                .toCompletableFuture()
+                                                                .join();
+        assertThat(syncStatistics.getReportMessage())
+            .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
+                + " failed to sync).", 5, 4, 1, 0));
+        assertThat(errorCallBackExceptions).isEmpty();
+        assertThat(errorCallBackMessages).isEmpty();
+        assertThat(warningCallBackMessages).isEmpty();
+    }
+
+    @Test
+    public void sync_withSameSlugInSingleBatch_ShouldNotSyncIt() {
+        @SuppressWarnings("ConstantConditions")
+        //PREPARE BATCHES FROM EXTERNAL SOURCE
+        final ProductDraft productDraft = createProductDraft(PRODUCT_KEY_1_CHANGED_RESOURCE_PATH,
+            productType, categories, product.getMasterData().getCurrent().getCategoryOrderHints());
+
+        final ProductDraft key3Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey3")
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key4Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey4")
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key5Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey5")
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final ProductDraft key6Draft = createProductDraftBuilder(PRODUCT_KEY_2_RESOURCE_PATH, productType)
+            .categories(new ArrayList<>())
+            .categoryOrderHints(CategoryOrderHints.of(new HashMap<>()))
+            .key("productKey6")
+            .masterVariant(ProductVariantDraftBuilder.of().build())
+            .build();
+
+        final List<ProductDraft> batch1 = new ArrayList<>();
+        batch1.add(productDraft);
+        batch1.add(key3Draft);
+        batch1.add(key4Draft);
+        batch1.add(key5Draft);
+        batch1.add(key6Draft);
+
+        final ProductSync productSync = new ProductSync(syncOptions);
+        final ProductSyncStatistics syncStatistics = productSync.sync(batch1)
+                                                                .toCompletableFuture()
+                                                                .join();
+        assertThat(syncStatistics.getReportMessage())
+            .isEqualTo(format("Summary: %d products were processed in total (%d created, %d updated and %d products"
+                + " failed to sync).", 5, 1, 1, 3));
+        assertThat(errorCallBackExceptions).hasSize(3);
+        errorCallBackExceptions
+            .forEach(exception -> assertThat(exception).isExactlyInstanceOf(ErrorResponseException.class));
+        assertThat(errorCallBackMessages).hasSize(3);
+        errorCallBackMessages.forEach(errorMessage -> assertThat(errorMessage)
+                .contains(format("A duplicate value '\\\"%s\\\"' exists for field 'slug.en' on",
+                        key3Draft.getSlug().get(Locale.ENGLISH))));
+        assertThat(warningCallBackMessages).isEmpty();
     }
 }
