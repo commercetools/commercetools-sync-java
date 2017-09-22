936a33865e49631ea72dc9d802a97f8b4e7d1801
Hesham Massoud <hesham.massoud@commercetools.de>
2017-09-22 14:31:51 +0200

    #98: Add implementation for caching product attributes metadata from product type.

--- src/main/java/com/commercetools/sync/services/impl/ProductTypeServiceImpl.java
+++ src/main/java/com/commercetools/sync/services/impl/ProductTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.commercetools.sync.services.impl;
 
 import com.commercetools.sync.commons.utils.CtpQueryUtils;
+import com.commercetools.sync.products.AttributeMetaData;
 import com.commercetools.sync.products.ProductSyncOptions;
 import com.commercetools.sync.services.ProductTypeService;
 import io.sphere.sdk.producttypes.ProductType;
@@ -15,12 +16,14 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
 public class ProductTypeServiceImpl implements ProductTypeService {
     private final ProductSyncOptions syncOptions;
     private final Map<String, String> keyToIdCache = new ConcurrentHashMap<>();
+    private final Map<String, Map<String, AttributeMetaData>> productsAttributesMetaData = new ConcurrentHashMap<>();
     private static final String PRODUCT_TYPE_KEY_NOT_SET = "ProductType with id: '%s' has no key set. "
         + "Keys are required for productType matching.";
 
@@ -29,11 +32,21 @@ public class ProductTypeServiceImpl implements ProductTypeService {
     }
 
     @Nonnull
+    @Override
+    public CompletionStage<Optional<String>> fetchCachedProductTypeId(@Nonnull final String key) {
+        if (keyToIdCache.isEmpty()) {
+            return cacheAndFetch(key);
+        }
+        return CompletableFuture.completedFuture(Optional.ofNullable(keyToIdCache.get(key)));
+    }
+
+    @Nonnull
     private CompletionStage<Optional<String>> cacheAndFetch(@Nonnull final String key) {
         final Consumer<List<ProductType>> productTypePageConsumer = productTypePage ->
             productTypePage.forEach(type -> {
                 final String fetchedTypekey = type.getKey();
                 final String id = type.getId();
+                productsAttributesMetaData.put(id, getAttributeMetaDataMap(type));
                 if (StringUtils.isNotBlank(fetchedTypekey)) {
                     keyToIdCache.put(fetchedTypekey, id);
                 } else {
@@ -46,11 +59,36 @@ public class ProductTypeServiceImpl implements ProductTypeService {
     }
 
     @Nonnull
+    private static Map<String, AttributeMetaData> getAttributeMetaDataMap(@Nonnull final ProductType productType) {
+        return productType.getAttributes().stream()
+                          .map(AttributeMetaData::of)
+                          .collect(
+                              Collectors.toMap(AttributeMetaData::getName, attributeMetaData -> attributeMetaData));
+    }
+
+    @Nonnull
     @Override
-    public CompletionStage<Optional<String>> fetchCachedProductTypeId(@Nonnull final String key) {
-        if (keyToIdCache.isEmpty()) {
-            return cacheAndFetch(key);
+    public CompletionStage<Optional<Map<String, AttributeMetaData>>> fetchCachedProductAttributeMetaDataMap(
+        @Nonnull final String productTypeId) {
+        if (productsAttributesMetaData.isEmpty()) {
+            return cacheAndFetchProductMetaData(productTypeId);
         }
-        return CompletableFuture.completedFuture(Optional.ofNullable(keyToIdCache.get(key)));
+        return CompletableFuture.completedFuture(
+            Optional.ofNullable(productsAttributesMetaData.get(productTypeId))
+        );
+    }
+
+    @Nonnull
+    private CompletionStage<Optional<Map<String, AttributeMetaData>>> cacheAndFetchProductMetaData(
+        @Nonnull final String productTypeId) {
+        final Consumer<List<ProductType>> productTypePageConsumer = productTypePage ->
+            productTypePage.forEach(type -> {
+                final String id = type.getId();
+                productsAttributesMetaData.put(id, getAttributeMetaDataMap(type));
+            });
+
+        return CtpQueryUtils.queryAll(syncOptions.getCtpClient(), ProductTypeQuery.of(), productTypePageConsumer)
+                            .thenApply(result ->
+                                Optional.ofNullable(productsAttributesMetaData.get(productTypeId)));
     }
 }
