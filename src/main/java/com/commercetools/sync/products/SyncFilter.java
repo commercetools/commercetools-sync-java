71391b23ac8cb3eee82d21e8a9c298f1049c1757
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-04 09:48:31 +0200

    #122: Add JavaDoc to SyncFilter.

--- src/main/java/com/commercetools/sync/products/SyncFilter.java
+++ src/main/java/com/commercetools/sync/products/SyncFilter.java
@@ -3,7 +3,30 @@ package com.commercetools.sync.products;
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class SyncFilter {
+/**
+ * Defines either a blacklist or a whitelist for filtering certain update action groups ({@link ActionGroup}).
+ *
+ * <p>The action groups can be a list of any of the values of the enum {@link ActionGroup}, namely:
+ * <ul>
+ * <li>NAME</li>
+ * <li>DESCRIPTION</li>
+ * <li>SLUG</li>
+ * <li>SEARCHKEYWORDS</li>
+ * <li>METATITLE</li>
+ * <li>METADESCRIPTION</li>
+ * <li>METAKEYWORDS</li>
+ * <li>VARIANTS</li>
+ * <li>ATTRIBUTES</li>
+ * <li>PRICES</li>
+ * <li>IMAGES</li>
+ * <li>CATEGORIES</li>
+ * </ul>
+ *
+ * <p>The {@code filterType} defines whether the list is to be blacklisted ({@link UpdateFilterType#BLACKLIST}) or
+ * whitelisted ({@link UpdateFilterType#WHITELIST}). A blacklist means that <b>everything but</b> these action
+ * groups will be synced. A whitelist means that <b>only</b> these action groups will be synced.
+ */
+public final class SyncFilter {
 
     /**
      * Defines which attributes to calculate update actions for.
