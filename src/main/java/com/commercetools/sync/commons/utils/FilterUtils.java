f5c5639a791767bf4ea7657488a0b2744de3f19e
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-06 12:57:24 +0200

    #122: Add Javadocs and improve naming of util.

--- src/main/java/com/commercetools/sync/commons/utils/FilterUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/FilterUtils.java
@@ -11,30 +11,39 @@ import java.util.function.Supplier;
 
 public final class FilterUtils {
 
-    /** TODO
-     * @param syncFilter
-     * @param filter
-     * @param resultIfFilteredIn
-     * @param resultIfFilteredOut
-     * @param <T>
-     * @return
+    /**
+     * Given a {@link SyncFilter}, a {@link ActionGroup} and 2 {@link Supplier}. This method checks if the action group
+     * passes the criteria of the syncFiler, i.e. one of the following cases:
+     * 1. Is not blacklisted
+     * 2. Is whitelisted
+     * 3. There {@link SyncFilter} is null.
+     *
+     * <p>If the action group passes the filter, then the first supplier is executed, if not then the second supplier is
+     * executed.
+     *
+     * @param syncFilter          defines the blacklisting/whitelisted action groups.
+     * @param actionGroup         the action group under check.
+     * @param resultIfFilteredIn  the supplier to be executed if the action group passes the filter.
+     * @param resultIfFilteredOut the supplier to be executed if the action group doesn't pass the filter.
+     * @param <T>                 the type of the result of the suppliers and, in turn, the return of this method.
+     * @return the result of the executed supplier.
      */
     @Nonnull
     public static <T> T executeSupplierIfPassesFilter(
-        @Nullable final SyncFilter syncFilter, @Nonnull final ActionGroup filter,
+        @Nullable final SyncFilter syncFilter, @Nonnull final ActionGroup actionGroup,
         @Nonnull final Supplier<T> resultIfFilteredIn, @Nonnull final Supplier<T> resultIfFilteredOut) {
         if (syncFilter == null) {
-            // If there is no filter at all, then execute filter by default.
+            // If there is no filter at all, then execute first supplier by default.
             return resultIfFilteredIn.get();
         }
         final UpdateFilterType filterType = syncFilter.getFilterType();
         if (filterType.equals(UpdateFilterType.BLACKLIST)) {
             final List<ActionGroup> blackList = syncFilter.getFilters();
-            return !blackList.contains(filter) ? resultIfFilteredIn.get() : resultIfFilteredOut.get();
+            return !blackList.contains(actionGroup) ? resultIfFilteredIn.get() : resultIfFilteredOut.get();
         }
 
         final List<ActionGroup> whiteList = syncFilter.getFilters();
-        return whiteList.contains(filter) ? resultIfFilteredIn.get() : resultIfFilteredOut.get();
+        return whiteList.contains(actionGroup) ? resultIfFilteredIn.get() : resultIfFilteredOut.get();
     }
 
     private FilterUtils() {
