dfb731a2a66a08fa0085efacbd26379a3aaabce4
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-09 14:55:47 +0200

    #122: Improve JavaDoc and remove unneeded setter after change.

--- src/main/java/com/commercetools/sync/products/ProductSyncOptionsBuilder.java
+++ src/main/java/com/commercetools/sync/products/ProductSyncOptionsBuilder.java
@@ -42,61 +42,17 @@ public final class ProductSyncOptionsBuilder
     }
 
     /**
-     * Set option that defines either a blacklist or a whitelist for filtering certain update action groups.
-     *
-     * <p>The action groups can be a list of any of the values of the enum {@link ActionGroup}, namely:
-     *  <ul>
-     *      <li>NAME</li>
-     *      <li>DESCRIPTION</li>
-     *      <li>SLUG</li>
-     *      <li>SEARCHKEYWORDS</li>
-     *      <li>METATITLE</li>
-     *      <li>METADESCRIPTION</li>
-     *      <li>METAKEYWORDS</li>
-     *      <li>VARIANTS</li>
-     *      <li>ATTRIBUTES</li>
-     *      <li>PRICES</li>
-     *      <li>IMAGES</li>
-     *      <li>CATEGORIES</li>
-     *  </ul>
-     *
-     * <p>The {@code filterType} defines whether the list is to be blacklisted ({@link UpdateFilterType#BLACKLIST}) or
-     * whitelisted ({@link UpdateFilterType#WHITELIST}). A blacklist means that <b>everything but</b> these action
-     * groups will be synced. A whitelist means that <b>only</b> these action groups will be synced.
-     *
-     *
-     * @param filterType defines the type of the filter (either {@link UpdateFilterType#BLACKLIST} or
-     *                  {@link UpdateFilterType#WHITELIST}).
-     *
-     * @param actionGroups defines the action groups that should be filtered.
-     * @return {@code this} instance of {@link ProductSyncOptionsBuilder}
-     */
-    @Nonnull
-    public ProductSyncOptionsBuilder setSyncFilter(@Nonnull final UpdateFilterType filterType,
-                                                   @Nonnull final List<ActionGroup> actionGroups) {
-        this.syncFilter = SyncFilter.of(filterType, actionGroups);
-        return this;
-    }
-
-    /**
      * Set option that defines {@link SyncFilter} for the sync, which defines either a blacklist or a whitelist for
      * filtering certain update action groups.
      *
      * <p>The action groups can be a list of any of the values of the enum {@link ActionGroup}, namely:
-     *  <ul>
-     *      <li>NAME</li>
-     *      <li>DESCRIPTION</li>
-     *      <li>SLUG</li>
-     *      <li>SEARCHKEYWORDS</li>
-     *      <li>METATITLE</li>
-     *      <li>METADESCRIPTION</li>
-     *      <li>METAKEYWORDS</li>
-     *      <li>VARIANTS</li>
-     *      <li>ATTRIBUTES</li>
-     *      <li>PRICES</li>
-     *      <li>IMAGES</li>
-     *      <li>CATEGORIES</li>
-     *  </ul>
+     * <ul>
+     * <li>ATTRIBUTES</li>
+     * <li>PRICES</li>
+     * <li>IMAGES</li>
+     * <li>CATEGORIES</li>
+     * <li>.. and others</li>
+     * </ul>
      *
      * <p>The {@code filterType} defines whether the list is to be blacklisted ({@link UpdateFilterType#BLACKLIST}) or
      * whitelisted ({@link UpdateFilterType#WHITELIST}). A blacklist means that <b>everything but</b> these action
