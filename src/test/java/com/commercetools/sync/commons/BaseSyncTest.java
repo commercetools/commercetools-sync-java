e82c83392e4e1502e2713bcf36e9e6a9288a07bd
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-17 10:14:02 +0200

    #94: Move unit test for executeSupploerIfConcurrentModificationException.

---
+++ src/test/java/com/commercetools/sync/commons/BaseSyncTest.java
@@ -0,0 +1,37 @@
+package com.commercetools.sync.commons;
+
+import io.sphere.sdk.client.BadRequestException;
+import io.sphere.sdk.client.ConcurrentModificationException;
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static com.commercetools.sync.commons.BaseSync.executeSupplierIfConcurrentModificationException;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class BaseSyncTest {
+
+    @Test
+    public void
+        executeSupplierIfConcurrentModificationException_WithConcurrentModException_ShouldExecuteFirstSupplier() {
+        final Throwable sphereException = new ConcurrentModificationException();
+        final Supplier<Optional<String>> firstSupplier = () -> Optional.of("firstSupplier");
+        final Supplier<Optional<String>> secondSupplier = () -> Optional.of("SecondSupplier");
+        final Optional<String> result = executeSupplierIfConcurrentModificationException(sphereException, firstSupplier,
+            secondSupplier);
+        assertThat(result).contains("firstSupplier");
+    }
+
+    @Test
+    public void
+    executeSupplierIfConcurrentModificationException_WithConcurrentModException_ShouldExecuteSecondSupplier() {
+        final Throwable sphereException = new BadRequestException("message");
+        final Supplier<Optional<String>> firstSupplier = () -> Optional.of("firstSupplier");
+        final Supplier<Optional<String>> secondSupplier = () -> Optional.of("SecondSupplier");
+        final Optional<String> result = executeSupplierIfConcurrentModificationException(sphereException, firstSupplier,
+            secondSupplier);
+        assertThat(result).contains("SecondSupplier");
+    }
+}
