a60e6f565bda9d0a7d42c455ddf3148154172dc7
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-18 11:43:27 +0200

    #120: Add Javadocs to exposed methods.

--- src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public final class SyncUtils {
+
     /**
      * Given a list of resource (e.g. categories, products, etc..) drafts and a {@code batchSize}, this method separates
      * the drafts into batches with the {@code batchSize}. Each batch is represented by a
@@ -33,6 +34,20 @@ public final class SyncUtils {
         return batches;
     }
 
+    /**
+     * Given a resource of type {@link T} that extends {@link Custom} (i.e. it has {@link CustomFields}, this method
+     * checks if the custom fields are existing (not null) and they are reference expanded. If they are then
+     * it returns a {@link CustomFieldsDraft} instance with the custom type key in place of the id of the reference.
+     * Otherwise, if its not reference expanded it returns a {@link CustomFieldsDraft} with the id not replaced. If the
+     * resource has no or null {@link Custom}, then it returns {@code null}.
+     *
+     * @param resource the resource to replace its custom type id, if possible.
+     * @param <T> the type of the resource.
+     * @return an instance of {@link CustomFieldsDraft} instance with the custom type key in place of the id, if the
+     *         custom type reference was existing and reference expanded on the resource. Otherwise, if its not
+     *         reference expanded it returns a {@link CustomFieldsDraft} with the id not replaced with key. If the
+     *         resource has no or null {@link Custom}, then it returns {@code null}.
+     */
     @Nullable
     public static <T extends Custom> CustomFieldsDraft replaceCustomTypeIdWithKeys(@Nonnull final T resource) {
         final CustomFields custom = resource.getCustom();
@@ -46,9 +61,23 @@ public final class SyncUtils {
         return null;
     }
 
+    /**
+     * Given a reference to a resource of type {@link T}, this method
+     * checks if the reference is expanded. If it is, then it executes the {@code keyInReferenceSupplier} and returns
+     * it's result. Otherwise, it returns the supplied reference as is. Since, the reference could be {@code null}, this
+     * method could also return null if the reference was not expanded.
+     *
+     * @param reference              the reference of the resource to check if it's expanded.
+     * @param <T>                    the type of the resource.
+     * @param keyInReferenceSupplier the supplier to execute and return its result if the {@code reference} was
+     *                               expanded.
+     * @return returns the result of the {@code keyInReferenceSupplier} if the {@code reference} was expanded.
+     *         Otherwise, it returns the supplied reference as is.
+     */
     @Nullable
     public static <T> Reference<T> replaceReferenceIdWithKey(@Nullable final Reference<T> reference,
-                                                             @Nonnull final Supplier<Reference<T>> keyInReferenceSupplier) {
+                                                             @Nonnull final Supplier<Reference<T>>
+                                                                 keyInReferenceSupplier) {
         if (reference != null && reference.getObj() != null) {
             return keyInReferenceSupplier.get();
         }
