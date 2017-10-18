34c186608a216301b5a24c6f04c68de5829e8c83
Hesham Massoud <hesham.massoud@commercetools.de>
2017-10-18 10:30:57 +0200

    #120: Add state and taxCategory creation in ITUTils.

--- src/integration-test/java/com/commercetools/sync/integration/commons/utils/StateITUtils.java
+++ src/integration-test/java/com/commercetools/sync/integration/commons/utils/StateITUtils.java
@@ -3,7 +3,10 @@ package com.commercetools.sync.integration.commons.utils;
 import com.commercetools.sync.commons.utils.CtpQueryUtils;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.states.State;
+import io.sphere.sdk.states.StateDraft;
+import io.sphere.sdk.states.StateDraftBuilder;
 import io.sphere.sdk.states.StateType;
+import io.sphere.sdk.states.commands.StateCreateCommand;
 import io.sphere.sdk.states.commands.StateDeleteCommand;
 
 import javax.annotation.Nonnull;
@@ -15,8 +18,11 @@ import java.util.function.Consumer;
 import static com.commercetools.sync.integration.commons.utils.SphereClientUtils.CTP_SOURCE_CLIENT;
 import static com.commercetools.sync.integration.commons.utils.SphereClientUtils.CTP_TARGET_CLIENT;
 import static com.commercetools.sync.services.impl.StateServiceImpl.buildStateQuery;
+import static com.commercetools.tests.utils.CompletionStageUtil.executeBlocking;
 
 public class StateITUtils {
+
+    private static final String STATE_KEY = "old_state_key";
 
     /**
      * Deletes all states with {@code stateType} from CTP projects defined
@@ -48,4 +54,19 @@ public class StateITUtils {
                              .toArray(new CompletableFuture[stateDeleteFutures.size()])))
                      .toCompletableFuture().join();
     }
+
+    /**
+     * Creates a {@link State} with the {@link StateType} supplied in the CTP project defined by the {@code ctpClient}
+     * in a blocking fashion. The create state will have a key with the value {@value STATE_KEY}.
+     *
+     * @param ctpClient defines the CTP project to create the state in.
+     * @param stateType defines the state type to create the state with.
+     * @return the created state.
+     */
+    public static State createState(@Nonnull final SphereClient ctpClient, @Nonnull final StateType stateType) {
+        final StateDraft stateDraft = StateDraftBuilder.of(STATE_KEY, stateType)
+                                                       .build();
+        return executeBlocking(ctpClient.execute(StateCreateCommand.of(stateDraft)));
+    }
+
 }
