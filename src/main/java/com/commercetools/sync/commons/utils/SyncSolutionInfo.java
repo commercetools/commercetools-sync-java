0fcdd69853a9de031052158aeaba81eb998aeab4
Hesham Massoud <hesham.massoud@commercetools.de>
2017-11-20 16:48:34 +0100

    #191: Remove emergency contact and website from header.

--- src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
@@ -2,7 +2,6 @@ package com.commercetools.sync.commons.utils;
 
 import io.sphere.sdk.client.SolutionInfo;
 
-import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 
 public class SyncSolutionInfo extends SolutionInfo {
@@ -12,19 +11,15 @@ public class SyncSolutionInfo extends SolutionInfo {
      *
      * <p>A User-Agent header with a solution information looks like this:
      * {@code commercetools-jvm-sdk/1.4.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64)
-     * {@code implementationTitle}/{@code implementationVersion}
-     * (+https://github.com/commercetools/commercetools-sync-java; +ps-dev@commercetools.com)}</p>
+     * {implementationTitle}/{implementationVersion}}</p>
      *
      */
     public SyncSolutionInfo() {
-        final String libraryName = "commercetools-sync-java";
         final String implementationTitle = ofNullable(getClass().getPackage().getImplementationTitle())
-            .orElse(libraryName);
+            .orElse("commercetools-sync-java");
         final String implementationVersion = ofNullable(getClass().getPackage().getImplementationVersion())
             .orElse("DEBUG-VERSION");
         setName(implementationTitle);
         setVersion(implementationVersion);
-        setWebsite(format("https://github.com/commercetools/%s", libraryName));
-        setEmergencyContact("ps-dev@commercetools.com");
     }
 }
