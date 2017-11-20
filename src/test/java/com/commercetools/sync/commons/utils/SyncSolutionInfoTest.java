1e9e9bbd6afd15de41c26bb8bff1fd09e14b0853
Hesham Massoud <hesham.massoud@commercetools.de>
2017-11-20 17:45:21 +0100

    #191: Make Checkstyle happy.

--- src/test/java/com/commercetools/sync/commons/utils/SyncSolutionInfoTest.java
+++ src/test/java/com/commercetools/sync/commons/utils/SyncSolutionInfoTest.java
@@ -8,22 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SyncSolutionInfoTest {
 
     @Test
-    public void isAttributeUnspecified_WithNullAttribute_ShouldReturnTrue(){
+    public void isAttributeUnspecified_WithNullAttribute_ShouldReturnTrue() {
         assertThat(SyncSolutionInfo.isAttributeUnspecified(null)).isTrue();
     }
 
     @Test
-    public void isAttributeUnspecified_WithEmptyAttribute_ShouldReturnTrue(){
+    public void isAttributeUnspecified_WithEmptyAttribute_ShouldReturnTrue() {
         assertThat(SyncSolutionInfo.isAttributeUnspecified("")).isTrue();
     }
 
     @Test
-    public void isAttributeUnspecified_WithUnspecifiedAttribute_ShouldReturnTrue(){
+    public void isAttributeUnspecified_WithUnspecifiedAttribute_ShouldReturnTrue() {
         assertThat(SyncSolutionInfo.isAttributeUnspecified(UNSPECIFIED)).isTrue();
     }
 
     @Test
-    public void isAttributeUnspecified_WithAttribute_ShouldReturnFalse(){
+    public void isAttributeUnspecified_WithAttribute_ShouldReturnFalse() {
         assertThat(SyncSolutionInfo.isAttributeUnspecified("v1.0.0-M6")).isFalse();
     }
 }
