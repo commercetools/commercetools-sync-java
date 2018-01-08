c5ed21cbc680a3076b277e4816eac938476067b5
Hesham Massoud <hesham.massoud@commercetools.de>
2018-01-08 14:56:57 +0100

    #227: Remove fetching lib info from manifest and instead use constant string which is potentially injected by bash script.

--- src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
@@ -2,12 +2,12 @@ package com.commercetools.sync.commons.utils;
 
 import io.sphere.sdk.client.SolutionInfo;
 
-import javax.annotation.Nullable;
-
-import static org.apache.commons.lang3.StringUtils.isBlank;
-
-public class SyncSolutionInfo extends SolutionInfo {
-    static final String UNSPECIFIED = "unspecified";
+public final class SyncSolutionInfo extends SolutionInfo {
+    private static final String LIB_NAME = "commercetools-sync-java";
+    /**
+     * This value is injected from set-solutioninfo-version.sh.
+     */
+    private static final String LIB_VERSION = "#{LIB_VERSION}";
 
     /**
      * Extends {@link SolutionInfo} class of the JVM SDK to append to the User-Agent header with information of the
@@ -15,21 +15,11 @@ public class SyncSolutionInfo extends SolutionInfo {
      *
      * <p>A User-Agent header with a solution information looks like this:
      * {@code commercetools-jvm-sdk/1.4.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64)
-     * {implementationTitle}/{implementationVersion}}</p>
+     * {@value LIB_NAME}/{@value LIB_VERSION}}</p>
      *
      */
     public SyncSolutionInfo() {
-        final String implementationTitle = getClass().getPackage().getImplementationTitle();
-        final String implementationVersion = getClass().getPackage().getImplementationVersion();
-        final String solutionName = isAttributeUnspecified(implementationTitle)
-            ? "commercetools-sync-java" : implementationTitle;
-        final String solutionVersion = isAttributeUnspecified(implementationVersion)
-            ? "DEBUG-VERSION" : implementationVersion;
-        setName(solutionName);
-        setVersion(solutionVersion);
-    }
-
-    static boolean isAttributeUnspecified(@Nullable final String attributeValue) {
-        return isBlank(attributeValue) || UNSPECIFIED.equals(attributeValue);
+        setName(LIB_NAME);
+        setVersion(LIB_VERSION);
     }
 }
