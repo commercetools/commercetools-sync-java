a38fad8cacbed79a84c847496ffd3b2074da93b7
Hesham Massoud <hesham.massoud@commercetools.de>
2018-01-08 15:39:41 +0100

    #227: Remove extra space before .bak

--- set-solutioninfo-version.sh
+++ set-solutioninfo-version.sh
@@ -12,7 +12,7 @@ if [ "$TAG" ]; then
   fi
 
 printf "\n\nInjecting library version '$LIB_VERSION' into $SOLUTION_INFO_PATH\n\n"
-sed -i .bak "s/$VERSION_PLACEHOLDER/$LIB_VERSION/g" "$SOLUTION_INFO_PATH"
+sed -i.bak "s/$VERSION_PLACEHOLDER/$LIB_VERSION/g" "$SOLUTION_INFO_PATH"
 
 
 
