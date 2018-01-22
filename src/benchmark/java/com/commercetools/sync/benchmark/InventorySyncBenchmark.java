fe6dc8b3cc9435136bbc4ca80b6e99ab9762ef4a
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-01-23 00:50:57 +0100

    #155: Don’t ignore inventory sync benchmark.

--- src/benchmark/java/com/commercetools/sync/benchmark/InventorySyncBenchmark.java
+++ src/benchmark/java/com/commercetools/sync/benchmark/InventorySyncBenchmark.java
@@ -9,7 +9,6 @@ import io.sphere.sdk.inventory.InventoryEntryDraft;
 import io.sphere.sdk.inventory.InventoryEntryDraftBuilder;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
@@ -33,7 +32,7 @@ import static com.commercetools.tests.utils.CompletionStageUtil.executeBlocking;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Ignore
+
 public class InventorySyncBenchmark {
     @Before
     public void setup() {
