778581a5b97ca43b5af4e9e4bb3bf17d45f64b5f
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-16 19:30:14 +0100

    #3: Add ProductVariantAssetUpdateActionUtils.

---
+++ src/main/java/com/commercetools/sync/products/utils/ProductVariantAssetUpdateActionUtils.java
@@ -0,0 +1,86 @@
+package com.commercetools.sync.products.utils;
+
+import com.commercetools.sync.commons.utils.CustomUpdateActionUtils;
+import com.commercetools.sync.products.ProductSyncOptions;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.models.Asset;
+import io.sphere.sdk.models.AssetDraft;
+import io.sphere.sdk.products.Product;
+import io.sphere.sdk.products.commands.updateactions.ChangeAssetName;
+import io.sphere.sdk.products.commands.updateactions.SetAssetDescription;
+import io.sphere.sdk.products.commands.updateactions.SetAssetSources;
+import io.sphere.sdk.products.commands.updateactions.SetAssetTags;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Optional;
+
+import static com.commercetools.sync.commons.utils.CommonTypeUpdateActionUtils.buildUpdateAction;
+
+// TODO: Add tests.
+public final class ProductVariantAssetUpdateActionUtils {
+    private static final String ATTRIBUTE_NOT_IN_ATTRIBUTE_METADATA = "Cannot find the attribute with the name '%s'"
+        + " in the supplied attribute metadata.";
+    private static final String REQUIRED_ATTRIBUTE_VALUE_IS_NULL = "The attribute with the name '%s' is null but it is"
+        + " required to have a value according to the supplied attribute metadata.";
+
+    @Nonnull
+    public static Optional<UpdateAction<Product>> buildChangeAssetNameUpdateAction(
+        final int variantId,
+        @Nonnull final Asset oldProductVariantAsset,
+        @Nonnull final AssetDraft newProductVariantAsset) {
+        return buildUpdateAction(oldProductVariantAsset.getName(), newProductVariantAsset.getName(),
+            () -> ChangeAssetName.ofAssetKeyAndVariantId(variantId, oldProductVariantAsset.getKey(),
+                newProductVariantAsset.getName(), true));
+    }
+
+    @Nonnull
+    public static Optional<UpdateAction<Product>> buildSetAssetDescriptionUpdateAction(
+        final int variantId,
+        @Nonnull final Asset oldProductVariantAsset,
+        @Nonnull final AssetDraft newProductVariantAsset) {
+        return buildUpdateAction(oldProductVariantAsset.getDescription(), newProductVariantAsset.getDescription(),
+            () -> SetAssetDescription.ofVariantIdAndAssetKey(variantId, oldProductVariantAsset.getKey(),
+                newProductVariantAsset.getDescription(), true));
+    }
+
+    @Nonnull
+    public static Optional<UpdateAction<Product>> buildSetAssetTagsUpdateAction(
+        final int variantId,
+        @Nonnull final Asset oldProductVariantAsset,
+        @Nonnull final AssetDraft newProductVariantAsset) {
+        return buildUpdateAction(oldProductVariantAsset.getTags(), newProductVariantAsset.getTags(),
+            () -> SetAssetTags.ofVariantIdAndAssetKey(variantId, oldProductVariantAsset.getKey(),
+                newProductVariantAsset.getTags(), true));
+    }
+
+    @Nonnull
+    public static Optional<UpdateAction<Product>> buildSetAssetSourcesUpdateAction(
+        final int variantId,
+        @Nonnull final Asset oldProductVariantAsset,
+        @Nonnull final AssetDraft newProductVariantAsset) {
+        return buildUpdateAction(oldProductVariantAsset.getSources(), newProductVariantAsset.getSources(),
+            () -> SetAssetSources.ofVariantIdAndAssetKey(variantId, oldProductVariantAsset.getKey(),
+                newProductVariantAsset.getSources(), true));
+    }
+
+    @Nonnull
+    public static List<UpdateAction<Product>> buildCustomUpdateActions(
+        final int variantId,
+        @Nonnull final Asset oldProductVariantAsset,
+        @Nonnull final AssetDraft newProductVariantAsset,
+        @Nonnull final ProductSyncOptions syncOptions) {
+        return CustomUpdateActionUtils.buildCustomUpdateActions(oldProductVariantAsset, newProductVariantAsset,
+            Product.class, variantId, Asset::getId, asset -> Asset.resourceTypeId(), Asset::getKey, syncOptions);
+    }
+
+    /*
+    @Nonnull
+    public static List<UpdateAction<Product>> buildPriceCustomUpdateActions(
+        @Nonnull final Price oldPrice,
+        @Nonnull final PriceDraft newPrice,
+        @Nonnull final ProductSyncOptions syncOptions) {
+        return CustomUpdateActionUtils.buildCustomUpdateActions(oldPrice, newPrice, Product.class, -1,
+            Price::getId, price -> Price.resourceTypeId(), Price::getId, syncOptions);
+    }*/
+}
