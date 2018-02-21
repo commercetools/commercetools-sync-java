d9695b1c120aca18adfd857035e1347adce8a848
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-21 15:08:46 +0100

    #3: Add more test cases.

--- src/test/java/com/commercetools/sync/commons/utils/StreamUtilsTest.java
+++ src/test/java/com/commercetools/sync/commons/utils/StreamUtilsTest.java
@@ -19,20 +19,44 @@ public class StreamUtilsTest {
     }
 
     @Test
-    public void map_nonEmptyStream_ShouldReturnMappedStream() {
-        final Stream<Integer> mappedStream = map(Stream.of("foo", "bar"), String::length);
+    public void map_streamWithOnlyNullElements_ShouldReturnAnEmptyStream() {
+        final Stream<Integer> mappedStream = map(Stream.of(null, null, null), String::length);
+        final List<Integer> mappedList = mappedStream.collect(toList());
+        assertThat(mappedList).isEmpty();
+    }
+
+    @Test
+    public void map_streamWithOneNullElement_ShouldReturnAnEmptyStream() {
+        final String nullString = null;
+        final Stream<Integer> mappedStream = map(Stream.of(nullString), String::length);
+        final List<Integer> mappedList = mappedStream.collect(toList());
+        assertThat(mappedList).isEmpty();
+    }
+
+    @Test
+    public void map_singleElementStream_ShouldReturnMappedStream() {
+        final Stream<Integer> mappedStream = map(Stream.of("foo"), String::length);
+        final List<Integer> mappedList = mappedStream.collect(toList());
+        assertThat(mappedList).isNotEmpty();
+        assertThat(mappedList).hasSize(1);
+        assertThat(mappedList).containsOnly(3);
+    }
+
+    @Test
+    public void map_multipleElementsStream_ShouldReturnMappedStream() {
+        final Stream<Integer> mappedStream = map(Stream.of("james", "hetfield"), String::length);
         final List<Integer> mappedList = mappedStream.collect(toList());
         assertThat(mappedList).isNotEmpty();
         assertThat(mappedList).hasSize(2);
-        assertThat(mappedList).containsExactlyInAnyOrder(3, 3);
+        assertThat(mappedList).containsExactly(5, 8);
     }
 
     @Test
     public void map_nonEmptyStreamWithNullElements_ShouldReturnMappedStream() {
-        final Stream<Integer> mappedStream = map(Stream.of("foo", "bar", null), String::length);
+        final Stream<Integer> mappedStream = map(Stream.of("james", "hetfield", null), String::length);
         final List<Integer> mappedList = mappedStream.collect(toList());
         assertThat(mappedList).isNotEmpty();
         assertThat(mappedList).hasSize(2);
-        assertThat(mappedList).containsExactlyInAnyOrder(3, 3);
+        assertThat(mappedList).containsExactly(5, 8);
     }
 }
