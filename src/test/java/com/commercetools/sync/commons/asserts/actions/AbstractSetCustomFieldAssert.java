1b6054f7370aedec2462347d191dc6b923e1dba0
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-26 09:59:26 +0100

    #3: Add update actions assertion helpers.

---
+++ src/test/java/com/commercetools/sync/commons/asserts/actions/AbstractSetCustomFieldAssert.java
@@ -0,0 +1,39 @@
+package com.commercetools.sync.commons.asserts.actions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.sphere.sdk.types.customupdateactions.SetCustomFieldBase;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AbstractSetCustomFieldAssert<T extends AbstractSetCustomFieldAssert<T, S>, S extends SetCustomFieldBase>
+    extends AbstractUpdateActionAssert<T, S> {
+
+
+    AbstractSetCustomFieldAssert(@Nullable final S actual, @Nonnull final Class<T> selfType) {
+        super(actual, selfType);
+    }
+
+    /**
+     * Verifies that the actual {@link SetCustomFieldBase} value has identical fields as the ones supplied.
+     *
+     * @param actionName       the update action name.
+     * @param customFieldName  the custom field name to update.
+     * @param customFieldValue the new custom field name to update.
+     * @return {@code this} assertion object.
+     * @throws AssertionError if the actual value is {@code null}.
+     * @throws AssertionError if the actual fields do not match the supplied values.
+     */
+    public AbstractSetCustomFieldAssert hasValues(
+        @Nonnull final String actionName,
+        @Nullable final String customFieldName,
+        @Nullable final JsonNode customFieldValue) {
+
+        super.hasValues(actionName);
+        assertThat(actual.getName()).isEqualTo(customFieldName);
+        assertThat(actual.getValue()).isEqualTo(customFieldValue);
+        return myself;
+    }
+}
---
+++ src/test/java/com/commercetools/sync/commons/asserts/actions/SetPriceCustomTypeAssert.java
@@ -0,0 +1,47 @@
+package com.commercetools.sync.commons.asserts.actions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.sphere.sdk.models.ResourceIdentifier;
+import io.sphere.sdk.products.commands.updateactions.SetProductPriceCustomType;
+import io.sphere.sdk.types.Type;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SetPriceCustomTypeAssert
+    extends AbstractSetCustomTypeAssert<SetPriceCustomTypeAssert, SetProductPriceCustomType> {
+
+    SetPriceCustomTypeAssert(@Nullable final SetProductPriceCustomType actual) {
+        super(actual, SetPriceCustomTypeAssert.class);
+    }
+
+    /**
+     * Verifies that the actual {@link io.sphere.sdk.products.commands.updateactions.SetAssetCustomType} value has
+     * identical fields as the ones supplied.
+     *
+     * @param actionName   the update action name.
+     * @param priceId      the price id that has the custom fields.
+     * @param staged       the staged flag of the action.
+     * @param customFields the new custom type fields.
+     * @param type         the new custom type.
+     * @return {@code this} assertion object.
+     * @throws AssertionError if the actual value is {@code null}.
+     * @throws AssertionError if the actual fields do not match the supplied values.
+     */
+    public SetPriceCustomTypeAssert hasValues(
+        @Nonnull final String actionName,
+        @Nullable final String priceId,
+        @Nullable final Boolean staged,
+        @Nullable final Map<String, JsonNode> customFields,
+        @Nullable final ResourceIdentifier<Type> type) {
+
+        super.hasValues(actionName, customFields, type);
+        assertThat(actual.getStaged()).isEqualTo(staged);
+        assertThat(actual.getPriceId()).isEqualTo(priceId);
+        return myself;
+    }
+}
