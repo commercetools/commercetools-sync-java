bc34e705c6a658f6d2f6eee12437a7d141fd6c14
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-27 14:35:43 +0100

    #3: Add unit tests and Javadoc for CategoryAssetUpdateActionUtils.

--- src/main/java/com/commercetools/sync/categories/utils/CategoryAssetUpdateActionUtils.java
+++ src/main/java/com/commercetools/sync/categories/utils/CategoryAssetUpdateActionUtils.java
@@ -11,6 +11,7 @@ import io.sphere.sdk.categories.commands.updateactions.SetAssetTags;
 import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.models.Asset;
 import io.sphere.sdk.models.AssetDraft;
+import io.sphere.sdk.models.LocalizedString;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -20,10 +21,19 @@ import java.util.stream.Collectors;
 import static com.commercetools.sync.commons.utils.CommonTypeUpdateActionUtils.buildUpdateAction;
 import static java.util.Arrays.asList;
 
-//TODO: CONSIDER CHANGE NAME.
-// TODO: Add tests.
 public final class CategoryAssetUpdateActionUtils {
 
+    /**
+     * Compares all the fields of an {@link Asset} and an {@link AssetDraft} and returns a list of
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result. If both the {@link Asset} and the {@link AssetDraft}
+     * have identical fields, then no update action is needed and hence an empty {@link List} is returned.
+     *
+     * @param oldAsset    the asset which should be updated.
+     * @param newAsset    the asset draft where we get the new fields.
+     * @param syncOptions responsible for supplying the sync options to the sync utility method. It is used for
+     *                    triggering the error callback within the utility, in case of errors.
+     * @return A list with the update actions or an empty list if the asset fields are identical.
+     */
     @Nonnull
     public static List<UpdateAction<Category>> buildActions(@Nonnull final Asset oldAsset,
                                                             @Nonnull final AssetDraft newAsset,
@@ -49,7 +59,7 @@ public final class CategoryAssetUpdateActionUtils {
      * @param optionalUpdateActions list of category {@link UpdateAction} elements,
      *                              where each is wrapped in an {@link Optional}.
      * @return a List of category update actions from the optionals that were present in the
-     * {@code optionalUpdateActions} list parameter.
+     *         {@code optionalUpdateActions} list parameter.
      */
     @Nonnull
     private static List<UpdateAction<Category>> buildUpdateActionsFromOptionals(
@@ -60,6 +70,16 @@ public final class CategoryAssetUpdateActionUtils {
                                     .collect(Collectors.toList());
     }
 
+    /**
+     * Compares the {@link LocalizedString} names of an {@link Asset} and an {@link AssetDraft} and returns an
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result in an {@link Optional}. If both the {@link Asset} and
+     * the {@link AssetDraft} have the same name, then no update action is needed and hence an empty {@link Optional}
+     * is returned.
+     *
+     * @param oldAsset the asset which should be updated.
+     * @param newAsset the asset draft where we get the new name.
+     * @return A filled optional with the update action or an empty optional if the names are identical.
+     */
     @Nonnull
     public static Optional<UpdateAction<Category>> buildChangeAssetNameUpdateAction(
         @Nonnull final Asset oldAsset,
@@ -68,6 +88,16 @@ public final class CategoryAssetUpdateActionUtils {
             () -> ChangeAssetName.ofKey(oldAsset.getKey(), newAsset.getName()));
     }
 
+    /**
+     * Compares the {@link LocalizedString} descriptions of an {@link Asset} and an {@link AssetDraft} and returns an
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result in an {@link Optional}. If both the {@link Asset} and
+     * the {@link AssetDraft} have the same description, then no update action is needed and hence an empty
+     * {@link Optional} is returned.
+     *
+     * @param oldAsset the asset which should be updated.
+     * @param newAsset the asset draft where we get the new description.
+     * @return A filled optional with the update action or an empty optional if the descriptions are identical.
+     */
     @Nonnull
     public static Optional<UpdateAction<Category>> buildSetAssetDescriptionUpdateAction(
         @Nonnull final Asset oldAsset,
@@ -76,6 +106,16 @@ public final class CategoryAssetUpdateActionUtils {
             () -> SetAssetDescription.ofKey(oldAsset.getKey(), newAsset.getDescription()));
     }
 
+    /**
+     * Compares the tags of an {@link Asset} and an {@link AssetDraft} and returns an
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result in an {@link Optional}. If both the {@link Asset} and
+     * the {@link AssetDraft} have the same tags, then no update action is needed and hence an empty {@link Optional} is
+     * returned.
+     *
+     * @param oldAsset the asset which should be updated.
+     * @param newAsset the asset draft where we get the new tags.
+     * @return A filled optional with the update action or an empty optional if the tags are identical.
+     */
     @Nonnull
     public static Optional<UpdateAction<Category>> buildSetAssetTagsUpdateAction(
         @Nonnull final Asset oldAsset,
@@ -84,6 +124,16 @@ public final class CategoryAssetUpdateActionUtils {
             () -> SetAssetTags.ofKey(oldAsset.getKey(), newAsset.getTags()));
     }
 
+    /**
+     * Compares the sources of an {@link Asset} and an {@link AssetDraft} and returns an
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result in an {@link Optional}. If both the {@link Asset} and
+     * the {@link AssetDraft} have the same sources, then no update action is needed and hence an empty {@link Optional}
+     * is returned.
+     *
+     * @param oldAsset the asset which should be updated.
+     * @param newAsset the asset draft where we get the new sources.
+     * @return A filled optional with the update action or an empty optional if the sources are identical.
+     */
     @Nonnull
     public static Optional<UpdateAction<Category>> buildSetAssetSourcesUpdateAction(
         @Nonnull final Asset oldAsset,
@@ -92,6 +142,19 @@ public final class CategoryAssetUpdateActionUtils {
             () -> SetAssetSources.ofKey(oldAsset.getKey(), newAsset.getSources()));
     }
 
+    /**
+     * Compares the custom fields and custom types of an {@link Asset} and an {@link AssetDraft} and returns a list of
+     * {@link UpdateAction}&lt;{@link Category}&gt; as a result. If both the {@link Asset} and the {@link AssetDraft}
+     * have identical custom fields and types, then no update action is needed and hence an empty {@link List} is
+     * returned.
+     *
+     * @param oldAsset    the asset which should be updated.
+     * @param newAsset    the asset draft where we get the new custom fields and types.
+     * @param syncOptions responsible for supplying the sync options to the sync utility method. It is used for
+     *                    triggering the error callback within the utility, in case of errors.
+     * @return A list with the custom field/type update actions or an empty list if the custom fields/types are
+     *         identical.
+     */
     @Nonnull
     public static List<UpdateAction<Category>> buildCustomUpdateActions(
         @Nonnull final Asset oldAsset,
---
+++ src/test/java/com/commercetools/sync/categories/utils/CategoryAssetUpdateActionUtilsTest.java
@@ -0,0 +1,341 @@
+package com.commercetools.sync.categories.utils;
+
+import com.commercetools.sync.categories.CategorySyncOptions;
+import com.commercetools.sync.categories.CategorySyncOptionsBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.categories.commands.updateactions.ChangeAssetName;
+import io.sphere.sdk.categories.commands.updateactions.SetAssetCustomField;
+import io.sphere.sdk.categories.commands.updateactions.SetAssetDescription;
+import io.sphere.sdk.categories.commands.updateactions.SetAssetSources;
+import io.sphere.sdk.categories.commands.updateactions.SetAssetTags;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.models.Asset;
+import io.sphere.sdk.models.AssetDraft;
+import io.sphere.sdk.models.AssetDraftBuilder;
+import io.sphere.sdk.models.AssetSource;
+import io.sphere.sdk.models.AssetSourceBuilder;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.types.CustomFields;
+import io.sphere.sdk.types.CustomFieldsDraft;
+import io.sphere.sdk.types.Type;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildActions;
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildChangeAssetNameUpdateAction;
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildCustomUpdateActions;
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildSetAssetDescriptionUpdateAction;
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildSetAssetSourcesUpdateAction;
+import static com.commercetools.sync.categories.utils.CategoryAssetUpdateActionUtils.buildSetAssetTagsUpdateAction;
+import static io.sphere.sdk.models.LocalizedString.empty;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CategoryAssetUpdateActionUtilsTest {
+    private static final CategorySyncOptions SYNC_OPTIONS = CategorySyncOptionsBuilder
+        .of(mock(SphereClient.class)).build();
+
+    @Test
+    public void buildActions_WithDifferentValues_ShouldBuildUpdateAction() {
+        final LocalizedString oldName = LocalizedString.of(Locale.GERMAN, "oldName");
+        final LocalizedString newName = LocalizedString.of(Locale.GERMAN, "newName");
+
+        final Map<String, JsonNode> oldCustomFieldsMap = new HashMap<>();
+        oldCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(true));
+        oldCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("de", "rot"));
+
+        final Map<String, JsonNode> newCustomFieldsMap = new HashMap<>();
+        newCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(false));
+        newCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("es", "rojo"));
+
+        final CustomFields oldCustomFields = mock(CustomFields.class);
+        when(oldCustomFields.getType()).thenReturn(Type.referenceOfId("1"));
+        when(oldCustomFields.getFieldsJsonMap()).thenReturn(oldCustomFieldsMap);
+
+        final CustomFieldsDraft newCustomFieldsDraft =
+            CustomFieldsDraft.ofTypeIdAndJson("1", newCustomFieldsMap);
+
+        final Set<String> oldTags = new HashSet<>();
+        oldTags.add("oldTag");
+        final Set<String> newTags = new HashSet<>();
+        oldTags.add("newTag");
+
+        final List<AssetSource> oldAssetSources = singletonList(AssetSourceBuilder.ofUri("oldUri").build());
+        final List<AssetSource> newAssetSources = singletonList(AssetSourceBuilder.ofUri("newUri").build());
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getName()).thenReturn(oldName);
+        when(oldAsset.getSources()).thenReturn(oldAssetSources);
+        when(oldAsset.getTags()).thenReturn(oldTags);
+        when(oldAsset.getCustom()).thenReturn(oldCustomFields);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(newAssetSources, newName)
+                                                          .tags(newTags)
+                                                          .custom(newCustomFieldsDraft)
+                                                          .build();
+
+
+        final List<UpdateAction<Category>> updateActions = buildActions(oldAsset, newAssetDraft, SYNC_OPTIONS);
+
+        assertThat(updateActions).hasSize(5);
+        assertThat(updateActions).containsExactlyInAnyOrder(
+            ChangeAssetName.ofKey(null, newName),
+            SetAssetTags.ofKey(null, newTags),
+            SetAssetSources.ofKey(null, newAssetSources),
+            SetAssetCustomField
+                .ofJsonValueWithKey(null, "invisibleInShop", newCustomFieldsMap.get("invisibleInShop")),
+            SetAssetCustomField
+                .ofJsonValueWithKey(null, "backgroundColor", newCustomFieldsMap.get("backgroundColor"))
+        );
+    }
+
+    @Test
+    public void buildActions_WithIdenticalValues_ShouldBuildUpdateAction() {
+        final LocalizedString oldName = LocalizedString.of(Locale.GERMAN, "oldName");
+
+        final Map<String, JsonNode> oldCustomFieldsMap = new HashMap<>();
+        oldCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(true));
+        oldCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("de", "rot"));
+
+        final CustomFields oldCustomFields = mock(CustomFields.class);
+        when(oldCustomFields.getType()).thenReturn(Type.referenceOfId("1"));
+        when(oldCustomFields.getFieldsJsonMap()).thenReturn(oldCustomFieldsMap);
+
+        final Set<String> oldTags = new HashSet<>();
+        oldTags.add("oldTag");
+
+        final List<AssetSource> oldAssetSources = singletonList(AssetSourceBuilder.ofUri("oldUri").build());
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getName()).thenReturn(oldName);
+        when(oldAsset.getSources()).thenReturn(oldAssetSources);
+        when(oldAsset.getTags()).thenReturn(oldTags);
+        when(oldAsset.getCustom()).thenReturn(oldCustomFields);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(oldAsset).build();
+
+
+        final List<UpdateAction<Category>> updateActions = buildActions(oldAsset, newAssetDraft, SYNC_OPTIONS);
+
+        assertThat(updateActions).isEmpty();
+    }
+
+    @Test
+    public void buildChangeAssetNameUpdateAction_WithDifferentValues_ShouldBuildUpdateAction() {
+        final LocalizedString oldName = LocalizedString.of(Locale.GERMAN, "oldName");
+        final LocalizedString newName = LocalizedString.of(Locale.GERMAN, "newName");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getName()).thenReturn(oldName);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), newName).build();
+
+        final UpdateAction<Category> changeNameUpdateAction = buildChangeAssetNameUpdateAction(oldAsset, newAssetDraft)
+            .orElse(null);
+
+        assertThat(changeNameUpdateAction).isNotNull();
+        assertThat(changeNameUpdateAction).isInstanceOf(ChangeAssetName.class);
+        assertThat(((ChangeAssetName) changeNameUpdateAction).getName()).isEqualTo(newName);
+    }
+
+    @Test
+    public void buildChangeAssetNameUpdateAction_WithSameValues_ShouldNotBuildUpdateAction() {
+        final LocalizedString oldName = LocalizedString.of(Locale.GERMAN, "oldName");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getName()).thenReturn(oldName);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), oldName).build();
+
+        final Optional<UpdateAction<Category>> changeNameUpdateAction =
+            buildChangeAssetNameUpdateAction(oldAsset, newAssetDraft);
+
+        assertThat(changeNameUpdateAction).isEmpty();
+    }
+
+    @Test
+    public void buildSetAssetDescriptionUpdateAction_WithDifferentValues_ShouldBuildUpdateAction() {
+        final LocalizedString oldDesc = LocalizedString.of(Locale.GERMAN, "oldDesc");
+        final LocalizedString newDesc = LocalizedString.of(Locale.GERMAN, "newDesc");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getDescription()).thenReturn(oldDesc);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .description(newDesc).build();
+
+        final UpdateAction<Category> setAssetDescription =
+            buildSetAssetDescriptionUpdateAction(oldAsset, newAssetDraft).orElse(null);
+
+        assertThat(setAssetDescription).isNotNull();
+        assertThat(setAssetDescription).isInstanceOf(SetAssetDescription.class);
+        assertThat(((SetAssetDescription) setAssetDescription).getDescription()).isEqualTo(newDesc);
+    }
+
+    @Test
+    public void buildSetAssetDescriptionUpdateAction_WithSameValues_ShouldNotBuildUpdateAction() {
+        final LocalizedString oldDesc = LocalizedString.of(Locale.GERMAN, "oldDesc");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getDescription()).thenReturn(oldDesc);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .description(oldDesc).build();
+
+        final Optional<UpdateAction<Category>> setAssetDescription =
+            buildSetAssetDescriptionUpdateAction(oldAsset, newAssetDraft);
+
+        assertThat(setAssetDescription).isEmpty();
+    }
+
+    @Test
+    public void buildSetAssetTagsUpdateAction_WithDifferentValues_ShouldBuildUpdateAction() {
+        final Set<String> oldTags = new HashSet<>();
+        oldTags.add("oldTag");
+        final Set<String> newTags = new HashSet<>();
+        oldTags.add("newTag");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getTags()).thenReturn(oldTags);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .tags(newTags).build();
+
+        final UpdateAction<Category> productUpdateAction =
+            buildSetAssetTagsUpdateAction(oldAsset, newAssetDraft).orElse(null);
+
+        assertThat(productUpdateAction).isNotNull();
+        assertThat(productUpdateAction).isInstanceOf(SetAssetTags.class);
+        assertThat(((SetAssetTags) productUpdateAction).getTags()).isEqualTo(newTags);
+    }
+
+    @Test
+    public void buildSetAssetTagsUpdateAction_WithSameValues_ShouldNotBuildUpdateAction() {
+        final Set<String> oldTags = new HashSet<>();
+        oldTags.add("oldTag");
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getTags()).thenReturn(oldTags);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .tags(oldTags).build();
+
+
+        final Optional<UpdateAction<Category>> productUpdateAction =
+            buildSetAssetTagsUpdateAction(oldAsset, newAssetDraft);
+
+        assertThat(productUpdateAction).isEmpty();
+    }
+
+    @Test
+    public void buildSetAssetSourcesUpdateAction_WithDifferentValues_ShouldBuildUpdateAction() {
+        final List<AssetSource> oldAssetSources = singletonList(AssetSourceBuilder.ofUri("oldUri").build());
+        final List<AssetSource> newAssetSources = singletonList(AssetSourceBuilder.ofUri("newUri").build());
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getSources()).thenReturn(oldAssetSources);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .sources(newAssetSources).build();
+
+        final UpdateAction<Category> productUpdateAction =
+            buildSetAssetSourcesUpdateAction(oldAsset, newAssetDraft).orElse(null);
+
+        assertThat(productUpdateAction).isNotNull();
+        assertThat(productUpdateAction).isInstanceOf(SetAssetSources.class);
+        assertThat(((SetAssetSources) productUpdateAction).getSources()).isEqualTo(newAssetSources);
+    }
+
+    @Test
+    public void buildSetAssetSourcesUpdateAction_WithSameValues_ShouldNotBuildUpdateAction() {
+        final List<AssetSource> oldAssetSources = singletonList(AssetSourceBuilder.ofUri("oldUri").build());
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getSources()).thenReturn(oldAssetSources);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .sources(oldAssetSources).build();
+
+        final Optional<UpdateAction<Category>> productUpdateAction =
+            buildSetAssetSourcesUpdateAction(oldAsset, newAssetDraft);
+
+        assertThat(productUpdateAction).isEmpty();
+    }
+
+    @Test
+    public void buildCustomUpdateActions_WithSameValues_ShouldNotBuildUpdateAction() {
+        final Map<String, JsonNode> oldCustomFieldsMap = new HashMap<>();
+        oldCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(true));
+        oldCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("de", "rot"));
+
+
+        final CustomFields oldCustomFields = mock(CustomFields.class);
+        when(oldCustomFields.getType()).thenReturn(Type.referenceOfId("1"));
+        when(oldCustomFields.getFieldsJsonMap()).thenReturn(oldCustomFieldsMap);
+
+        final CustomFieldsDraft newCustomFieldsDraft =
+            CustomFieldsDraft.ofTypeIdAndJson("1", oldCustomFieldsMap);
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getCustom()).thenReturn(oldCustomFields);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .custom(newCustomFieldsDraft)
+                                                          .build();
+
+        final CategorySyncOptions syncOptions = CategorySyncOptionsBuilder.of(mock(SphereClient.class)).build();
+
+
+        final List<UpdateAction<Category>> updateActions =
+            buildCustomUpdateActions(oldAsset, newAssetDraft, syncOptions);
+
+        assertThat(updateActions).isEmpty();
+    }
+
+    @Test
+    public void buildCustomUpdateActions_WithDifferentValues_ShouldBuildUpdateAction() {
+        final Map<String, JsonNode> oldCustomFieldsMap = new HashMap<>();
+        oldCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(true));
+        oldCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("de", "rot"));
+
+        final Map<String, JsonNode> newCustomFieldsMap = new HashMap<>();
+        newCustomFieldsMap.put("invisibleInShop", JsonNodeFactory.instance.booleanNode(false));
+        newCustomFieldsMap.put("backgroundColor", JsonNodeFactory.instance.objectNode().put("es", "rojo"));
+
+
+        final CustomFields oldCustomFields = mock(CustomFields.class);
+        when(oldCustomFields.getType()).thenReturn(Type.referenceOfId("1"));
+        when(oldCustomFields.getFieldsJsonMap()).thenReturn(oldCustomFieldsMap);
+
+        final CustomFieldsDraft newCustomFieldsDraft =
+            CustomFieldsDraft.ofTypeIdAndJson("1", newCustomFieldsMap);
+
+        final Asset oldAsset = mock(Asset.class);
+        when(oldAsset.getCustom()).thenReturn(oldCustomFields);
+
+        final AssetDraft newAssetDraft = AssetDraftBuilder.of(emptyList(), empty())
+                                                          .custom(newCustomFieldsDraft)
+                                                          .build();
+
+        final CategorySyncOptions syncOptions = CategorySyncOptionsBuilder.of(mock(SphereClient.class)).build();
+
+
+        final List<UpdateAction<Category>> updateActions =
+            buildCustomUpdateActions(oldAsset, newAssetDraft, syncOptions);
+
+        assertThat(updateActions).hasSize(2);
+    }
+}
