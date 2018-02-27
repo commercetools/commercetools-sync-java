76a48e6963b48b28b3e725dfc33d7117889c4b9e
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-27 09:55:36 +0100

    #3: Remove unneeded test.

--- src/test/java/com/commercetools/sync/products/utils/BuildVariantAssetsUpdateActionsTest.java
+++ src/test/java/com/commercetools/sync/products/utils/BuildVariantAssetsUpdateActionsTest.java
@@ -4,8 +4,6 @@ import com.commercetools.sync.products.ProductSyncOptions;
 import com.commercetools.sync.products.ProductSyncOptionsBuilder;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.commands.UpdateAction;
-import io.sphere.sdk.models.Asset;
-import io.sphere.sdk.models.AssetDraft;
 import io.sphere.sdk.products.Product;
 import io.sphere.sdk.products.ProductDraft;
 import io.sphere.sdk.products.ProductVariant;
@@ -16,10 +14,8 @@ import io.sphere.sdk.products.commands.updateactions.RemoveAsset;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Optional;
 
-import static com.commercetools.sync.products.utils.ProductVariantUpdateActionUtils.buildChangeAssetOrderUpdateAction;
-import static com.commercetools.sync.products.utils.ProductVariantUpdateActionUtils.buildProductVariantAssetsUpdateActions;
+import static com.commercetools.sync.products.utils.ProductVariantAssetUpdateActionUtils.buildProductVariantAssetsUpdateActions;
 import static io.sphere.sdk.json.SphereJsonUtils.readObjectFromResource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -39,24 +35,6 @@ public class BuildVariantAssetsUpdateActionsTest {
     private static final String PRODUCT_DRAFT_WITH_ASSETS_CBD = RES_ROOT + "product-draft-with-assets-cbd.json";
     private static final ProductSyncOptions SYNC_OPTIONS = ProductSyncOptionsBuilder.of(mock(SphereClient.class))
                                                                                     .build();
-
-    @Test
-    public void buildChangeAssetOrderUpdateAction_WithDifferentOrder_ShouldBuildAction() {
-        final Product oldProduct = readObjectFromResource(PRODUCT_WITH_ASSETS_ABC, Product.class);
-        final ProductDraft newProductDraft = readObjectFromResource(PRODUCT_DRAFT_WITH_ASSETS_CAB, ProductDraft.class);
-
-
-        final List<Asset> oldAssets = oldProduct.getMasterData().getStaged().getMasterVariant().getAssets();
-        final List<AssetDraft> newAssetDrafts = newProductDraft.getMasterVariant().getAssets();
-
-        final Optional<UpdateAction<Product>> changeAssetOrderUpdateAction =
-            buildChangeAssetOrderUpdateAction(1, oldAssets, newAssetDrafts);
-
-        assertThat(changeAssetOrderUpdateAction).isNotEmpty();
-        assertThat(changeAssetOrderUpdateAction).containsInstanceOf(ChangeAssetOrder.class);
-        final ChangeAssetOrder updateAction = (ChangeAssetOrder) changeAssetOrderUpdateAction.get();
-        assertThat(updateAction.getAssetOrder()).containsExactly("3", "1", "2");
-    }
 
     @Test
     public void testCase1() {
