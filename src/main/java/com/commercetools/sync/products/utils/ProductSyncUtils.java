e5c9d35603738218fb4c1d2e4dae4c354ee45c61
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-27 10:14:47 +0100

    #3: Remove unneeded buildCoreActions.

--- src/main/java/com/commercetools/sync/products/utils/ProductSyncUtils.java
+++ src/main/java/com/commercetools/sync/products/utils/ProductSyncUtils.java
@@ -34,23 +34,21 @@ import static com.commercetools.sync.products.utils.ProductUpdateActionUtils.bui
 import static com.commercetools.sync.products.utils.ProductUpdateActionUtils.buildVariantsUpdateActions;
 
 public final class ProductSyncUtils {
-
     /**
-     * Compares the name, description, slug, search keywords, metaTitle, metaDescription, metaKeywords, categories,
-     * variants (comparing all variants see
-     * {@link ProductUpdateActionUtils#buildVariantsUpdateActions(Product, ProductDraft, ProductSyncOptions, Map)}),
-     * and publish state of a {@link Product} and a {@link ProductDraft}, given that each of these fields pass the
+     * Compares all the fields (including the variants see
+     * {@link ProductUpdateActionUtils#buildVariantsUpdateActions(Product, ProductDraft, ProductSyncOptions, Map)})
+     * of a {@link Product} and a {@link ProductDraft}, given that each of these fields pass the
      * specified {@link SyncFilter}. It returns a {@link List} of {@link UpdateAction}&lt;{@link Product}&gt; as a
      * result. If no update action is needed, for example in case where both the {@link Product} and the
      * {@link ProductDraft} have the same names, an empty {@link List} is returned. Then it applies a specified filter
      * function in the {@link ProductSyncOptions} instance on the resultant list and returns this result.
      *
-     * @param oldProduct the product which should be updated.
-     * @param newProduct the product draft where we get the new data.
-     * @param syncOptions the sync options wrapper which contains options related to the sync process supplied by
-     *                    the user. For example, custom callbacks to call in case of warnings or errors occurring
-     *                    on the build update action process. And other options (See {@link ProductSyncOptions}
-     *                    for more info.
+     * @param oldProduct         the product which should be updated.
+     * @param newProduct         the product draft where we get the new data.
+     * @param syncOptions        the sync options wrapper which contains options related to the sync process supplied by
+     *                           the user. For example, custom callbacks to call in case of warnings or errors occurring
+     *                           on the build update action process. And other options (See {@link ProductSyncOptions}
+     *                           for more info.
      * @param attributesMetaData a map of attribute name -&gt; {@link AttributeMetaData}; which defines attribute
      *                           information: its name, whether a value is required or not and whether it has the
      *                           constraint "SameForAll" or not.
@@ -61,68 +59,37 @@ public final class ProductSyncUtils {
                                                            @Nonnull final ProductDraft newProduct,
                                                            @Nonnull final ProductSyncOptions syncOptions,
                                                            @Nonnull final Map<String, AttributeMetaData>
-                                                                   attributesMetaData) {
-        final List<UpdateAction<Product>> updateActions =
-            buildCoreActions(oldProduct, newProduct, syncOptions, attributesMetaData);
-        return syncOptions.applyBeforeUpdateCallBack(updateActions, newProduct, oldProduct);
-    }
-
-    /**
-     * Compares the name, description, slug, search keywords, metaTitle, metaDescription, metaKeywords, categories,
-     * variants (comparing all variants see
-     * {@link ProductUpdateActionUtils#buildVariantsUpdateActions(Product, ProductDraft, ProductSyncOptions, Map)})
-     * and publish state of a {@link Product} and a {@link ProductDraft}, given that each of these fields pass the
-     * specified {@link SyncFilter}. It returns a {@link List} of {@link UpdateAction}&lt;{@link Product}&gt; as a
-     * result. If no update action is needed, for example in case where both the {@link Product} and the
-     * {@link ProductDraft} have the same names, an empty {@link List} is returned.
-     *
-     * @param oldProduct the product which should be updated.
-     * @param newProduct the product draft where we get the new data.
-     * @param syncOptions the sync options wrapper which contains options related to the sync process supplied by
-     *                    the user. For example, custom callbacks to call in case of warnings or errors occurring
-     *                    on the build update action process. And other options (See {@link ProductSyncOptions}
-     *                    for more info.
-     * @param attributesMetaData a map of attribute name -&gt; {@link AttributeMetaData}; which defines attribute
-     *                           information: its name, whether a value is required or not and whether it has the
-     *                           constraint "SameForAll" or not.
-     * @return A list of product-specific update actions.
-     */
-    @Nonnull
-    public static List<UpdateAction<Product>> buildCoreActions(@Nonnull final Product oldProduct,
-                                                               @Nonnull final ProductDraft newProduct,
-                                                               @Nonnull final ProductSyncOptions syncOptions,
-                                                               @Nonnull final Map<String, AttributeMetaData>
-                                                                       attributesMetaData) {
+                                                               attributesMetaData) {
         final SyncFilter syncFilter = syncOptions.getSyncFilter();
 
         final List<UpdateAction<Product>> updateActions = new ArrayList<>(buildUpdateActionsFromOptionals(Arrays.asList(
-                buildActionIfPassesFilter(syncFilter, ActionGroup.NAME, () ->
-                    buildChangeNameUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.NAME, () ->
+                buildChangeNameUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.DESCRIPTION, () ->
-                    buildSetDescriptionUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.DESCRIPTION, () ->
+                buildSetDescriptionUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.SLUG, () ->
-                     buildChangeSlugUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.SLUG, () ->
+                buildChangeSlugUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.SEARCHKEYWORDS, () ->
-                    buildSetSearchKeywordsUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.SEARCHKEYWORDS, () ->
+                buildSetSearchKeywordsUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.METATITLE, () ->
-                    buildSetMetaTitleUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.METATITLE, () ->
+                buildSetMetaTitleUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.METADESCRIPTION, () ->
-                    buildSetMetaDescriptionUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.METADESCRIPTION, () ->
+                buildSetMetaDescriptionUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.METAKEYWORDS, () ->
-                    buildSetMetaKeywordsUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.METAKEYWORDS, () ->
+                buildSetMetaKeywordsUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.TAXCATEGORY, () ->
-                    buildSetTaxCategoryUpdateAction(oldProduct, newProduct)),
+            buildActionIfPassesFilter(syncFilter, ActionGroup.TAXCATEGORY, () ->
+                buildSetTaxCategoryUpdateAction(oldProduct, newProduct)),
 
-                buildActionIfPassesFilter(syncFilter, ActionGroup.STATE, () ->
-                    buildTransitionStateUpdateAction(oldProduct, newProduct))
-            )));
+            buildActionIfPassesFilter(syncFilter, ActionGroup.STATE, () ->
+                buildTransitionStateUpdateAction(oldProduct, newProduct))
+        )));
 
         final List<UpdateAction<Product>> productCategoryUpdateActions =
             buildActionsIfPassesFilter(syncFilter, ActionGroup.CATEGORIES, () ->
@@ -133,7 +100,8 @@ public final class ProductSyncUtils {
 
         // lastly publish/unpublish product
         buildPublishUpdateAction(oldProduct, newProduct).ifPresent(updateActions::add);
-        return updateActions;
+
+        return syncOptions.applyBeforeUpdateCallBack(updateActions, newProduct, oldProduct);
     }
 
     /**
