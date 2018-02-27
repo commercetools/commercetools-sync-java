6f26375618ad796305f40f0594a603bfd2d15454
Hesham Massoud <hesham.massoud@commercetools.de>
2018-02-27 11:31:01 +0100

    #3: Implement category asset update actions utils.

--- src/main/java/com/commercetools/sync/categories/utils/CategoryAssetUpdateActionUtils.java
+++ src/main/java/com/commercetools/sync/categories/utils/CategoryAssetUpdateActionUtils.java
@@ -15,12 +15,50 @@ import io.sphere.sdk.models.AssetDraft;
 import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.commercetools.sync.commons.utils.CommonTypeUpdateActionUtils.buildUpdateAction;
+import static java.util.Arrays.asList;
 
 //TODO: CONSIDER CHANGE NAME.
 // TODO: Add tests.
 public final class CategoryAssetUpdateActionUtils {
+
+    @Nonnull
+    public static List<UpdateAction<Category>> buildActions(@Nonnull final Asset oldAsset,
+                                                            @Nonnull final AssetDraft newAsset,
+                                                            @Nonnull final CategorySyncOptions syncOptions) {
+
+        final List<UpdateAction<Category>> updateActions = buildUpdateActionsFromOptionals(asList(
+            buildChangeAssetNameUpdateAction(oldAsset, newAsset),
+            buildSetAssetDescriptionUpdateAction(oldAsset, newAsset),
+            buildSetAssetTagsUpdateAction(oldAsset, newAsset),
+            buildSetAssetSourcesUpdateAction(oldAsset, newAsset)
+        ));
+
+        updateActions.addAll(buildCustomUpdateActions(oldAsset, newAsset, syncOptions));
+        return updateActions;
+    }
+
+    /**
+     * TODO: THIS WILL BE REMOVED AS SOON AS GITHUB ISSUE#255 is resolved.
+     * Given a list of category {@link UpdateAction} elements, where each is wrapped in an {@link Optional}; this method
+     * filters out the optionals which are only present and returns a new list of category {@link UpdateAction}
+     * elements.
+     *
+     * @param optionalUpdateActions list of category {@link UpdateAction} elements,
+     *                              where each is wrapped in an {@link Optional}.
+     * @return a List of category update actions from the optionals that were present in the
+     * {@code optionalUpdateActions} list parameter.
+     */
+    @Nonnull
+    private static List<UpdateAction<Category>> buildUpdateActionsFromOptionals(
+        @Nonnull final List<Optional<UpdateAction<Category>>> optionalUpdateActions) {
+        return optionalUpdateActions.stream()
+                                    .filter(Optional::isPresent)
+                                    .map(Optional::get)
+                                    .collect(Collectors.toList());
+    }
 
     @Nonnull
     public static Optional<UpdateAction<Category>> buildChangeAssetNameUpdateAction(
@@ -62,5 +100,8 @@ public final class CategoryAssetUpdateActionUtils {
         return CustomUpdateActionUtils.buildCustomUpdateActions(oldAsset, newAsset,
             new AssetCustomActionBuilder(), -1, Asset::getId, asset -> Asset.resourceTypeId(), Asset::getKey,
             syncOptions);
+    }
+
+    private CategoryAssetUpdateActionUtils() {
     }
 }
