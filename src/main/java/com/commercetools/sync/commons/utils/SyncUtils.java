ec5d3e8666776525ff36f86870e8a572c3c396b5
Hesham Massoud <hesham.massoud@commercetools.de>
2018-03-01 17:48:49 +0100

    #3: Implement SyncUtils#replaceAssetsReferenceIdsWithKeys.

--- src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncUtils.java
@@ -1,5 +1,8 @@
 package com.commercetools.sync.commons.utils;
 
+import io.sphere.sdk.models.Asset;
+import io.sphere.sdk.models.AssetDraft;
+import io.sphere.sdk.models.AssetDraftBuilder;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.types.Custom;
 import io.sphere.sdk.types.CustomFields;
@@ -11,6 +14,8 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
+
+import static java.util.stream.Collectors.toList;
 
 public final class SyncUtils {
 
@@ -80,6 +85,25 @@ public final class SyncUtils {
             return keyInReferenceSupplier.get();
         }
         return reference;
+    }
+
+    /**
+     * Takes a an asset list that is supposed to have all its assets' custom references expanded in order to be able to
+     * fetch the keys and replace the reference ids with the corresponding keys for the custom references. This method
+     * returns as a result a {@link List} of {@link AssetDraft} that has all custom references with keys replacing the
+     * ids.
+     *
+     * <p>Any custom reference that is not expanded will have it's id in place and not replaced by the key.
+     *
+     * @param assets the list of assets to replace their custom ids with keys.
+     * @return a {@link List} of {@link AssetDraft} that has all channel references with keys replacing the ids.
+     */
+    @Nonnull
+    public static List<AssetDraft> replaceAssetsReferencesIdsWithKeys(@Nonnull final List<Asset> assets) {
+        return assets.stream().map(asset ->
+            AssetDraftBuilder.of(asset)
+                             .custom(replaceCustomTypeIdWithKeys(asset)).build())
+                     .collect(toList());
     }
 
     private SyncUtils() {
