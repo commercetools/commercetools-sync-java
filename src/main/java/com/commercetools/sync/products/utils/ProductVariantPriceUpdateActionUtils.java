5fe99cfc8ea98921bf4ef0693fe64929e022626f
Hesham Massoud <hesham.massoud@commercetools.de>
2018-07-23 14:39:01 +0200

    #101: Add Javadoc and non null annotations.

--- src/main/java/com/commercetools/sync/products/utils/ProductVariantPriceUpdateActionUtils.java
+++ src/main/java/com/commercetools/sync/products/utils/ProductVariantPriceUpdateActionUtils.java
@@ -19,13 +19,18 @@ import static com.commercetools.sync.commons.utils.CommonTypeUpdateActionUtils.b
 public final class ProductVariantPriceUpdateActionUtils {
 
     /**
-     * TODO.
-     * @param variantId
-     * @param oldPrice
-     * @param newPrice
-     * @param syncOptions
-     * @return
+     * Compares all the fields of a {@link Price} and a {@link PriceDraft} and returns a list of
+     * {@link UpdateAction}&lt;{@link Product}&gt; as a result. If both the {@link Price} and the {@link PriceDraft}
+     * have identical fields, then no update action is needed and hence an empty {@link List} is returned.
+     *
+     * @param variantId   the variantId needed for building the update action.
+     * @param oldPrice    the price which should be updated.
+     * @param newPrice    the price draft where we get the new fields.
+     * @param syncOptions responsible for supplying the sync options to the sync utility method. It is used for
+     *                    triggering the error callback within the utility, in case of errors.
+     * @return A list with the update actions or an empty list if the price fields are identical.
      */
+    @Nonnull
     public static List<UpdateAction<Product>> buildActions(
         @Nonnull final Integer variantId,
         @Nonnull final Price oldPrice,
@@ -42,11 +47,22 @@ public final class ProductVariantPriceUpdateActionUtils {
 
 
     /**
-     * TODO
-     * @param oldPrice
-     * @param newPrice
-     * @return
+     * Builds a {@link ChangePrice} action based on the comparison of the following fields of the supplied {@link Price}
+     * and {@link PriceDraft}:
+     * <ul>
+     *     <li>{@link Price#getValue()} and {@link PriceDraft#getValue()}</li>
+     *     <li>{@link Price#getTiers()} and {@link PriceDraft#getTiers()}</li>
+     * </ul>
+     *
+     * If any of the aforementioned fields are different a {@link ChangePrice} update action will be returned in an
+     * {@link Optional}, otherwise if both are identical in the {@link Price} and the {@link PriceDraft}, then no update
+     * action is needed and hence an empty {@link Optional} is returned.
+     *
+     * @param oldPrice  the price which should be updated.
+     * @param newPrice  the price draft where we get the new name.
+     * @return A filled optional with the update action or an empty optional if the names are identical.
      */
+    @Nonnull
     public static Optional<ChangePrice> buildChangePriceUpdateAction(
         @Nonnull final Price oldPrice,
         @Nonnull final PriceDraft newPrice) {
