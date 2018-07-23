dd51e08095d6311526a334c0fbc8004b333c1457
Hesham Massoud <hesham.massoud@commercetools.de>
2018-07-23 14:42:59 +0200

    #101: Add support for price update actions in ProductSyncUtilsTest.

--- src/test/java/com/commercetools/sync/products/utils/ProductSyncUtilsTest.java
+++ src/test/java/com/commercetools/sync/products/utils/ProductSyncUtilsTest.java
@@ -10,10 +10,10 @@ import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.models.AssetDraft;
 import io.sphere.sdk.models.AssetDraftBuilder;
 import io.sphere.sdk.models.AssetSourceBuilder;
-import io.sphere.sdk.models.DefaultCurrencyUnits;
 import io.sphere.sdk.models.LocalizedString;
 import io.sphere.sdk.products.Image;
 import io.sphere.sdk.products.ImageDimensions;
+import io.sphere.sdk.products.Price;
 import io.sphere.sdk.products.PriceDraft;
 import io.sphere.sdk.products.Product;
 import io.sphere.sdk.products.ProductDraft;
@@ -22,17 +22,18 @@ import io.sphere.sdk.products.ProductVariantDraft;
 import io.sphere.sdk.products.ProductVariantDraftBuilder;
 import io.sphere.sdk.products.commands.updateactions.AddAsset;
 import io.sphere.sdk.products.commands.updateactions.AddExternalImage;
+import io.sphere.sdk.products.commands.updateactions.AddPrice;
 import io.sphere.sdk.products.commands.updateactions.AddToCategory;
 import io.sphere.sdk.products.commands.updateactions.ChangeName;
 import io.sphere.sdk.products.commands.updateactions.ChangeSlug;
 import io.sphere.sdk.products.commands.updateactions.RemoveFromCategory;
 import io.sphere.sdk.products.commands.updateactions.RemoveImage;
+import io.sphere.sdk.products.commands.updateactions.RemovePrice;
 import io.sphere.sdk.products.commands.updateactions.SetCategoryOrderHint;
 import io.sphere.sdk.products.commands.updateactions.SetDescription;
 import io.sphere.sdk.products.commands.updateactions.SetMetaDescription;
 import io.sphere.sdk.products.commands.updateactions.SetMetaKeywords;
 import io.sphere.sdk.products.commands.updateactions.SetMetaTitle;
-import io.sphere.sdk.products.commands.updateactions.SetPrices;
 import io.sphere.sdk.products.commands.updateactions.SetSearchKeywords;
 import io.sphere.sdk.products.commands.updateactions.SetSku;
 import io.sphere.sdk.producttypes.ProductType;
@@ -42,19 +43,19 @@ import io.sphere.sdk.utils.MoneyImpl;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
 import static com.commercetools.sync.products.ProductSyncMockUtils.PRODUCT_KEY_1_CHANGED_WITH_PRICES_RESOURCE_PATH;
-import static com.commercetools.sync.products.ProductSyncMockUtils.PRODUCT_KEY_1_RESOURCE_PATH;
+import static com.commercetools.sync.products.ProductSyncMockUtils.PRODUCT_KEY_1_WITH_PRICES_RESOURCE_PATH;
 import static com.commercetools.sync.products.ProductSyncMockUtils.createProductDraftBuilder;
 import static io.sphere.sdk.json.SphereJsonUtils.readObjectFromResource;
+import static io.sphere.sdk.models.DefaultCurrencyUnits.EUR;
 import static io.sphere.sdk.models.LocalizedString.ofEnglish;
+import static io.sphere.sdk.utils.MoneyImpl.createCurrencyByCode;
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -72,27 +73,22 @@ public class ProductSyncUtilsTest {
         productSyncOptions = ProductSyncOptionsBuilder.of(mock(SphereClient.class))
                                                       .build();
 
-        oldProduct = readObjectFromResource(PRODUCT_KEY_1_RESOURCE_PATH, Product.class);
+        oldProduct = readObjectFromResource(PRODUCT_KEY_1_WITH_PRICES_RESOURCE_PATH, Product.class);
     }
 
     @Test
     public void buildActions_FromDraftsWithDifferentNameValues_ShouldBuildUpdateActions() {
         final LocalizedString newName = LocalizedString.of(Locale.ENGLISH, "newName");
-
         final ProductDraft newProductDraft =
-            createProductDraftBuilder(PRODUCT_KEY_1_RESOURCE_PATH, ProductType.referenceOfId("anyProductType"))
+            createProductDraftBuilder(PRODUCT_KEY_1_WITH_PRICES_RESOURCE_PATH, ProductType.referenceOfId("anyProductType"))
                 .name(newName)
                 .build();
 
         final List<UpdateAction<Product>> updateActions =
             ProductSyncUtils.buildActions(oldProduct, newProductDraft, productSyncOptions, new HashMap<>());
-        assertThat(updateActions).isNotNull();
 
-        //TODO: Should contain only changeName but no setPrices. See: GITHUB ISSUE: #101
-        assertThat(updateActions).containsExactly(
-            ChangeName.of(newName, true),
-            SetPrices.of(1, emptyList())
-        );
+
+        assertThat(updateActions).containsExactly(ChangeName.of(newName, true));
     }
 
     @Test
@@ -124,8 +120,8 @@ public class ProductSyncUtilsTest {
 
         final List<UpdateAction<Product>> updateActions =
             ProductSyncUtils.buildActions(oldProduct, newProductDraft, productSyncOptions, new HashMap<>());
-        assertThat(updateActions).isNotNull();
-        assertThat(updateActions).containsExactly(
+
+        assertThat(updateActions).containsExactlyInAnyOrder(
             ChangeName.of(ofEnglish("new name"), true),
             SetDescription.of(ofEnglish("new description"), true),
             ChangeSlug.of(ofEnglish("rehruecken-o-k1"), true),
@@ -148,13 +144,76 @@ public class ProductSyncUtilsTest {
             AddExternalImage.ofVariantId(1,
                 Image.of("https://53346cfbf3c7e017ed3d-6de74c3efa80f1c837c6a988b57abe66.ssl.cf3.rackcdn.com/"
                     + "new-image-2.png", ImageDimensions.of(0, 0), null), true),
-            SetPrices.of(1,
-                asList(PriceDraft.of(BigDecimal.valueOf(1.18), DefaultCurrencyUnits.EUR)
-                                 .withChannel(Channel.referenceOfId("channel-key_1")),
-                    PriceDraft.of(BigDecimal.valueOf(1), MoneyImpl.createCurrencyByCode("EGP"))
-                              .withChannel(Channel.referenceOfId("channel-key_1")))),
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(108, EUR)).withId("4dfc8bea-84f2-45bc-b3c2-cdc94bf96f1f"),
+                true),
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(10, createCurrencyByCode("EGP"))).withId("4dfc8bea-84f2-45bc-b3c2-r9e7wv99vfb"),
+                true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(118, EUR))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(100, createCurrencyByCode("EGP")))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true),
             AddAsset.ofVariantId(1, assetDraft).withPosition(0).withStaged(true),
             SetSku.of(1, "3065831", true)
+        );
+    }
+
+    @Test
+    public void buildActions_FromDraftsWithMultipleDifferentValues_ShouldBuildUpdateActionsInCorrectOrder() {
+        final ProductDraftBuilder draftBuilder = createProductDraftBuilder(
+            PRODUCT_KEY_1_CHANGED_WITH_PRICES_RESOURCE_PATH, ProductType.referenceOfId("anyProductType"));
+
+        final ProductDraft newProductDraft = ProductDraftBuilder.of(draftBuilder.build())
+                                                                .build();
+
+        final List<UpdateAction<Product>> updateActions =
+            ProductSyncUtils.buildActions(oldProduct, newProductDraft, productSyncOptions, new HashMap<>());
+
+        //Assert that "removeImage" actions are always before "addExternalImage"
+        assertThat(updateActions).containsSubsequence(
+            RemoveImage.ofVariantId(1,
+                Image.of("https://53346cfbf3c7e017ed3d-6de74c3efa80f1c837c6a988b57abe66.ssl.cf3.rackcdn.com/"
+                    + "old-image.png", ImageDimensions.of(0, 0), null), true),
+            AddExternalImage.ofVariantId(1,
+                Image.of("https://53346cfbf3c7e017ed3d-6de74c3efa80f1c837c6a988b57abe66.ssl.cf3.rackcdn.com/"
+                    + "new-image.png", ImageDimensions.of(0, 0), null), true),
+            AddExternalImage.ofVariantId(1,
+                Image.of("https://53346cfbf3c7e017ed3d-6de74c3efa80f1c837c6a988b57abe66.ssl.cf3.rackcdn.com/"
+                    + "new-image-2.png", ImageDimensions.of(0, 0), null), true)
+        );
+
+        //Assert that "removePrice" actions are always before "addPrice"
+        assertThat(updateActions).containsSubsequence(
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(108, EUR)).withId("4dfc8bea-84f2-45bc-b3c2-cdc94bf96f1f"),
+                true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(100, createCurrencyByCode("EGP")))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true)
+        );
+
+        assertThat(updateActions).containsSubsequence(
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(108, EUR)).withId("4dfc8bea-84f2-45bc-b3c2-cdc94bf96f1f"),
+                true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(118, EUR))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true)
+        );
+
+        assertThat(updateActions).containsSubsequence(
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(10, createCurrencyByCode("EGP"))).withId("4dfc8bea-84f2-45bc-b3c2-r9e7wv99vfb"),
+                true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(100, createCurrencyByCode("EGP")))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true)
+        );
+
+        assertThat(updateActions).containsSubsequence(
+            RemovePrice.of(Price.of(MoneyImpl.ofCents(10, createCurrencyByCode("EGP"))).withId("4dfc8bea-84f2-45bc-b3c2-r9e7wv99vfb"),
+                true),
+            AddPrice.ofVariantId(1,
+                PriceDraft.of(MoneyImpl.ofCents(118, EUR))
+                          .withChannel(Channel.referenceOfId("channel-key_1")), true)
         );
     }
 
@@ -202,16 +261,12 @@ public class ProductSyncUtilsTest {
     @Test
     public void buildActions_FromDraftsWithSameNameValues_ShouldNotBuildUpdateActions() {
         final ProductDraft newProductDraft =
-            createProductDraftBuilder(PRODUCT_KEY_1_RESOURCE_PATH, ProductType.referenceOfId("anyProductType"))
+            createProductDraftBuilder(PRODUCT_KEY_1_WITH_PRICES_RESOURCE_PATH, ProductType.referenceOfId("anyProductType"))
                 .build();
 
         final List<UpdateAction<Product>> updateActions =
             ProductSyncUtils.buildActions(oldProduct, newProductDraft, productSyncOptions, new HashMap<>());
-        assertThat(updateActions).isNotNull();
 
-        //TODO: Should be empty but because of SetPrices GITHUB ISSUE: #101
-        assertThat(updateActions).containsExactly(
-            SetPrices.of(1, emptyList())
-        );
+        assertThat(updateActions).isEmpty();
     }
 }
