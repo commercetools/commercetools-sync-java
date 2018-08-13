e93cbf8102c6a2375f8ce8f775afbf9c591f388b
Javier Ortiz <javier.ortizsaorin@gmail.com>
2018-08-13 16:33:58 +0200

    #286 Update test case to avoid variables mismatch

--- src/test/java/com/commercetools/sync/producttypes/helpers/ProductTypeSyncStatisticsTest.java
+++ src/test/java/com/commercetools/sync/producttypes/helpers/ProductTypeSyncStatisticsTest.java
@@ -16,12 +16,12 @@ public class ProductTypeSyncStatisticsTest {
     @Test
     public void getReportMessage_WithIncrementedStats_ShouldGetCorrectMessage() {
         productTypeSyncStatistics.incrementCreated(1);
-        productTypeSyncStatistics.incrementFailed(1);
-        productTypeSyncStatistics.incrementUpdated(1);
-        productTypeSyncStatistics.incrementProcessed(3);
+        productTypeSyncStatistics.incrementFailed(2);
+        productTypeSyncStatistics.incrementUpdated(3);
+        productTypeSyncStatistics.incrementProcessed(6);
 
         assertThat(productTypeSyncStatistics.getReportMessage())
-            .isEqualTo("Summary: 3 product types were processed in total "
-                + "(1 created, 1 updated and 1 failed to sync).");
+            .isEqualTo("Summary: 6 product types were processed in total "
+                + "(1 created, 3 updated and 2 failed to sync).");
     }
 }
