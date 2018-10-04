0c26148a47b0e0f4e78a9ffe9c0867e280edb67d
Hesham Massoud <hesham.massoud@commercetools.de>
2018-10-04 22:26:05 +0200

    #255: Add varargs method varaiation.

--- src/main/java/com/commercetools/sync/commons/utils/OptionalUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/OptionalUtils.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
+
 public final class OptionalUtils {
 
     /**
@@ -22,6 +24,20 @@ public final class OptionalUtils {
     @Nonnull
     public static <T> List<T> filterEmptyOptionals(@Nonnull final Collection<Optional<T>> optionals) {
         return filterEmptyOptionals(optionals, ArrayList::new);
+    }
+
+    /**
+     *  Takes zero or more {@link Optional}s containing some elements of type {@link T}, this method filters
+     *  out any empty {@link Optional} and returns a new list containing the values of the non-empty optionals.
+     *
+     * @param optionals zero or more optionals that should be filtered out of empty optionals.
+     * @param <T> The type of the elements in the Optionals in the supplied collection {@code optionals}.
+     * @return a new list containing the values of the non-empty optionals from the supplied {@code optionals}.
+     */
+    @Nonnull
+    @SafeVarargs
+    public static <T> List<T> filterEmptyOptionals(@Nonnull final Optional<T>... optionals) {
+        return filterEmptyOptionals(asList(optionals));
     }
 
     /**
