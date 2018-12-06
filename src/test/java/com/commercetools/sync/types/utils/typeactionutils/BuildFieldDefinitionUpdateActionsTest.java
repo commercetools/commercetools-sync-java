658a005906b0bd55846c64e802b0af917d8d99a1
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-06 12:18:45 +0100

    #300: Add a unit test with a definition with a null name.

--- src/test/java/com/commercetools/sync/types/utils/typeactionutils/BuildFieldDefinitionUpdateActionsTest.java
+++ src/test/java/com/commercetools/sync/types/utils/typeactionutils/BuildFieldDefinitionUpdateActionsTest.java
@@ -457,4 +457,43 @@ public class BuildFieldDefinitionUpdateActionsTest {
         assertThat(updateActions).containsExactly(
             ChangeFieldDefinitionLabel.of(newFieldDefinition.getName(), newFieldDefinition.getLabel()));
     }
+
+    @Test
+    public void buildUpdateActions_WithDefinitionWithNullName_ShouldBuildChangeFieldDefinitionOrderAction() {
+        // preparation
+        final Type oldType = mock(Type.class);
+        final FieldDefinition oldFieldDefinition = FieldDefinition.of(
+            LocalizedEnumFieldType.of(emptyList()),
+            "field_1",
+            LocalizedString.ofEnglish("label1"),
+            false,
+            TextInputHint.SINGLE_LINE);
+
+
+        when(oldType.getFieldDefinitions()).thenReturn(singletonList(oldFieldDefinition));
+
+        final FieldDefinition newFieldDefinition = FieldDefinition.of(
+            LocalizedEnumFieldType.of(emptyList()),
+            null,
+            LocalizedString.ofEnglish("label2"),
+            false,
+            TextInputHint.SINGLE_LINE);
+
+        final TypeDraft typeDraft = TypeDraftBuilder
+            .of("key", LocalizedString.ofEnglish("label"), emptySet())
+            .fieldDefinitions(asList(null, newFieldDefinition))
+            .build();
+
+        // test
+        final List<UpdateAction<Type>> updateActions = buildFieldDefinitionsUpdateActions(
+            oldType,
+            typeDraft,
+            SYNC_OPTIONS
+        );
+
+        // assertion
+        assertThat(updateActions).containsExactly(
+            RemoveFieldDefinition.of(oldFieldDefinition.getName()),
+            AddFieldDefinition.of(newFieldDefinition));
+    }
 }
