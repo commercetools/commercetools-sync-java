908505925828dabe26ea7876b9b929d07aefbeec
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-10 15:54:12 +0100

    Push data for new benchmarks.

--- benchmarks/assets/js/chart-renderer.js
+++ benchmarks/assets/js/chart-renderer.js
@@ -72,9 +72,53 @@ var inventorySyncCreatesUpdates = {
     label: 'Inventory Sync (creates and updates)',
     backgroundColor: color(window.chartColors.red).alpha(0.9).rgbString(),
     borderColor: window.chartColors.red,
+// skyBlue
+var productTypeSyncCreatesOnly = {
+    label: 'ProductType Sync (creates only)',
+    backgroundColor: color(window.chartColors.skyBlue).alpha(0.2).rgbString(),
+    borderColor: color(window.chartColors.skyBlue).alpha(0.2).rgbString(),
+    borderWidth: 1,
+    data: []
+}
+var productTypeSyncUpdatesOnly = {
+    label: 'ProductType Sync (updates only)',
+    backgroundColor: color(window.chartColors.skyBlue).alpha(0.4).rgbString(),
+    borderColor: color(window.chartColors.skyBlue).alpha(0.4).rgbString(),
+    borderWidth: 1,
+    data: []
+}
+var productTypeSyncCreatesUpdates = {
+    label: 'ProductType Sync (creates and updates)',
+    backgroundColor: color(window.chartColors.skyBlue).alpha(0.6).rgbString(),
+    borderColor: color(window.chartColors.skyBlue).alpha(0.6).rgbString(),
     borderWidth: 1,
     data: [],
 }
+
+// green
+var typeSyncCreatesOnly = {
+    label: 'Type Sync (creates only)',
+    backgroundColor: color(window.chartColors.green).alpha(0.2).rgbString(),
+    borderColor: color(window.chartColors.green).alpha(0.2).rgbString(),
+    borderWidth: 1,
+    data: []
+}
+var typeSyncUpdatesOnly = {
+    label: 'Type Sync (updates only)',
+    backgroundColor: color(window.chartColors.green).alpha(0.4).rgbString(),
+    borderColor: color(window.chartColors.green).alpha(0.4).rgbString(),
+    borderWidth: 1,
+    data: []
+}
+var typeSyncCreatesUpdates = {
+    label: 'Type Sync (creates and updates)',
+    backgroundColor: color(window.chartColors.green).alpha(0.6).rgbString(),
+    borderColor: color(window.chartColors.green).alpha(0.6).rgbString(),
+    borderWidth: 1,
+    data: [],
+}
+
+
 
 
 var barChartData = {
@@ -83,12 +127,22 @@ var barChartData = {
         productSyncCreatesOnly,
         productSyncUpdatesOnly,
         productSyncCreatesUpdates,
+
         categorySyncCreatesOnly,
         categorySyncUpdatesOnly,
         categorySyncCreatesUpdates,
+
         inventorySyncCreatesOnly,
         inventorySyncUpdatesOnly,
-        inventorySyncCreatesUpdates
+        inventorySyncCreatesUpdates,
+
+        productTypeSyncCreatesOnly,
+        productTypeSyncUpdatesOnly,
+        productTypeSyncCreatesUpdates,
+
+        typeSyncCreatesOnly,
+        typeSyncUpdatesOnly,
+        typeSyncCreatesUpdates
     ]
 
 };
@@ -138,16 +192,43 @@ window.onload = function () {
     function addData(data) {
         $.each(data, function (key, val) {
             barChartData.labels.push(key);
+
             productSyncCreatesOnly.data.push(val.productSync.createsOnly.average / 1000)
             productSyncUpdatesOnly.data.push(val.productSync.updatesOnly.average / 1000)
             productSyncCreatesUpdates.data.push(val.productSync.mix.average / 1000)
+
             categorySyncCreatesOnly.data.push(val.categorySync.createsOnly.average / 1000)
             categorySyncUpdatesOnly.data.push(val.categorySync.updatesOnly.average / 1000)
             categorySyncCreatesUpdates.data.push(val.categorySync.mix.average / 1000)
+
             inventorySyncCreatesOnly.data.push(val.inventorySync.createsOnly.average / 1000)
             inventorySyncUpdatesOnly.data.push(val.inventorySync.updatesOnly.average / 1000)
             inventorySyncCreatesUpdates.data.push(val.inventorySync.mix.average / 1000)
+
+
+            if (val.productTypeSync) {
+                //console.log("product type Sync k:" + key)
+                //console.log("product type Sync v:" + JSON.stringify(val))
+                productTypeSyncCreatesOnly.data.push(val.productTypeSync.createsOnly.average / 1000)
+                productTypeSyncUpdatesOnly.data.push(val.productTypeSync.updatesOnly.average / 1000)
+                productTypeSyncCreatesUpdates.data.push(val.productTypeSync.mix.average / 1000)
+            } else {
+                productTypeSyncCreatesOnly.data.push(0)
+                productTypeSyncUpdatesOnly.data.push(0)
+                productTypeSyncCreatesUpdates.data.push(0)
+            }
+
+            if (val.typeSync) {
+                typeSyncCreatesOnly.data.push(val.typeSync.createsOnly.average / 1000)
+                typeSyncUpdatesOnly.data.push(val.typeSync.updatesOnly.average / 1000)
+                typeSyncCreatesUpdates.data.push(val.typeSync.mix.average / 1000)
+            } else {
+                typeSyncCreatesOnly.data.push(0)
+                typeSyncUpdatesOnly.data.push(0)
+                typeSyncCreatesUpdates.data.push(0)
+            }
         });
+        console.log(barChartData);
         window.myBar.update();
     }
 
