2ae0e99076967880f328cc8ef7fdcab53af68bfe
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-19 14:48:38 +0100

    Adapt chart-render.js to new data structure.

--- benchmarks/assets/js/chart-renderer.js
+++ benchmarks/assets/js/chart-renderer.js
@@ -204,42 +204,27 @@ window.onload = function () {
         $.each(data, function (key, val) {
             barChartData.labels.push(key);
 
-            productSyncCreatesOnly.data.push(val.productSync.createsOnly.average / 1000)
-            productSyncUpdatesOnly.data.push(val.productSync.updatesOnly.average / 1000)
-            productSyncCreatesUpdates.data.push(val.productSync.mix.average / 1000)
+            productSyncCreatesOnly.data.push(val.productSync.createsOnly.executionTime / 1000)
+            productSyncUpdatesOnly.data.push(val.productSync.updatesOnly.executionTime / 1000)
+            productSyncCreatesUpdates.data.push(val.productSync.mix.executionTime / 1000)
 
-            categorySyncCreatesOnly.data.push(val.categorySync.createsOnly.average / 1000)
-            categorySyncUpdatesOnly.data.push(val.categorySync.updatesOnly.average / 1000)
-            categorySyncCreatesUpdates.data.push(val.categorySync.mix.average / 1000)
+            categorySyncCreatesOnly.data.push(val.categorySync.createsOnly.executionTime / 1000)
+            categorySyncUpdatesOnly.data.push(val.categorySync.updatesOnly.executionTime / 1000)
+            categorySyncCreatesUpdates.data.push(val.categorySync.mix.executionTime / 1000)
 
-            inventorySyncCreatesOnly.data.push(val.inventorySync.createsOnly.average / 1000)
-            inventorySyncUpdatesOnly.data.push(val.inventorySync.updatesOnly.average / 1000)
-            inventorySyncCreatesUpdates.data.push(val.inventorySync.mix.average / 1000)
+            inventorySyncCreatesOnly.data.push(val.inventorySync.createsOnly.executionTime / 1000)
+            inventorySyncUpdatesOnly.data.push(val.inventorySync.updatesOnly.executionTime / 1000)
+            inventorySyncCreatesUpdates.data.push(val.inventorySync.mix.executionTime / 1000)
 
+            productTypeSyncCreatesOnly.data.push(val.productTypeSync.createsOnly.executionTime / 1000)
+            productTypeSyncUpdatesOnly.data.push(val.productTypeSync.updatesOnly.executionTime / 1000)
+            productTypeSyncCreatesUpdates.data.push(val.productTypeSync.mix.executionTime / 1000)
 
-            if (val.productTypeSync) {
-                //console.log("product type Sync k:" + key)
-                //console.log("product type Sync v:" + JSON.stringify(val))
-                productTypeSyncCreatesOnly.data.push(val.productTypeSync.createsOnly.average / 1000)
-                productTypeSyncUpdatesOnly.data.push(val.productTypeSync.updatesOnly.average / 1000)
-                productTypeSyncCreatesUpdates.data.push(val.productTypeSync.mix.average / 1000)
-            } else {
-                productTypeSyncCreatesOnly.data.push(0)
-                productTypeSyncUpdatesOnly.data.push(0)
-                productTypeSyncCreatesUpdates.data.push(0)
-            }
+            typeSyncCreatesOnly.data.push(val.typeSync.createsOnly.executionTime / 1000)
+            typeSyncUpdatesOnly.data.push(val.typeSync.updatesOnly.executionTime / 1000)
+            typeSyncCreatesUpdates.data.push(val.typeSync.mix.executionTime / 1000)
 
-            if (val.typeSync) {
-                typeSyncCreatesOnly.data.push(val.typeSync.createsOnly.average / 1000)
-                typeSyncUpdatesOnly.data.push(val.typeSync.updatesOnly.average / 1000)
-                typeSyncCreatesUpdates.data.push(val.typeSync.mix.average / 1000)
-            } else {
-                typeSyncCreatesOnly.data.push(0)
-                typeSyncUpdatesOnly.data.push(0)
-                typeSyncCreatesUpdates.data.push(0)
-            }
         });
-        console.log(barChartData);
         window.myBar.update();
     }
 
