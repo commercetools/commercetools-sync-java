17ea123af867db70dff341a63f54b59930875403
Hesham Massoud <hesham.massoud@commercetools.de>
2019-05-31 12:03:23 +0200

    #394: Improve javadocs.

--- src/main/java/com/commercetools/sync/categories/utils/CategoryReferenceReplacementUtils.java
+++ src/main/java/com/commercetools/sync/categories/utils/CategoryReferenceReplacementUtils.java
@@ -23,13 +23,13 @@ public final class CategoryReferenceReplacementUtils {
 
     /**
      * Takes a list of Categories that are supposed to have their custom type, parent category and asset custom type
-     * references expanded in order to be able to fetch the keys and replace the reference ids with the corresponding
-     * keys and then return a new list of category drafts with their references containing keys instead of the ids.
-     * Note that if the references are not expanded for a category, the reference ids will not be replaced with keys and
-     * will still have their ids in place.
+     * references expanded in order to be able to fetch the keys and replace the resource identifier ids with the
+     * corresponding keys and then return a new list of category drafts with their resource identifiers containing keys
+     * instead of the ids. Note that if the references are not expanded for a category, the resource identifier ids will
+     * not be replaced with keys and will still have their ids in place.
      *
-     * @param categories the categories to replace their reference ids with keys
-     * @return a list of category drafts with keys instead of ids for references.
+     * @param categories the categories to replace their resource identifier ids with keys
+     * @return a list of category drafts with keys set on the resource identifiers.
      */
     @Nonnull
     public static List<CategoryDraft> replaceCategoriesReferenceIdsWithKeys(@Nonnull final List<Category> categories) {
