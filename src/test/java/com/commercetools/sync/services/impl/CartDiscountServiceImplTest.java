5808b996a972d5e8db017000a55c538972e38759
Ahmet Öz <bilmuhahmet@gmail.com>
2019-06-12 16:07:39 +0200

    Update src/test/java/com/commercetools/sync/services/impl/CartDiscountServiceImplTest.java
    Co-Authored-By: Hesham Massoud <heshamhamdymassoud@gmail.com>

--- src/test/java/com/commercetools/sync/services/impl/CartDiscountServiceImplTest.java
+++ src/test/java/com/commercetools/sync/services/impl/CartDiscountServiceImplTest.java
@@ -36,7 +36,7 @@ class CartDiscountServiceImplTest {
         final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
                 .of(sphereClient)
                 .build();
-        final CartDiscountServiceImpl cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
 
         // test
         CompletionStage<Optional<CartDiscount>> result = cartDiscountService.fetchCartDiscount("");
