0dca0646c271bca4cd6a2b57bf9ba648212fd9f2
Hesham Massoud <hesham.massoud@commercetools.de>
2019-06-12 16:32:25 +0200

    #372: Add InvalidAttributeDefinitionException and test.

---
+++ src/main/java/com/commercetools/sync/commons/exceptions/InvalidAttributeDefinitionException.java
@@ -0,0 +1,11 @@
+package com.commercetools.sync.commons.exceptions;
+
+import javax.annotation.Nonnull;
+
+public class InvalidAttributeDefinitionException extends Exception {
+
+
+    public InvalidAttributeDefinitionException(@Nonnull final String message, @Nonnull final Throwable cause) {
+        super(message, cause);
+    }
+}
