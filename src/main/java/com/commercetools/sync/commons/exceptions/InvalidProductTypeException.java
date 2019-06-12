71ef27e5ca2e0edd8d978deaf7b73ccd098d777b
Hesham Massoud <hesham.massoud@commercetools.de>
2019-06-12 10:42:58 +0200

    #372: Add InvalidProductTypeException and unit test.

---
+++ src/main/java/com/commercetools/sync/commons/exceptions/InvalidProductTypeException.java
@@ -0,0 +1,40 @@
+package com.commercetools.sync.commons.exceptions;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public class InvalidProductTypeException extends Exception {
+    private Set<Throwable> causes;
+
+    public InvalidProductTypeException(@Nonnull final String message, @Nonnull final Set<Throwable> causes) {
+        super(buildMessage(message, causes));
+        this.causes = causes;
+    }
+
+    @Nonnull
+    private static String buildMessage(@Nonnull final String message, @Nonnull final Set<Throwable> causes) {
+        final String causesString = asMessage(causes);
+        return isNotBlank(causesString) ? format("%s Causes:%n\t\t%s", message, causesString) :  message;
+    }
+
+    @Nonnull
+    private static String asMessage(@Nonnull final Set<Throwable> causes) {
+        final List<String> causesMessages = causes
+            .stream()
+            .map(Throwable::getMessage)
+            .filter(StringUtils::isNotBlank)
+            .collect(Collectors.toList());
+        return String.join("\n\t\t", causesMessages);
+    }
+
+    public Set<Throwable> getCauses() {
+        return causes;
+    }
+}
