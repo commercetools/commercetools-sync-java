4f576cfe1791f8be13df7d2ecdfa6131e4f39e69
Hesham Massoud <hesham.massoud@commercetools.de>
2019-06-13 12:08:37 +0200

    #372: Add buildProductTypeQuery Utils.

--- src/main/java/com/commercetools/sync/producttypes/utils/ProductTypeReferenceReplacementUtils.java
+++ src/main/java/com/commercetools/sync/producttypes/utils/ProductTypeReferenceReplacementUtils.java
@@ -4,6 +4,7 @@ import com.commercetools.sync.commons.exceptions.InvalidAttributeDefinitionExcep
 import com.commercetools.sync.commons.exceptions.InvalidProductTypeException;
 import com.commercetools.sync.commons.exceptions.InvalidReferenceException;
 import com.commercetools.sync.commons.exceptions.ReferenceReplacementException;
+import io.sphere.sdk.expansion.ExpansionPath;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.products.attributes.AttributeDefinitionDraft;
 import io.sphere.sdk.products.attributes.AttributeDefinitionDraftBuilder;
@@ -13,12 +14,16 @@ import io.sphere.sdk.products.attributes.SetAttributeType;
 import io.sphere.sdk.producttypes.ProductType;
 import io.sphere.sdk.producttypes.ProductTypeDraft;
 import io.sphere.sdk.producttypes.ProductTypeDraftBuilder;
+import io.sphere.sdk.producttypes.queries.ProductTypeQuery;
+import io.sphere.sdk.queries.QueryExecutionUtils;
 
 import javax.annotation.Nonnull;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
@@ -146,6 +151,86 @@ public final class ProductTypeReferenceReplacementUtils {
         } else {
             throw new InvalidReferenceException("ProductType reference is not expanded.");
         }
+    }
+
+    /**
+     * Builds a {@link ProductTypeQuery} for fetching products from a source CTP project with an expansion to the
+     * {@link ProductType} reference of any
+     * <a href="https://docs.commercetools.com/http-api-projects-productTypes#nestedtype">NestedType Attribute</a>
+     *
+     * <p>Note: Please only use this util if you have nestedType attributes in the productTypes that you will fetch from
+     * a source commercetools project. Otherwise, it is more efficient to build the query without expansions, if they
+     * are not needed, to avoid unnecessarily bigger payloads fetched from the source project.
+     *
+     * @return the query for fetching products from the source CTP project with an expansion to the {@link ProductType}
+     *         reference of any NestedType Attribute.
+     */
+    @Nonnull
+    public static ProductTypeQuery buildProductTypeQuery() {
+        return buildProductTypeQuery(0);
+    }
+
+    /**
+     * Builds a {@link ProductTypeQuery} for fetching products from a source CTP project with an expansion to the
+     * {@link ProductType} reference of any
+     * <a href="https://docs.commercetools.com/http-api-projects-productTypes#nestedtype">NestedType Attribute</a> and
+     * also any <a href="https://docs.commercetools.com/http-api-projects-productTypes#settype">SetType Attribute</a>
+     * that has an {@code elementType} of {@code NestedType} depending on the given {@code maximumSetDepth} will provide
+     * an expansion for the nestedType of this set depth and all nested types of smaller depths. For example:
+     * <br><br>
+     *     if {@code maximumSetDepth} is 0, it means there is no atttribute that is a type of set of nestedType.
+     *     Therefore, the built query will have the following expansion paths:<br>
+     *     attributes[*].type.typeReference
+     * <br><br>
+     *      if {@code maximumSetDepth} is 1, it means the maximum nesting of sets is set of a NestedType.
+     *      Therefore, the built query will have the following expansion paths:<br>
+     *      attributes[*].type.typeReference, attributes[*].type.elementType.typeReference
+     * <br><br>
+     *     if {@code maximumSetDepth} is 2, it means the maximum nesting of sets is set of set of a NestedType.
+     *     Therefore, the built query will have the following expansion paths:<br>
+     *     attributes[*].type.typeReference, attributes[*].type.elementType.typeReference,
+     *     attributes[*].type.elementType.elementType.typeReference
+     *<br><br>
+     *     and so on..
+     *
+     * <p>Note: Please only use this util if you have nestedType attributes in the productTypes that you will fetch from
+     *    a source commercetools project. Otherwise, it is more efficient to build the query without expansions, if they
+     *    are not needed, to avoid unnecessarily bigger payloads fetched from the source project.
+     *
+     * @return the query for fetching productTypes from the source CTP project with all the aforementioned references
+     *         expanded.
+     */
+    @Nonnull
+    public static ProductTypeQuery buildProductTypeQuery(final int maximumSetDepth) {
+        final String nestedTypeReferenceExpansionPath = "attributes[*].type.typeReference";
+        final ProductTypeQuery productTypeQuery = ProductTypeQuery
+            .of()
+            .withLimit(QueryExecutionUtils.DEFAULT_PAGE_SIZE)
+            .plusExpansionPaths(ExpansionPath.of(nestedTypeReferenceExpansionPath));
+
+
+        final List<ExpansionPath<ProductType>> setsExpansionPath =
+            buildSetOfNestedTypeReferenceExpansionPath(maximumSetDepth);
+
+        return setsExpansionPath.isEmpty()
+            ? productTypeQuery : productTypeQuery.plusExpansionPaths(setsExpansionPath);
+    }
+
+    @Nonnull
+    private static List<ExpansionPath<ProductType>>
+    buildSetOfNestedTypeReferenceExpansionPath(final int maximumSetDepth) {
+
+        return IntStream.rangeClosed(1, maximumSetDepth)
+                        .mapToObj(ProductTypeReferenceReplacementUtils::getExpansionPathForSetDept)
+                        .collect(toList());
+    }
+
+    private static ExpansionPath<ProductType> getExpansionPathForSetDept(final int maximumSetDepth) {
+
+        final String elementTypePath = IntStream.range(0, maximumSetDepth)
+                                                .mapToObj(index -> "elementType")
+                                                .collect(Collectors.joining("."));
+        return ExpansionPath.of(format("attributes[*].type.%s.typeReference", elementTypePath));
     }
 
     private ProductTypeReferenceReplacementUtils() {
