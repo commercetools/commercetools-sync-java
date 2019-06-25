3b0985fca94db3c53627cabe245d9b2773733c05
Ahmet Öz <bilmuhahmet@gmail.com>
2019-06-25 16:29:10 +0200

    #379 add CartDiscountService and CartDiscountSync
    * #379 add Integration tests for CartDiscountSync 
    * #379 Add CartDiscount Sync Benchmarks.
    * #379 add Integration tests for CartDiscountService 
    * #379 use cart discount keys instead of eng name

---
+++ src/test/java/com/commercetools/sync/cartdiscounts/CartDiscountSyncTest.java
@@ -0,0 +1,199 @@
+package com.commercetools.sync.cartdiscounts;
+
+import com.commercetools.sync.cartdiscounts.helpers.CartDiscountSyncStatistics;
+import com.commercetools.sync.services.CartDiscountService;
+import io.sphere.sdk.cartdiscounts.CartDiscount;
+import io.sphere.sdk.cartdiscounts.CartDiscountDraft;
+import io.sphere.sdk.cartdiscounts.CartDiscountDraftBuilder;
+import io.sphere.sdk.cartdiscounts.CartDiscountValue;
+import io.sphere.sdk.cartdiscounts.CartPredicate;
+import io.sphere.sdk.cartdiscounts.ShippingCostTarget;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.models.SphereException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.concurrent.CompletionException;
+
+import static com.commercetools.sync.commons.asserts.statistics.AssertionsForStatistics.assertThat;
+import static java.lang.String.format;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonList;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class CartDiscountSyncTest {
+
+    private static CartDiscountDraft newCartDiscount;
+    private static final String KEY = "cart-discount-key";
+
+    @BeforeAll
+    static void setup() {
+        newCartDiscount = CartDiscountDraftBuilder
+            .of(LocalizedString.of(Locale.GERMAN, "Neu Name", Locale.ENGLISH, "new name"),
+                CartPredicate.of("totalPrice >= \"50 EUR\""),
+                CartDiscountValue.ofRelative(1000),
+                ShippingCostTarget.of(),
+                "0.25",
+                false)
+            .key(KEY)
+            .active(false)
+            .description(LocalizedString.of(Locale.GERMAN, "Beschreibung",
+                Locale.ENGLISH, "description"))
+            .validFrom(ZonedDateTime.parse("2019-05-05T00:00:00.000Z"))
+            .validUntil(ZonedDateTime.parse("2019-05-15T00:00:00.000Z"))
+            .build();
+    }
+
+    @Test
+    void sync_WithErrorFetchingExistingKeys_ShouldExecuteCallbackOnErrorAndIncreaseFailedCounter() {
+        // preparation
+        final List<String> errorMessages = new ArrayList<>();
+        final List<Throwable> exceptions = new ArrayList<>();
+
+        final CartDiscountSyncOptions syncOptions = CartDiscountSyncOptionsBuilder
+            .of(mock(SphereClient.class))
+            .errorCallback((errorMessage, exception) -> {
+                errorMessages.add(errorMessage);
+                exceptions.add(exception);
+            })
+            .build();
+
+
+        final CartDiscountService mockCartDiscountService = mock(CartDiscountService.class);
+
+        when(mockCartDiscountService
+            .fetchMatchingCartDiscountsByKeys(singleton(KEY)))
+            .thenReturn(supplyAsync(() -> {
+                throw new SphereException();
+            }));
+
+        final CartDiscountSync cartDiscountSync = new CartDiscountSync(syncOptions, mockCartDiscountService);
+
+        // test
+        final CartDiscountSyncStatistics cartDiscountSyncStatistics = cartDiscountSync
+            .sync(singletonList(newCartDiscount))
+            .toCompletableFuture().join();
+
+        // assertions
+        assertThat(errorMessages)
+            .hasSize(1)
+            .hasOnlyOneElementSatisfying(message ->
+                assertThat(message).isEqualTo(
+                        format("Failed to fetch existing cart discounts with keys: '[%s]'.", KEY))
+            );
+
+        assertThat(exceptions)
+            .hasSize(1)
+            .hasOnlyOneElementSatisfying(throwable -> {
+                assertThat(throwable).isExactlyInstanceOf(CompletionException.class);
+                assertThat(throwable).hasCauseExactlyInstanceOf(SphereException.class);
+            });
+
+        assertThat(cartDiscountSyncStatistics).hasValues(1, 0, 0, 1);
+    }
+
+    @Test
+    void sync_WithOnlyDraftsToCreate_ShouldCallBeforeCreateCallback() {
+        // preparation
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+            .of(mock(SphereClient.class))
+            .build();
+
+        final CartDiscountService cartDiscountService = mock(CartDiscountService.class);
+        when(cartDiscountService.fetchMatchingCartDiscountsByKeys(anySet())).thenReturn(completedFuture(emptySet()));
+        when(cartDiscountService.createCartDiscount(any())).thenReturn(completedFuture(Optional.empty()));
+
+        final CartDiscountSyncOptions spyCartDiscountSyncOptions = spy(cartDiscountSyncOptions);
+
+        // test
+        new CartDiscountSync(spyCartDiscountSyncOptions, cartDiscountService)
+            .sync(singletonList(newCartDiscount)).toCompletableFuture().join();
+
+        // assertion
+        verify(spyCartDiscountSyncOptions).applyBeforeCreateCallBack(newCartDiscount);
+        verify(spyCartDiscountSyncOptions, never()).applyBeforeUpdateCallBack(any(), any(), any());
+    }
+
+    @Test
+    void sync_WithOnlyDraftsToUpdate_ShouldOnlyCallBeforeUpdateCallback() {
+        // preparation
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+            .of(mock(SphereClient.class))
+            .build();
+
+        final CartDiscount mockedExistingCartDiscount = mock(CartDiscount.class);
+        when(mockedExistingCartDiscount.getKey()).thenReturn(newCartDiscount.getKey());
+
+        final CartDiscountService cartDiscountService = mock(CartDiscountService.class);
+        when(cartDiscountService.fetchMatchingCartDiscountsByKeys(anySet()))
+            .thenReturn(completedFuture(singleton(mockedExistingCartDiscount)));
+
+        when(cartDiscountService.updateCartDiscount(any(), any()))
+            .thenReturn(completedFuture(mockedExistingCartDiscount));
+
+        final CartDiscountSyncOptions spyCartDiscountSyncOptions = spy(cartDiscountSyncOptions);
+
+        // test
+        new CartDiscountSync(spyCartDiscountSyncOptions, cartDiscountService)
+            .sync(singletonList(newCartDiscount)).toCompletableFuture().join();
+
+        // assertion
+        verify(spyCartDiscountSyncOptions).applyBeforeUpdateCallBack(any(), any(), any());
+        verify(spyCartDiscountSyncOptions, never()).applyBeforeCreateCallBack(newCartDiscount);
+    }
+
+    @Test
+    void sync_WithNullDraft_ShouldExecuteCallbackOnErrorAndIncreaseFailedCounter() {
+        //preparation
+        final CartDiscountDraft newCartDiscountDraft = null;
+
+        final List<String> errorMessages = new ArrayList<>();
+        final List<Throwable> exceptions = new ArrayList<>();
+
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(mock(SphereClient.class))
+                .errorCallback((errorMessage, exception) -> {
+                    errorMessages.add(errorMessage);
+                    exceptions.add(exception);
+                })
+                .build();
+
+        final CartDiscountSync cartDiscountSync = new CartDiscountSync(cartDiscountSyncOptions);
+
+        //test
+        final CartDiscountSyncStatistics cartDiscountSyncStatistics = cartDiscountSync
+                .sync(singletonList(newCartDiscountDraft))
+                .toCompletableFuture()
+                .join();
+
+        //assertions
+        assertThat(errorMessages)
+            .hasSize(1)
+            .hasOnlyOneElementSatisfying(message ->
+                assertThat(message).isEqualTo("Failed to process null cart discount draft.")
+            );
+
+        assertThat(exceptions)
+                .hasSize(1)
+                .hasOnlyOneElementSatisfying(throwable -> assertThat(throwable).isNull());
+
+        assertThat(cartDiscountSyncStatistics).hasValues(1, 0, 0, 1);
+    }
+
+}
---
+++ src/test/java/com/commercetools/sync/services/impl/CartDiscountServiceImplTest.java
@@ -0,0 +1,236 @@
+package com.commercetools.sync.services.impl;
+
+import com.commercetools.sync.cartdiscounts.CartDiscountSyncOptions;
+import com.commercetools.sync.cartdiscounts.CartDiscountSyncOptionsBuilder;
+import com.commercetools.sync.services.CartDiscountService;
+import io.sphere.sdk.cartdiscounts.CartDiscount;
+import io.sphere.sdk.cartdiscounts.CartDiscountDraft;
+import io.sphere.sdk.cartdiscounts.commands.CartDiscountUpdateCommand;
+import io.sphere.sdk.cartdiscounts.commands.updateactions.SetDescription;
+import io.sphere.sdk.cartdiscounts.queries.CartDiscountQuery;
+import io.sphere.sdk.client.InternalServerErrorException;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.queries.PagedQueryResult;
+import io.sphere.sdk.utils.CompletableFutureUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.Collections.singletonList;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.only;
+
+class CartDiscountServiceImplTest {
+
+    @Test
+    void fetchCartDiscount_WithEmptyKey_ShouldNotFetchAnyCartDiscount() {
+        // preparation
+        final SphereClient sphereClient = mock(SphereClient.class);
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(sphereClient)
+                .build();
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result = cartDiscountService.fetchCartDiscount("");
+
+
+        // assertions
+        assertThat(result).isCompletedWithValue(Optional.empty());
+        verify(sphereClient, never()).execute(any());
+    }
+
+    @Test
+    void fetchCartDiscount_WithNullKey_ShouldNotFetchAnyCartDiscount() {
+        // preparation
+        final SphereClient sphereClient = mock(SphereClient.class);
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(sphereClient)
+                .build();
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result = cartDiscountService.fetchCartDiscount(null);
+
+
+        // assertions
+        assertThat(result).isCompletedWithValue(Optional.empty());
+        verify(sphereClient, never()).execute(any());
+    }
+
+    @Test
+    void fetchCartDiscount_WithValidKey_ShouldReturnMockCartDiscount() {
+        // preparation
+        final SphereClient sphereClient = mock(SphereClient.class);
+        final CartDiscount mockCartDiscount = mock(CartDiscount.class);
+        when(mockCartDiscount.getId()).thenReturn("testId");
+        when(mockCartDiscount.getKey()).thenReturn("any_key");
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(sphereClient)
+                .build();
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        @SuppressWarnings("unchecked")
+        final PagedQueryResult<CartDiscount> pagedQueryResult =  mock(PagedQueryResult.class);
+        when(pagedQueryResult.head()).thenReturn(Optional.of(mockCartDiscount));
+        when(cartDiscountSyncOptions.getCtpClient().execute(any(CartDiscountQuery.class)))
+                .thenReturn(completedFuture(pagedQueryResult));
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result =
+                cartDiscountService.fetchCartDiscount("any_key");
+
+        // assertions
+        assertThat(result).isCompletedWithValue(Optional.of(mockCartDiscount));
+        verify(sphereClient, only()).execute(any());
+    }
+
+    @Test
+    void createCartDiscount_WithNullCartDiscountKey_ShouldNotCreateCartDiscount() {
+        // preparation
+        final CartDiscountDraft mockCartDiscountDraft = mock(CartDiscountDraft.class);
+        final Map<String, Throwable> errors = new HashMap<>();
+        when(mockCartDiscountDraft.getKey()).thenReturn(null);
+
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(mock(SphereClient.class))
+                .errorCallback(errors::put)
+                .build();
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result = cartDiscountService
+            .createCartDiscount(mockCartDiscountDraft);
+
+        // assertions
+        assertThat(result).isCompletedWithValue(Optional.empty());
+        assertThat(errors).isNotEmpty();
+        assertThat(errors.size()).isEqualTo(1);
+        assertTrue(errors.keySet().stream().anyMatch(e -> e.contains("Draft key is blank!")));
+        verify(cartDiscountSyncOptions.getCtpClient(), times(0)).execute(any());
+    }
+
+    @Test
+    void createCartDiscount_WithUnsuccessfulMockCtpResponse_ShouldNotCreateCartDiscount() {
+        // preparation
+        final CartDiscountDraft mockCartDiscountDraft = mock(CartDiscountDraft.class);
+        final Map<String, Throwable> errors = new HashMap<>();
+        when(mockCartDiscountDraft.getKey()).thenReturn("cartDiscountKey");
+
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(mock(SphereClient.class))
+                .errorCallback(errors::put)
+                .build();
+
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        when(cartDiscountSyncOptions.getCtpClient().execute(any()))
+                .thenReturn(CompletableFutureUtils.failed(new InternalServerErrorException()));
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result =
+                cartDiscountService.createCartDiscount(mockCartDiscountDraft);
+
+        // assertions
+        assertThat(result).isCompletedWithValue(Optional.empty());
+        assertThat(errors.keySet())
+                .hasSize(1)
+                .hasOnlyOneElementSatisfying(message -> {
+                    assertThat(message).contains("Failed to create draft with key: 'cartDiscountKey'.");
+                });
+
+        assertThat(errors.values())
+                .hasSize(1)
+                .hasOnlyOneElementSatisfying(exception ->
+                        assertThat(exception).isExactlyInstanceOf(InternalServerErrorException.class));
+
+
+    }
+
+    @Test
+    void updateCartDiscount_WithMockSuccessfulCtpResponse_ShouldCallCartDiscountUpdateCommand() {
+        // preparation
+        final CartDiscount mockCartDiscount = mock(CartDiscount.class);
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(mock(SphereClient.class))
+                .build();
+
+        when(cartDiscountSyncOptions.getCtpClient().execute(any())).thenReturn(completedFuture(mockCartDiscount));
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        final List<UpdateAction<CartDiscount>> updateActions =
+                singletonList(SetDescription.of(LocalizedString.ofEnglish("new_desc")));
+        // test
+        final CompletionStage<CartDiscount> result =
+                cartDiscountService.updateCartDiscount(mockCartDiscount, updateActions);
+
+        // assertions
+        assertThat(result).isCompletedWithValue(mockCartDiscount);
+        verify(cartDiscountSyncOptions.getCtpClient())
+                .execute(eq(CartDiscountUpdateCommand.of(mockCartDiscount, updateActions)));
+    }
+
+    @Test
+    void updateCartDiscount_WithMockUnsuccessfulCtpResponse_ShouldCompleteExceptionally() {
+        // preparation
+        final CartDiscount mockCartDiscount = mock(CartDiscount.class);
+        final CartDiscountSyncOptions cartDiscountSyncOptions = CartDiscountSyncOptionsBuilder
+                .of(mock(SphereClient.class))
+                .build();
+
+        when(cartDiscountSyncOptions.getCtpClient().execute(any()))
+                .thenReturn(CompletableFutureUtils.exceptionallyCompletedFuture(new InternalServerErrorException()));
+
+        final CartDiscountService cartDiscountService = new CartDiscountServiceImpl(cartDiscountSyncOptions);
+
+        final List<UpdateAction<CartDiscount>> updateActions =
+                singletonList(SetDescription.of(LocalizedString.ofEnglish("new_desc")));
+        // test
+        final CompletionStage<CartDiscount> result =
+                cartDiscountService.updateCartDiscount(mockCartDiscount, updateActions);
+
+        // assertions
+        assertThat(result).hasFailedWithThrowableThat()
+                .isExactlyInstanceOf(InternalServerErrorException.class);
+    }
+
+    @Test
+    void createCartDiscount_WithEmptyCartDiscountKey_ShouldHaveEmptyOptionalAsAResult() {
+        //preparation
+        final SphereClient sphereClient = mock(SphereClient.class);
+        final CartDiscountDraft mockCartDiscountDraft = mock(CartDiscountDraft.class);
+        final Map<String, Throwable> errors = new HashMap<>();
+        when(mockCartDiscountDraft.getName()).thenReturn(LocalizedString.ofEnglish(""));
+
+        final CartDiscountSyncOptions options = CartDiscountSyncOptionsBuilder
+                .of(sphereClient)
+                .errorCallback(errors::put)
+                .build();
+
+        final CartDiscountServiceImpl cartDiscountService = new CartDiscountServiceImpl(options);
+
+        // test
+        final CompletionStage<Optional<CartDiscount>> result = cartDiscountService
+            .createCartDiscount(mockCartDiscountDraft);
+
+        // assertion
+        assertThat(result).isCompletedWithValue(Optional.empty());
+        assertThat(errors.keySet())
+                .containsExactly("Failed to create draft with key: ''. Reason: Draft key is blank!");
+    }
+}
