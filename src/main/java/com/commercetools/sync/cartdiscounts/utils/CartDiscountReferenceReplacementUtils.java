7c2cebb8f429aa812f52742a7c1d075c4fdcf058
Hesham Massoud <hesham.massoud@commercetools.de>
2019-06-27 13:46:59 +0200

    #379: Remove unneeded reference replacement utils for GiftLineItem value.

--- src/main/java/com/commercetools/sync/cartdiscounts/utils/CartDiscountReferenceReplacementUtils.java
+++ src/main/java/com/commercetools/sync/cartdiscounts/utils/CartDiscountReferenceReplacementUtils.java
@@ -3,23 +3,13 @@ package com.commercetools.sync.cartdiscounts.utils;
 import io.sphere.sdk.cartdiscounts.CartDiscount;
 import io.sphere.sdk.cartdiscounts.CartDiscountDraft;
 import io.sphere.sdk.cartdiscounts.CartDiscountDraftBuilder;
-import io.sphere.sdk.cartdiscounts.CartDiscountValue;
-import io.sphere.sdk.cartdiscounts.GiftLineItemCartDiscountValue;
-import io.sphere.sdk.cartdiscounts.queries.CartDiscountQuery;
-import io.sphere.sdk.channels.Channel;
-import io.sphere.sdk.expansion.ExpansionPath;
-import io.sphere.sdk.models.Reference;
-import io.sphere.sdk.models.ResourceIdentifier;
-import io.sphere.sdk.products.Product;
 import io.sphere.sdk.types.CustomFieldsDraft;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.commercetools.sync.commons.utils.CustomTypeReferenceReplacementUtils.replaceCustomTypeIdWithKeys;
-import static com.commercetools.sync.commons.utils.SyncUtils.getResourceIdentifierWithKeyReplaced;
 
 public final class CartDiscountReferenceReplacementUtils {
 
@@ -59,101 +49,7 @@ public final class CartDiscountReferenceReplacementUtils {
             })
             .collect(Collectors.toList());
     }
-
-
-    @Nonnull
-    private static CartDiscountValue replaceCartDiscountValueReferenceIdsWithKeys(
-        @Nonnull final CartDiscountValue cartDiscountValue) {
-
-        if (cartDiscountValue instanceof GiftLineItemCartDiscountValue) {
-
-            final GiftLineItemCartDiscountValue giftLineItemCartDiscountValue =
-                (GiftLineItemCartDiscountValue) cartDiscountValue;
-
-            return replaceGiftLineItemCartDiscountValueReferenceIdsWithKeys(giftLineItemCartDiscountValue);
-        }
-
-        return cartDiscountValue;
-    }
-
-    @Nonnull
-    private static GiftLineItemCartDiscountValue replaceGiftLineItemCartDiscountValueReferenceIdsWithKeys(
-        @Nonnull final GiftLineItemCartDiscountValue giftLineItemCartDiscountValue) {
-
-        final ResourceIdentifier<Product> productWithKeyInReference =
-            replaceProductReferenceIdWithKey(giftLineItemCartDiscountValue);
-        final ResourceIdentifier<Channel> distributionChannelWithKeyInReference =
-            replaceDistributionChannelReferenceIdWithKey(giftLineItemCartDiscountValue);
-        final ResourceIdentifier<Channel> supplyChannelWithKeyInReference =
-            replaceSupplyChannelReferenceIdWithKey(giftLineItemCartDiscountValue);
-
-        return CartDiscountValue.ofGiftLineItem(
-            productWithKeyInReference,
-            giftLineItemCartDiscountValue.getVariantId(),
-            supplyChannelWithKeyInReference,
-            distributionChannelWithKeyInReference);
-    }
-
-    @Nullable
-    @SuppressWarnings("ConstantConditions") // NPE checked in replaceReferenceIdWithKey
-    private static ResourceIdentifier<Channel> replaceDistributionChannelReferenceIdWithKey(
-        @Nonnull final GiftLineItemCartDiscountValue giftLineItemCartDiscountValue) {
-
-        final Reference<Channel> distributionChannel = (Reference)
-            giftLineItemCartDiscountValue.getDistributionChannel();
-
-        if (distributionChannel != null) {
-            return getResourceIdentifierWithKeyReplaced(distributionChannel,
-                () -> ResourceIdentifier.ofId(distributionChannel.getObj().getKey()));
-        }
-
-        return distributionChannel;
-    }
-
-    @Nullable
-    @SuppressWarnings("ConstantConditions") // NPE checked in replaceReferenceIdWithKey
-    private static ResourceIdentifier<Channel> replaceSupplyChannelReferenceIdWithKey(
-        @Nonnull final GiftLineItemCartDiscountValue giftLineItemCartDiscountValue) {
-
-        final Reference<Channel> supplyChannel = (Reference) giftLineItemCartDiscountValue.getSupplyChannel();
-
-        if (supplyChannel != null) {
-            return getResourceIdentifierWithKeyReplaced(supplyChannel,
-                () -> ResourceIdentifier.ofId(supplyChannel.getObj().getKey()));
-        }
-
-        return supplyChannel;
-    }
-
-    @Nullable
-    @SuppressWarnings("ConstantConditions") // NPE checked in replaceReferenceIdWithKey
-    private static ResourceIdentifier<Product> replaceProductReferenceIdWithKey(
-        @Nonnull final GiftLineItemCartDiscountValue giftLineItemCartDiscountValue) {
-        final Reference<Product> product = (Reference) giftLineItemCartDiscountValue.getProduct();
-        return getResourceIdentifierWithKeyReplaced(product, () -> ResourceIdentifier.ofId(product.getObj().getKey()));
-    }
-
-    /**
-     * Builds a {@link CartDiscountQuery} for fetching cart discounts from a source CTP project with all the needed
-     * references expanded for the sync:
-     * <ul>
-     *     <li>Custom Type</li>
-     *     <li>Gift lineItem distribution channel</li>
-     *     <li>Gift lineItem supply channel</li>
-     *     <li>Gift lineItem product reference</li>
-     * </ul>
-     *
-     * @return the query for fetching cart discounts from the source CTP project with all the aforementioned references
-     *         expanded.
-     */
-    public static CartDiscountQuery buildCartDiscountQuery() {
-        return CartDiscountQuery.of()
-                                .withExpansionPaths(ExpansionPath.of("custom.type"))
-                                .plusExpansionPaths(ExpansionPath.of("value.product"))
-                                .plusExpansionPaths(ExpansionPath.of("value.supplyChannel"))
-                                .plusExpansionPaths(ExpansionPath.of("value.distributionChannel"));
-    }
-
+    
 
     private CartDiscountReferenceReplacementUtils() {
     }
