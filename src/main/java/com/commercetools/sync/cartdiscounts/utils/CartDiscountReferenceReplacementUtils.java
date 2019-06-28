8ff8607961d344b416838eff672d3bb9efed1943
Hesham Massoud <hesham.massoud@commercetools.de>
2019-06-28 15:18:02 +0200

    #379: Add reference replacement utils and unit tests.

--- src/main/java/com/commercetools/sync/cartdiscounts/utils/CartDiscountReferenceReplacementUtils.java
+++ src/main/java/com/commercetools/sync/cartdiscounts/utils/CartDiscountReferenceReplacementUtils.java
@@ -3,6 +3,8 @@ package com.commercetools.sync.cartdiscounts.utils;
 import io.sphere.sdk.cartdiscounts.CartDiscount;
 import io.sphere.sdk.cartdiscounts.CartDiscountDraft;
 import io.sphere.sdk.cartdiscounts.CartDiscountDraftBuilder;
+import io.sphere.sdk.cartdiscounts.queries.CartDiscountQuery;
+import io.sphere.sdk.expansion.ExpansionPath;
 import io.sphere.sdk.types.CustomFieldsDraft;
 
 import javax.annotation.Nonnull;
@@ -14,14 +16,14 @@ import static com.commercetools.sync.commons.utils.CustomTypeReferenceReplacemen
 public final class CartDiscountReferenceReplacementUtils {
 
     /**
-     * Takes a list of Categories that are supposed to have their custom type, parent category and asset custom type
-     * references expanded in order to be able to fetch the keys and replace the resource identifier ids with the
-     * corresponding keys and then return a new list of category drafts with their resource identifiers containing keys
-     * instead of the ids. Note that if the references are not expanded for a category, the resource identifier ids will
+     * Takes a list of CartDiscounts that are supposed to have their custom type references expanded in order to be able
+     * to fetch the keys and replace the resource identifier ids with the corresponding keys and then return a new list
+     * of cartDiscountDrafts with their resource identifiers containing keys instead of the ids.
+     * Note that if the references are not expanded for a cart discount, the resource identifier ids will
      * not be replaced with keys and will still have their ids in place.
      *
-     * @param cartDiscounts the categories to replace their resource identifier ids with keys
-     * @return a list of category drafts with keys set on the resource identifiers.
+     * @param cartDiscounts the cartDiscounts to replace their resource identifier ids with keys
+     * @return a list of cartDiscountsDrafts with keys set on the resource identifiers.
      */
     @Nonnull
     public static List<CartDiscountDraft> replaceCartDiscountsReferenceIdsWithKeys(
@@ -49,7 +51,22 @@ public final class CartDiscountReferenceReplacementUtils {
             })
             .collect(Collectors.toList());
     }
-    
+
+    /**
+     * Builds a {@link CartDiscountQuery} for fetching cart discounts from a source CTP project with all the needed
+     * references expanded for the sync:
+     * <ul>
+     *     <li>Custom Type</li>
+     * </ul>
+     *
+     * @return the query for fetching cart discounts from the source CTP project with all the aforementioned references
+     *         expanded.
+     */
+    public static CartDiscountQuery buildCartDiscountQuery() {
+        return CartDiscountQuery.of().withExpansionPaths(ExpansionPath.of("custom.type"));
+    }
+
+
 
     private CartDiscountReferenceReplacementUtils() {
     }
