dfadac1f311bf7200f929b1f4bf17f165e8389f4
Hesham Massoud <heshamhamdymassoud@gmail.com>
2019-07-02 17:45:07 +0200

    #379: Use correct casing for error message.
    Co-Authored-By: Ahmet Öz <bilmuhahmet@gmail.com>

--- src/main/java/com/commercetools/sync/cartdiscounts/CartDiscountSync.java
+++ src/main/java/com/commercetools/sync/cartdiscounts/CartDiscountSync.java
@@ -47,7 +47,7 @@ public class CartDiscountSync extends BaseSync<CartDiscountDraft, CartDiscountSy
     private static final String CART_DISCOUNT_DRAFT_IS_NULL =
         "Failed to process null cart discount draft.";
     private static final String FAILED_TO_RESOLVE_REFERENCES = "Failed to resolve references on "
-        + "cartDiscount with key:'%s'. Reason: %s";
+        + "CartDiscount with key:'%s'. Reason: %s";
 
 
     private final CartDiscountService cartDiscountService;
