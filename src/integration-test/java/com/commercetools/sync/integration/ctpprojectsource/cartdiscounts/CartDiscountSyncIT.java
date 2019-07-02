e286292d57619f9789fb5f245ae3d9e70530c7f3
Hesham Massoud <hesham.massoud@commercetools.de>
2019-07-02 14:45:48 +0200

    #379: Add test for new custom type.

--- src/integration-test/java/com/commercetools/sync/integration/ctpprojectsource/cartdiscounts/CartDiscountSyncIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/ctpprojectsource/cartdiscounts/CartDiscountSyncIT.java
@@ -14,7 +14,10 @@ import io.sphere.sdk.cartdiscounts.ShippingCostTarget;
 import io.sphere.sdk.cartdiscounts.commands.updateactions.ChangeCartPredicate;
 import io.sphere.sdk.cartdiscounts.commands.updateactions.ChangeTarget;
 import io.sphere.sdk.cartdiscounts.commands.updateactions.ChangeValue;
+import io.sphere.sdk.cartdiscounts.commands.updateactions.SetCustomType;
 import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.types.CustomFieldsDraft;
+import io.sphere.sdk.types.Type;
 import io.sphere.sdk.utils.MoneyImpl;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,11 +26,13 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 import static com.commercetools.sync.cartdiscounts.utils.CartDiscountReferenceReplacementUtils.buildCartDiscountQuery;
 import static com.commercetools.sync.cartdiscounts.utils.CartDiscountReferenceReplacementUtils.replaceCartDiscountsReferenceIdsWithKeys;
 import static com.commercetools.sync.commons.asserts.statistics.AssertionsForStatistics.assertThat;
+import static com.commercetools.sync.integration.commons.utils.CartDiscountITUtils.createCartDiscountCustomType;
 import static com.commercetools.sync.integration.commons.utils.CartDiscountITUtils.deleteCartDiscountsFromTargetAndSource;
 import static com.commercetools.sync.integration.commons.utils.CartDiscountITUtils.populateSourceProject;
 import static com.commercetools.sync.integration.commons.utils.CartDiscountITUtils.populateTargetProject;
@@ -35,6 +40,7 @@ import static com.commercetools.sync.integration.commons.utils.ITUtils.deleteTyp
 import static com.commercetools.sync.integration.commons.utils.SphereClientUtils.CTP_SOURCE_CLIENT;
 import static com.commercetools.sync.integration.commons.utils.SphereClientUtils.CTP_TARGET_CLIENT;
 import static io.sphere.sdk.models.DefaultCurrencyUnits.EUR;
+import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CartDiscountSyncIT {
@@ -97,6 +103,10 @@ class CartDiscountSyncIT {
         final List<CartDiscount> cartDiscounts = CTP_SOURCE_CLIENT
             .execute(buildCartDiscountQuery())
             .toCompletableFuture().join().getResults();
+        final String newTypeKey = "new-type";
+        createCartDiscountCustomType(newTypeKey, Locale.ENGLISH, newTypeKey, CTP_SOURCE_CLIENT);
+        final Type newTargetCustomType = createCartDiscountCustomType(newTypeKey, Locale.ENGLISH, newTypeKey,
+            CTP_TARGET_CLIENT);
 
         final List<CartDiscountDraft> cartDiscountsReferenceIdsWithKeys =
             replaceCartDiscountsReferenceIdsWithKeys(cartDiscounts);
@@ -109,6 +119,7 @@ class CartDiscountSyncIT {
                 .cartPredicate(CartPredicate.of("totalPrice >= \"100 EUR\""))
                 .value(AbsoluteCartDiscountValue.of(MoneyImpl.of(40, EUR)))
                 .target(ShippingCostTarget.of())
+                .custom(CustomFieldsDraft.ofTypeIdAndJson(newTypeKey, emptyMap()))
                 .build())
             .collect(Collectors.toList());
 
@@ -139,9 +150,10 @@ class CartDiscountSyncIT {
         assertThat(errorMessages).isEmpty();
         assertThat(exceptions).isEmpty();
         assertThat(updateActionsList).containsExactly(
-                ChangeValue.of(CartDiscountValue.ofAbsolute(Collections.singletonList(MoneyImpl.of(40, EUR)))),
-                ChangeCartPredicate.of("totalPrice >= \"100 EUR\""),
-                ChangeTarget.of(ShippingCostTarget.of())
+            ChangeValue.of(CartDiscountValue.ofAbsolute(Collections.singletonList(MoneyImpl.of(40, EUR)))),
+            ChangeCartPredicate.of("totalPrice >= \"100 EUR\""),
+            ChangeTarget.of(ShippingCostTarget.of()),
+            SetCustomType.ofTypeIdAndJson(newTargetCustomType.getId(), emptyMap())
         );
         assertThat(cartDiscountSyncStatistics).hasValues(2, 1, 1, 0);
         assertThat(cartDiscountSyncStatistics
