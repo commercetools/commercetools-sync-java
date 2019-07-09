be4171fc34d0aa700ef3af5de3d261549986cee8
Hesham Massoud <hesham.massoud@commercetools.de>
2019-07-09 12:31:25 +0200

    #372: Refactor redundant code into exception utils.

--- src/main/java/com/commercetools/sync/commons/exceptions/ReferenceReplacementException.java
+++ src/main/java/com/commercetools/sync/commons/exceptions/ReferenceReplacementException.java
@@ -1,37 +1,16 @@
 package com.commercetools.sync.commons.exceptions;
 
-import org.apache.commons.lang3.StringUtils;
-
 import javax.annotation.Nonnull;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import static java.lang.String.format;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static com.commercetools.sync.commons.exceptions.ExceptionUtils.buildMessage;
 
 public class ReferenceReplacementException extends RuntimeException {
-    private Set<Throwable> causes;
+    private final Set<Throwable> causes;
 
     public ReferenceReplacementException(@Nonnull final String message, @Nonnull final Set<Throwable> causes) {
-        super(buildMessage(message, causes));
+        super(buildMessage(message, causes, 1));
         this.causes = causes;
-    }
-
-    @Nonnull
-    private static String buildMessage(@Nonnull final String message, @Nonnull final Set<Throwable> causes) {
-        final String causesString = asMessage(causes);
-        return isNotBlank(causesString) ? format("%s Causes:%n\t%s", message, causesString) :  message;
-    }
-
-    @Nonnull
-    private static String asMessage(@Nonnull final Set<Throwable> causes) {
-        final List<String> causesMessages = causes
-            .stream()
-            .map(Throwable::getMessage)
-            .filter(StringUtils::isNotBlank)
-            .collect(Collectors.toList());
-        return String.join("\n\t", causesMessages);
     }
 
     public Set<Throwable> getCauses() {
