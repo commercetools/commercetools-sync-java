519a9a7ae889332670cb6c851b1aa880b19a04d9
Hesham Massoud <hesham.massoud@commercetools.de>
2019-07-09 11:37:22 +0200

    #372: Remove empty line.

--- src/main/java/com/commercetools/sync/commons/exceptions/InvalidAttributeDefinitionException.java
+++ src/main/java/com/commercetools/sync/commons/exceptions/InvalidAttributeDefinitionException.java
@@ -4,7 +4,6 @@ import javax.annotation.Nonnull;
 
 public class InvalidAttributeDefinitionException extends Exception {
 
-
     public InvalidAttributeDefinitionException(@Nonnull final String message, @Nonnull final Throwable cause) {
         super(message, cause);
     }
