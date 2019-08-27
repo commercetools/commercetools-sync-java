a1f0cd6d362274115ec4e04dbc3283834203fa9c
Hesham Massoud <heshamhamdymassoud@gmail.com>
2019-08-27 16:48:37 +0200

    Update CartDiscountSync.java

--- src/main/java/com/commercetools/sync/cartdiscounts/CartDiscountSync.java
+++ src/main/java/com/commercetools/sync/cartdiscounts/CartDiscountSync.java
@@ -34,7 +34,7 @@ import static java.util.stream.Collectors.toSet;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
- * This class syncs cart discount drafts with the corresponding cart discounts in the CTP project. test
+ * This class syncs cart discount drafts with the corresponding cart discounts in the CTP project.
  */
 public class CartDiscountSync extends BaseSync<CartDiscountDraft, CartDiscountSyncStatistics, CartDiscountSyncOptions> {
 
