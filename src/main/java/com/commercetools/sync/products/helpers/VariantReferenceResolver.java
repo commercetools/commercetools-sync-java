7a5bb29c86f8040942e745c8d7d0e083f3cfa7e6
Hesham Massoud <hesham.massoud@commercetools.de>
2019-09-10 16:02:23 +0200

    #440: Add comment about null filtering.

--- src/main/java/com/commercetools/sync/products/helpers/VariantReferenceResolver.java
+++ src/main/java/com/commercetools/sync/products/helpers/VariantReferenceResolver.java
@@ -72,10 +72,13 @@ public final class VariantReferenceResolver extends BaseReferenceResolver<Produc
 
 
     /**
-     * Given a {@link ProductVariantDraft} this method attempts to resolve the prices and attributes to
+     * Given a {@link ProductVariantDraft} this method attempts to resolve the prices, assets and attributes to
      * return a {@link CompletionStage} which contains a new instance of the draft with the resolved
      * references. The keys of the references are either taken from the expanded references or
      * taken from the id field of the references.
+     *
+     * <p>Note: this method will filter out any null sub resources (e.g. prices, attributes or assets) under the
+     * returned resolved variant.
      *
      * @param productVariantDraft the product variant draft to resolve it's references.
      * @return a {@link CompletionStage} that contains as a result a new productDraft instance with resolved references
