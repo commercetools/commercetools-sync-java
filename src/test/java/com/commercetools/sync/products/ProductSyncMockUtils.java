0a8aecde455b0ad7fcbc60371c563f313ccb8caa
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2020-07-14 16:13:45 +0200

    Allways generate doubles with a point as decimalseperator.

--- src/test/java/com/commercetools/sync/products/ProductSyncMockUtils.java
+++ src/test/java/com/commercetools/sync/products/ProductSyncMockUtils.java
@@ -31,17 +31,13 @@ import io.sphere.sdk.states.State;
 import io.sphere.sdk.taxcategories.TaxCategory;
 import io.sphere.sdk.types.CustomFields;
 import io.sphere.sdk.types.Type;
+import org.mockito.internal.matchers.Or;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.text.DecimalFormatSymbols;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -92,6 +88,10 @@ public class ProductSyncMockUtils {
         ORDER_HINT_FORMAT = new DecimalFormat();
         ORDER_HINT_FORMAT.setMaximumFractionDigits(Integer.MAX_VALUE);
         ORDER_HINT_FORMAT.setMaximumIntegerDigits(1);
+        DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols(Locale.ENGLISH);
+        formatSymbols.setDecimalSeparator('.');
+        formatSymbols.setGroupingSeparator('.');
+        ORDER_HINT_FORMAT.setDecimalFormatSymbols(formatSymbols);
     }
 
     /**
