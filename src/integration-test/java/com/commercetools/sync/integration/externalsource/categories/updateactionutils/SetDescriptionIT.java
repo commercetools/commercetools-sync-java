616f6dec9802679d17019ed777c254ad5827463b
ninalindl <nina.lindl@web.de>
2020-08-24 18:15:13 +0200

    removed unnecessary IT tests on update action utils and from externalprojectsource

--- src/integration-test/java/com/commercetools/sync/integration/externalsource/categories/updateactionutils/SetDescriptionIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/externalsource/categories/updateactionutils/SetDescriptionIT.java
@@ -50,56 +50,6 @@ class SetDescriptionIT {
         deleteTypes(CTP_TARGET_CLIENT);
     }
 
-
-    @Test
-    void buildSetDescriptionUpdateAction_WithDifferentValues_ShouldBuildUpdateAction() {
-        // Prepare new category draft with a different description
-        final CategoryDraft newCategory = CategoryDraftBuilder
-            .of(oldCategory.getName(), oldCategory.getSlug())
-            .description(LocalizedString.of(Locale.ENGLISH, "amazing description"))
-            .build();
-
-        // Build set description update action
-        final UpdateAction<Category> setDescriptionUpdateAction =
-            buildSetDescriptionUpdateAction(oldCategory, newCategory).orElse(null);
-
-        assertThat(setDescriptionUpdateAction).isNotNull();
-        assertThat(setDescriptionUpdateAction.getAction()).isEqualTo("setDescription");
-        assertThat(((SetDescription) setDescriptionUpdateAction).getDescription())
-            .isEqualTo(LocalizedString.of(Locale.ENGLISH, "amazing description"));
-    }
-
-    @Test
-    void buildSetDescriptionUpdateAction_WithDifferentLocaleOrder_ShouldNotBuildUpdateAction() {
-        // Prepare new category draft with a different order of locales of description
-        final CategoryDraft newCategory = CategoryDraftBuilder
-            .of(oldCategory.getName(), oldCategory.getSlug())
-            .description(
-                LocalizedString.of(Locale.GERMAN, "Erstaunliche Beschreibung", Locale.ENGLISH, "amazing description"))
-            .build();
-
-        // Build set description update action
-        final UpdateAction<Category> setDescriptionUpdateAction =
-            buildSetDescriptionUpdateAction(oldCategory, newCategory).orElse(null);
-
-        assertThat(setDescriptionUpdateAction).isNull();
-    }
-
-    @Test
-    void buildSetDescriptionUpdateAction_WithSameValues_ShouldNotBuildUpdateAction() {
-        // Prepare new category draft with a same description as root category
-        final CategoryDraft newCategory = CategoryDraftBuilder
-            .of(oldCategory.getName(), oldCategory.getSlug())
-            .description(oldCategory.getDescription())
-            .build();
-
-        // Build set description update action
-        final UpdateAction<Category> setDescriptionUpdateAction =
-            buildSetDescriptionUpdateAction(oldCategory, newCategory).orElse(null);
-
-        assertThat(setDescriptionUpdateAction).isNull();
-    }
-
     @Test
     void buildSetDescriptionUpdateAction_WithNullValue_ShouldTriggerCallback() {
         // Prepare new category draft with no description
