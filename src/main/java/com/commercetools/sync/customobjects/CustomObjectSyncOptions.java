3d5f566a307af6dc51d9b54d6b18fed7aafbda4a
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-01 15:42:10 +0200

    Create SyncOptions and SyncOptionsBuilder

---
+++ src/main/java/com/commercetools/sync/customobjects/CustomObjectSyncOptions.java
@@ -0,0 +1,40 @@
+package com.commercetools.sync.customobjects;
+
+import com.commercetools.sync.commons.BaseSyncOptions;
+import com.commercetools.sync.commons.exceptions.SyncException;
+import com.commercetools.sync.commons.utils.QuadConsumer;
+import com.commercetools.sync.commons.utils.TriConsumer;
+import com.commercetools.sync.commons.utils.TriFunction;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.customobjects.CustomObject;
+import io.sphere.sdk.customobjects.CustomObjectDraft;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public final class CustomObjectSyncOptions extends BaseSyncOptions<CustomObject, CustomObjectDraft> {
+
+    CustomObjectSyncOptions(
+        @Nonnull final SphereClient ctpClient,
+        @Nullable final QuadConsumer<SyncException, Optional<CustomObjectDraft>, Optional<CustomObject>,
+            List<UpdateAction<CustomObject>>> errorCallBack,
+        @Nullable final TriConsumer<SyncException, Optional<CustomObjectDraft>, Optional<CustomObject>>
+            warningCallBack,
+        final int batchSize,
+        @Nullable final TriFunction<List<UpdateAction<CustomObject>>, CustomObjectDraft, CustomObject,
+            List<UpdateAction<CustomObject>>> beforeUpdateCallback,
+        @Nullable final Function<CustomObjectDraft, CustomObjectDraft> beforeCreateCallback) {
+        super(
+            ctpClient,
+            errorCallBack,
+            warningCallBack,
+            batchSize,
+            beforeUpdateCallback,
+            beforeCreateCallback);
+    }
+
+}
