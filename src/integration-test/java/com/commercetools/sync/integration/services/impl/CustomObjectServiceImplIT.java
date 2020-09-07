680cf698447d0c4dfb35755138c4cd51a640224e
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-07 20:11:13 +0200

    Revert "Remove redundant variable in CustomObjectServiceImplIT"
    This reverts commit 2eee4febffd5a3d9a93f1e0058f8fa51b28efdb9.

--- src/integration-test/java/com/commercetools/sync/integration/services/impl/CustomObjectServiceImplIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/services/impl/CustomObjectServiceImplIT.java
@@ -45,6 +45,7 @@ class CustomObjectServiceImplIT {
             JsonNodeFactory.instance.objectNode().put(
                     "old_custom_object_attribute", "old_custom_object_value");
 
+    private static final String NEW_CUSTOM_OBJECT_ID = "new_custom_object_id";
     private static final String NEW_CUSTOM_OBJECT_KEY = "new_custom_object_key";
     private static final String NEW_CUSTOM_OBJECT_CONTAINER = "new_custom_object_container";
     private static final ObjectNode NEW_CUSTOM_OBJECT_VALUE =
