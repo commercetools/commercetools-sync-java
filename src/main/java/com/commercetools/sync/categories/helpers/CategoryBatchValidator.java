01fde866769538eb09636878fe1033218cfa2a29
salander85 <sarah.lander@commercetools.com>
2020-09-09 15:51:45 +0200

    #235 Change CategoryBatchProcessor and add UnitTests
    Changes in CategoryBatchProcessor
    * Rename to CategoryBatchValidator
    * Handle errors inside
    * Return a ImmutablePair of valid drafts and valid keys from the validation method
    * Pass the batch of categoryDrafts in the validation method
    * Change constructor to init syncOptions and statistics fields

---
+++ src/main/java/com/commercetools/sync/categories/helpers/CategoryBatchValidator.java
@@ -0,0 +1,107 @@
+package com.commercetools.sync.categories.helpers;
+
+import com.commercetools.sync.categories.CategorySyncOptions;
+import io.sphere.sdk.categories.Category;
+import io.sphere.sdk.categories.CategoryDraft;
+import io.sphere.sdk.models.ResourceIdentifier;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+public class CategoryBatchValidator {
+    static final String CATEGORY_DRAFT_KEY_NOT_SET = "CategoryDraft with name: %s doesn't have a key. "
+        + "Please make sure all category drafts have keys.";
+    static final String CATEGORY_DRAFT_IS_NULL = "CategoryDraft is null.";
+    static final String PARENT_CATEGORY_KEY_NOT_SET = "Parent category reference of "
+        + "CategoryDraft with key '%s' has no key set. Please make sure parent category has a key.";
+
+    final CategorySyncOptions syncOptions;
+    final CategorySyncStatistics statistics;
+
+    public CategoryBatchValidator(@Nonnull final CategorySyncOptions syncOptions, @Nonnull final CategorySyncStatistics statistics) {
+        this.syncOptions = syncOptions;
+        this.statistics = statistics;
+    }
+
+    /**
+     * This method validates a list of category drafts.
+     *
+     *
+     * <p>A valid category draft is one which satisfies the following conditions:
+     * <ol>
+     * <li>It has a key which is not blank (null/empty)</li>
+     * <li>It has parent category valid</li>
+     * <li>A parent is valid if it satisfies the following conditions:
+     * <ol>
+     * <li>It has a key which is not blank (null/empty)</li>
+     * </ol>
+     * </li>
+     * </ol>
+     * @return a tuple of valid category drafts and valid keys
+     * @param categoryDrafts - a list of category drafts considered in this batch
+     */
+
+    public ImmutablePair<List<CategoryDraft>, List<String>> validateAndCollectValidDraftsAndKeys(List<CategoryDraft> categoryDrafts) {
+        List<CategoryDraft> validDrafts = new ArrayList<>();
+        List<String> validKeys = new ArrayList<>();
+        for (CategoryDraft categoryDraft : categoryDrafts) {
+            if (validateCategoryDraft(categoryDraft)) {
+                ResourceIdentifier<Category> parent = categoryDraft.getParent();
+                if (validateParentCategory(parent, categoryDraft.getKey())) {
+                    if (parent != null) {
+                        validKeys.add(parent.getKey());
+                    }
+                    validKeys.add(categoryDraft.getKey());
+                    validDrafts.add(categoryDraft);
+                }
+            }
+        }
+
+        return ImmutablePair.of(validDrafts, validKeys);
+    }
+
+    private boolean validateCategoryDraft(final CategoryDraft categoryDraft) {
+        String errorMessage = EMPTY;
+        if (categoryDraft != null) {
+            if (isBlank(categoryDraft.getKey())) {
+                errorMessage = format(CATEGORY_DRAFT_KEY_NOT_SET, categoryDraft.getName());
+            }
+        } else {
+            errorMessage = CATEGORY_DRAFT_IS_NULL;
+        }
+
+        if (!isBlank(errorMessage)) {
+            handleError(errorMessage);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateParentCategory(final ResourceIdentifier<Category> parent, final String categoryDraftKey) {
+        if (parent != null && isBlank(parent.getKey())) {
+                handleError(format(PARENT_CATEGORY_KEY_NOT_SET, categoryDraftKey));
+                return false;
+        }
+
+        return true;
+    }
+
+        /**
+         * Given a {@link String} {@code errorMessage}, this method calls the
+         * optional error callback specified in the {@code syncOptions} and updates the {@code statistics} instance by
+         * incrementing the total number of failed categories to sync.
+         *
+         * @param errorMessage The error message describing the reason(s) of failure.
+         */
+        private void handleError(@Nonnull final String errorMessage) {
+            this.syncOptions.applyErrorCallback(errorMessage);
+            this.statistics.incrementFailed();
+        }
+
+}
---
+++ src/test/java/com/commercetools/sync/categories/helpers/CategoryBatchValidatorTest.java
@@ -0,0 +1,138 @@
+package com.commercetools.sync.categories.helpers;
+
+import com.commercetools.sync.categories.CategorySyncOptions;
+import com.commercetools.sync.categories.CategorySyncOptionsBuilder;
+import io.sphere.sdk.categories.CategoryDraft;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.models.ResourceIdentifier;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.commercetools.sync.categories.helpers.CategoryBatchValidator.*;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CategoryBatchValidatorTest {
+
+    private CategoryBatchValidator categoryBatchValidator;
+    private List<String> errorCallBackMessages;
+    private List<Throwable> errorCallBackExceptions;
+
+    @BeforeEach
+    void setup() {
+        errorCallBackMessages = new ArrayList<>();
+        errorCallBackExceptions = new ArrayList<>();
+        final SphereClient ctpClient = mock(SphereClient.class);
+        final CategorySyncOptions syncOptions = CategorySyncOptionsBuilder.of(ctpClient)
+                .errorCallback((exception, oldResource, newResource, updateActions) -> {
+                    errorCallBackMessages.add(exception.getMessage());
+                    errorCallBackExceptions.add(exception.getCause());
+                })
+                .build();
+        CategorySyncStatistics statistics = mock(CategorySyncStatistics.class);
+
+        categoryBatchValidator = new CategoryBatchValidator(syncOptions, statistics);
+    }
+
+    @Test
+    void validateBatch_WithEmptyBatch_ShouldHaveEmptyResult() {
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.emptyList());
+
+        assertThat(errorCallBackMessages).hasSize(0);
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithNullCategoryDraft_ShouldHaveValidationErrorAndEmptyResult() {
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.singletonList(null));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(CATEGORY_DRAFT_IS_NULL);
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithCategoryDraftWithNullKey_ShouldHaveValidationErrorAndEmptyResult() {
+        final CategoryDraft categoryDraft = mock(CategoryDraft.class);
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.singletonList(categoryDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(format(CATEGORY_DRAFT_KEY_NOT_SET, categoryDraft.getName()));
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithCategoryDraftWithEmptyKey_ShouldHaveValidationErrorAndEmptyResult() {
+        final CategoryDraft categoryDraft = mock(CategoryDraft.class);
+        when(categoryDraft.getKey()).thenReturn(EMPTY);
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.singletonList(categoryDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(format(CATEGORY_DRAFT_KEY_NOT_SET, categoryDraft.getName()));
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithCategoryDraftWithParentHasNullKey_ShouldHaveValidationErrorAndEmptyResult() {
+        final CategoryDraft categoryDraft = mock(CategoryDraft.class);
+        when(categoryDraft.getKey()).thenReturn("key");
+        when(categoryDraft.getParent()).thenReturn(ResourceIdentifier.ofKey(null));
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.singletonList(categoryDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(format(PARENT_CATEGORY_KEY_NOT_SET, categoryDraft.getKey()));
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithCategoryDraftWithParentHasEmptyKey_ShouldHaveValidationErrorAndEmptyResult() {
+        final CategoryDraft categoryDraft = mock(CategoryDraft.class);
+        when(categoryDraft.getKey()).thenReturn("key");
+        when(categoryDraft.getParent()).thenReturn(ResourceIdentifier.ofKey(""));
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Collections.singletonList(categoryDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(format(PARENT_CATEGORY_KEY_NOT_SET, categoryDraft.getKey()));
+        assertThat(draftsAndKeys.left).isEmpty();
+        assertThat(draftsAndKeys.right).isEmpty();
+    }
+
+    @Test
+    void validateBatch_WithValidDrafts_ShouldReturnResult() {
+        final CategoryDraft validCategoryDraft = mock(CategoryDraft.class);
+        when(validCategoryDraft.getKey()).thenReturn("validDraftKey");
+        when(validCategoryDraft.getParent()).thenReturn(ResourceIdentifier.ofKey("validParentKey"));
+
+        final CategoryDraft validMainCategoryDraft = mock(CategoryDraft.class);
+        when(validMainCategoryDraft.getKey()).thenReturn("validDraftKey1");
+
+        final CategoryDraft invalidCategoryDraft = mock(CategoryDraft.class);
+        when(invalidCategoryDraft.getParent()).thenReturn(ResourceIdentifier.ofKey("key"));
+
+        final ImmutablePair<List<CategoryDraft>, List<String>> draftsAndKeys = categoryBatchValidator.validateAndCollectValidDraftsAndKeys(Arrays.asList(validCategoryDraft, invalidCategoryDraft, validMainCategoryDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0)).isEqualTo(format(CATEGORY_DRAFT_KEY_NOT_SET,invalidCategoryDraft.getName()));
+        assertThat(draftsAndKeys.right).hasSize(3);
+        assertThat(draftsAndKeys.right).containsExactlyInAnyOrder("validDraftKey", "validParentKey", "validDraftKey1");
+        assertThat(draftsAndKeys.left).hasSize(2);
+        assertThat(draftsAndKeys.left).containsExactlyInAnyOrder(validCategoryDraft, validMainCategoryDraft);
+    }
+
+
+
+}
