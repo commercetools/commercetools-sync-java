3930ddb3c62ca47870400de216c6e4f27a560a85
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-10 09:05:44 +0200

    Implement unit test for SyncUtils, SyncOptions and SyncOptionsBuilder

--- src/test/java/com/commercetools/sync/customobjects/CustomObjectSyncOptionsTest.java
+++ src/test/java/com/commercetools/sync/customobjects/CustomObjectSyncOptionsTest.java
@@ -1,22 +1,91 @@
 package com.commercetools.sync.customobjects;
 
+import com.commercetools.sync.commons.utils.TriFunction;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.customobjects.CustomObject;
 import io.sphere.sdk.customobjects.CustomObjectDraft;
-
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static java.util.Collections.emptyList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 
 class CustomObjectSyncOptionsTest {
 
     private static SphereClient CTP_CLIENT = mock(SphereClient.class);
+
+    private interface MockTriFunction extends
+            TriFunction<List<UpdateAction<CustomObject<JsonNode>>>,
+                    CustomObjectDraft<JsonNode>, CustomObject<JsonNode>, List<UpdateAction<CustomObject<JsonNode>>>> {
+    }
+
+    @Test
+    void applyBeforeUpdateCallback_WithNullCallbackAndEmptyUpdateActions_ShouldReturnIdenticalList() {
+        final CustomObjectSyncOptions customObjectSyncOptions = CustomObjectSyncOptionsBuilder.of(CTP_CLIENT)
+                .build();
+
+        final List<UpdateAction<CustomObject<JsonNode>>> updateActions = emptyList();
+
+        final List<UpdateAction<CustomObject<JsonNode>>> filteredList =
+                customObjectSyncOptions.applyBeforeUpdateCallback(updateActions,
+                    mock(CustomObjectDraft.class), mock(CustomObject.class));
+
+        assertThat(filteredList).isSameAs(updateActions);
+    }
+
+
+    @Test
+    void applyBeforeUpdateCallback_WithNullReturnCallbackAndEmptyUpdateActions_ShouldReturnEmptyList() {
+        final TriFunction<List<UpdateAction<CustomObject<JsonNode>>>, CustomObjectDraft<JsonNode>,
+                CustomObject<JsonNode>, List<UpdateAction<CustomObject<JsonNode>>>>
+                beforeUpdateCallback = (updateActions, newCustomObject, oldCustomObject) -> null;
+        final CustomObjectSyncOptions customObjectSyncOptions = CustomObjectSyncOptionsBuilder.of(CTP_CLIENT)
+                .beforeUpdateCallback(beforeUpdateCallback)
+                .build();
+        final List<UpdateAction<CustomObject<JsonNode>>> updateActions = emptyList();
+
+        final List<UpdateAction<CustomObject<JsonNode>>> filteredList =
+                customObjectSyncOptions.applyBeforeUpdateCallback(
+                        updateActions, mock(CustomObjectDraft.class), mock(CustomObject.class));
+
+        assertAll(
+            () -> assertThat(filteredList).isEqualTo(updateActions),
+            () -> assertThat(filteredList).isEmpty()
+        );
+    }
+
+
+
+    @Test
+    void applyBeforeUpdateCallback_WithEmptyUpdateActions_ShouldNotApplyBeforeUpdateCallback() {
+        final CustomObjectSyncOptionsTest.MockTriFunction beforeUpdateCallback =
+                mock(CustomObjectSyncOptionsTest.MockTriFunction.class);
+        final CustomObjectSyncOptions customObjectSyncOptions = CustomObjectSyncOptionsBuilder.of(CTP_CLIENT)
+                .beforeUpdateCallback(beforeUpdateCallback)
+                .build();
+
+        final List<UpdateAction<CustomObject<JsonNode>>> filteredList =
+                customObjectSyncOptions.applyBeforeUpdateCallback(emptyList(),
+                        mock(CustomObjectDraft.class), mock(CustomObject.class));
+
+        assertThat(filteredList).isEmpty();
+        verify(beforeUpdateCallback, never()).apply(any(), any(), any());
+    }
+
 
     @Test
     void applyBeforeCreateCallback_WithCallback_ShouldReturnFilteredDraft() {
--- src/test/java/com/commercetools/sync/customobjects/utils/CustomObjectSyncUtilsTest.java
+++ src/test/java/com/commercetools/sync/customobjects/utils/CustomObjectSyncUtilsTest.java
@@ -9,6 +9,15 @@ import io.sphere.sdk.customobjects.CustomObjectDraft;
 
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.commercetools.sync.commons.utils.SyncUtils.batchElements;
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,7 +32,7 @@ class CustomObjectSyncUtilsTest {
 
 
     @Test
-    void verifyIdenticalJsonNode_WithSameFieldAndValueInJsonNode_ShouldBeIdentical() {
+    void hasIdenticalValue_WithSameFieldAndValueInJsonNode_ShouldBeIdentical() {
 
         ObjectNode oldValue = JsonNodeFactory.instance.objectNode().put("username", "Peter");
         ObjectNode newValue = JsonNodeFactory.instance.objectNode().put("username", "Peter");
@@ -36,7 +45,7 @@ class CustomObjectSyncUtilsTest {
     }
 
     @Test
-    void verifyIdenticalJsonNode_WithSameFieldAndDifferentValueInJsonNode_ShouldNotBeIdentical() {
+    void hasIdenticalValue_WithSameFieldAndDifferentValueInJsonNode_ShouldNotBeIdentical() {
 
         ObjectNode oldValue = JsonNodeFactory.instance.objectNode().put("username", "Peter");
         ObjectNode newValue = JsonNodeFactory.instance.objectNode().put("username", "Joe");
@@ -49,7 +58,7 @@ class CustomObjectSyncUtilsTest {
     }
 
     @Test
-    void verifyIdenticalJsonNode_WithSameFieldAndValueInDifferentOrderInJsonNode_ShouldBeIdentical() {
+    void hasIdenticalValue_WithSameFieldAndValueInDifferentOrderInJsonNode_ShouldBeIdentical() {
 
         ObjectNode oldValue = JsonNodeFactory.instance.objectNode()
                 .put("username", "Peter")
@@ -70,7 +79,7 @@ class CustomObjectSyncUtilsTest {
     }
 
     @Test
-    void verifyIdenticalJsonNode_WithSameNestedJsonNode_WithSameAttributeOrderInNestedJson_ShouldBeIdentical() {
+    void hasIdenticalValue_WithSameNestedJsonNode_WithSameAttributeOrderInNestedJson_ShouldBeIdentical() {
 
         JsonNode oldNestedJson = JsonNodeFactory.instance.objectNode()
                 .put("username", "Peter")
@@ -97,7 +106,7 @@ class CustomObjectSyncUtilsTest {
     }
 
     @Test
-    void verifyIdenticalJsonNode_WithSameNestedJsonNode_WithDifferentAttributeOrderInNestedJson_ShouldBeIdentical() {
+    void hasIdenticalValue_WithSameNestedJsonNode_WithDifferentAttributeOrderInNestedJson_ShouldBeIdentical() {
 
         JsonNode oldNestedJson = JsonNodeFactory.instance.objectNode()
                 .put("username", "Peter")
@@ -124,7 +133,7 @@ class CustomObjectSyncUtilsTest {
     }
 
     @Test
-    void verifyIdenticalJsonNode_WithDifferentNestedJsonNode_ShouldNotBeIdentical() {
+    void hasIdenticalValue_WithDifferentNestedJsonNode_ShouldNotBeIdentical() {
 
         JsonNode oldNestedJson = JsonNodeFactory.instance.objectNode()
                 .put("username", "Peter")
@@ -148,5 +157,93 @@ class CustomObjectSyncUtilsTest {
 
         assertThat(oldJsonNode.toString()).isNotEqualTo(newJsonNode.toString());
         assertThat(CustomObjectSyncUtils.hasIdenticalValue(oldCustomObject, newCustomObjectdraft)).isFalse();
+    }
+
+    @Test
+    void batchElements_WithValidSize_ShouldReturnCorrectBatches() {
+        final int numberOfCustomObjectDrafts = 160;
+        final int batchSize = 10;
+        final ArrayList<CustomObjectDraft<JsonNode>> customObjectDrafts = new ArrayList<>();
+
+        for (int i = 0; i < numberOfCustomObjectDrafts; i++) {
+            customObjectDrafts.add(CustomObjectDraft
+                    .ofUnversionedUpsert("container_" + i, "key_" + i,
+                            JsonNodeFactory.instance.objectNode().put("field", "field_" + i)));
+        }
+        final List<List<CustomObjectDraft<JsonNode>>> batches = batchElements(customObjectDrafts, 10);
+        assertThat(batches.size()).isEqualTo(numberOfCustomObjectDrafts / batchSize);
+    }
+
+    @Test
+    void batchElements_WithNegativeSize_ShouldReturnNoBatches() {
+        final int numberOfCustomObjectDrafts = 160;
+        final ArrayList<CustomObjectDraft<JsonNode>> customObjectDrafts = new ArrayList<>();
+
+
+        final List<List<CustomObjectDraft<JsonNode>>> batches = batchElements(customObjectDrafts, -100);
+        assertThat(batches.size()).isEqualTo(0);
+    }
+
+    @Test
+    void batchElements_WithEmptyListAndAnySize_ShouldReturnNoBatches() {
+        final List<List<CustomObjectDraft<JsonNode>>> batches = batchElements(new ArrayList<>(), 100);
+        assertThat(batches.size()).isEqualTo(0);
+    }
+
+    @Test
+    void batchElements_WithUniformSeparation_ShouldReturnCorrectBatches() {
+        batchStringElementsAndAssertAfterBatching(100, 10);
+        batchStringElementsAndAssertAfterBatching(3, 1);
+    }
+
+    @Test
+    void batchElements_WithNonUniformSeparation_ShouldReturnCorrectBatches() {
+        batchStringElementsAndAssertAfterBatching(100, 9);
+        batchStringElementsAndAssertAfterBatching(3, 2);
+    }
+
+    @Nonnull
+    private List<String> getPrefixedStrings(final int numberOfElements, @Nonnull final String prefix) {
+        return IntStream.range(1, numberOfElements + 1)
+                .mapToObj(i -> format("%s#%s", prefix, i))
+                .collect(Collectors.toList());
+    }
+
+    private void batchStringElementsAndAssertAfterBatching(final int numberOfElements, final int batchSize) {
+        final List<String> elements = getPrefixedStrings(numberOfElements, "element");
+
+        final List<List<String>> batches = batchElements(elements, batchSize);
+
+        final int expectedNumberOfBatches = getExpectedNumberOfBatches(numberOfElements, batchSize);
+        assertThat(batches.size()).isEqualTo(expectedNumberOfBatches);
+
+        // Assert correct size of elements after batching
+        final Integer numberOfElementsAfterBatching = batches.stream()
+                .map(List::size)
+                .reduce(0,
+                    (element1, element2) -> element1 + element2);
+
+        assertThat(numberOfElementsAfterBatching).isEqualTo(numberOfElements);
+
+
+        // Assert correct splitting of batches
+        final int remainder = numberOfElements % batchSize;
+        if (remainder == 0) {
+            final int numberOfDistinctBatchSizes = batches.stream().collect(Collectors.groupingBy(List::size)).size();
+            assertThat(numberOfDistinctBatchSizes).isEqualTo(1);
+        } else {
+            final List<String> lastBatch = batches.get(batches.size() - 1);
+            assertThat(lastBatch).hasSize(remainder);
+        }
+
+        // Assert that all elements have been batched in correct order
+        final List<String> flatBatches = batches.stream()
+                .flatMap(Collection::stream).collect(Collectors.toList());
+        IntStream.range(0, flatBatches.size())
+                .forEach(index -> assertThat(flatBatches.get(index)).isEqualTo(format("element#%s", index + 1)));
+    }
+
+    private int getExpectedNumberOfBatches(int numberOfElements, int batchSize) {
+        return (int) (Math.ceil((double)numberOfElements / batchSize));
     }
 }
