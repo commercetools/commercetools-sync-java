d1fc24a90047b68d8de12d0f31c7a2381dc5fdce
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-14 15:31:25 +0200

    Remove unused import in CustomObjectSyncTest

--- src/test/java/com/commercetools/sync/customobjects/CustomObjectSyncTest.java
+++ src/test/java/com/commercetools/sync/customobjects/CustomObjectSyncTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletionException;
