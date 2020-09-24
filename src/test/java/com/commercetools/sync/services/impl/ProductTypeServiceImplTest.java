7bdb9a3c281de6a007377a2c6eed80a0470e778b
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-24 13:06:56 +0200

    Refactor ProductTypeServiceImplTest for decorated client

--- src/test/java/com/commercetools/sync/services/impl/ProductTypeServiceImplTest.java
+++ src/test/java/com/commercetools/sync/services/impl/ProductTypeServiceImplTest.java
@@ -1,21 +1,23 @@
 package com.commercetools.sync.services.impl;
 
+import com.commercetools.sync.internals.helpers.CustomHeaderSphereClientDecorator;
 import com.commercetools.sync.producttypes.ProductTypeSyncOptions;
 import com.commercetools.sync.producttypes.ProductTypeSyncOptionsBuilder;
 import com.commercetools.sync.services.ProductTypeService;
 import io.sphere.sdk.client.BadGatewayException;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.queries.ProductTypeQuery;
 import io.sphere.sdk.utils.CompletableFutureUtils;
-import org.junit.jupiter.api.Test;
-
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -59,11 +61,12 @@ class ProductTypeServiceImplTest {
     void fetchProductType_WithBadGateWayException_ShouldCompleteExceptionally() {
         // preparation
         final SphereClient sphereClient = mock(SphereClient.class);
-        final ProductTypeSyncOptions syncOptions = ProductTypeSyncOptionsBuilder
+        final SphereClient mockDecoratedClient = mock(CustomHeaderSphereClientDecorator.class);
+        final ProductTypeSyncOptions syncOptions = spy(ProductTypeSyncOptionsBuilder
                 .of(sphereClient)
-                .build();
-
-        when(syncOptions.getCtpClient().execute(any()))
+                .build());
+        when(syncOptions.getCtpClient()).thenReturn(mockDecoratedClient);
+        when(mockDecoratedClient.execute(any(ProductTypeQuery.class)))
             .thenReturn(CompletableFutureUtils.exceptionallyCompletedFuture(new BadGatewayException()));
 
         final ProductTypeService productTypeService = new ProductTypeServiceImpl(syncOptions);
