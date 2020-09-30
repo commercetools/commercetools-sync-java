4c2e75b486e916284350445525d30c59a047ae70
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-30 17:41:48 +0200

    Add CustomHeaderSphereClientDecoratorTest

---
+++ src/test/java/com/commercetools/sync/internals/helpers/CustomHeaderSphereClientDecoratorTest.java
@@ -0,0 +1,40 @@
+package com.commercetools.sync.internals.helpers;
+
+import io.sphere.sdk.client.HttpRequestIntent;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.client.SphereRequest;
+import io.sphere.sdk.http.HttpHeaders;
+import io.sphere.sdk.models.Versioned;
+import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.commands.ProductTypeUpdateCommand;
+import java.util.Collections;
+
+import io.sphere.sdk.producttypes.commands.updateactions.ChangeName;
+import org.junit.jupiter.api.Test;
+
+import static com.commercetools.sync.commons.utils.SyncSolutionInfo.LIB_VERSION;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+
+public class CustomHeaderSphereClientDecoratorTest {
+
+    private static final String LIB_NAME = "commercetools-sync-java";
+    
+    @Test
+    void executeRequest_shouldHaveLibVersionInRequestHeader() {
+
+        SphereClient sphereClient = CustomHeaderSphereClientDecorator.of(mock(SphereClient.class));
+        SphereRequest<ProductType> productTypeUpdateSphereRequest =
+                ProductTypeUpdateCommand.of(
+                        Versioned.of("ID", 1L),
+                        Collections.singletonList(ChangeName.of("newName")));
+
+        sphereClient.execute(productTypeUpdateSphereRequest);
+        HttpRequestIntent requestIntent = ((CustomHeaderSphereClientDecorator)sphereClient).getHttpRequestIntent();
+        HttpHeaders headers = requestIntent.getHeaders();
+        assertThat(headers.getHeader(LIB_NAME).size()).isGreaterThan(0);
+        assertThat(headers.getHeader(LIB_NAME).get(0)).isEqualTo(LIB_VERSION);
+
+    }
+}
