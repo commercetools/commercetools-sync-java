ea2809b1ede945c6d1c41e1bfc5f1f6732348555
King-Hin Leung <kinghin.leung@gmail.com>
2020-09-30 17:37:00 +0200

    Add CustomHeaderSphereClientDecorator

---
+++ src/main/java/com/commercetools/sync/internals/helpers/CustomHeaderSphereClientDecorator.java
@@ -0,0 +1,48 @@
+package com.commercetools.sync.internals.helpers;
+
+import io.sphere.sdk.client.HttpRequestIntent;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.client.SphereClientDecorator;
+import io.sphere.sdk.client.SphereRequest;
+import io.sphere.sdk.client.SphereRequestDecorator;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletionStage;
+
+import static com.commercetools.sync.commons.utils.SyncSolutionInfo.LIB_NAME;
+import static com.commercetools.sync.commons.utils.SyncSolutionInfo.LIB_VERSION;
+
+public final class CustomHeaderSphereClientDecorator extends SphereClientDecorator {
+
+    private SphereRequestDecorator delegatedSphereRequestDecorator;
+
+    private CustomHeaderSphereClientDecorator(@Nonnull final SphereClient sphereClient) {
+        super(sphereClient);
+    }
+
+    @Nonnull
+    public static SphereClient of(@Nonnull final SphereClient sphereClient) {
+        return new CustomHeaderSphereClientDecorator(sphereClient);
+    }
+
+    @Override
+    public <T> CompletionStage<T> execute(@Nonnull final SphereRequest<T> sphereRequest) {
+        delegatedSphereRequestDecorator = new CustomHeaderSphereRequestDecorator<>(sphereRequest);
+        return super.execute(new CustomHeaderSphereRequestDecorator<>(sphereRequest));
+    }
+
+    protected HttpRequestIntent getHttpRequestIntent() {
+        return delegatedSphereRequestDecorator.httpRequestIntent();
+    }
+
+    private static final class CustomHeaderSphereRequestDecorator<T> extends SphereRequestDecorator<T> {
+        CustomHeaderSphereRequestDecorator(@Nonnull final SphereRequest<T> delegate) {
+            super(delegate);
+        }
+
+        @Override
+        public HttpRequestIntent httpRequestIntent() {
+            return super.httpRequestIntent().plusHeader(LIB_NAME, LIB_VERSION);
+        }
+    }
+}
