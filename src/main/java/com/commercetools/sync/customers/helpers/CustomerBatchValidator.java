1b6aa6c1eaa0956f9f34214bd417d4d508edb8d4
aoz <bilmuhahmet@gmail.com>
2020-10-02 14:21:51 +0200

    add validation cases for the addresses

--- src/main/java/com/commercetools/sync/customers/helpers/CustomerBatchValidator.java
+++ src/main/java/com/commercetools/sync/customers/helpers/CustomerBatchValidator.java
@@ -4,26 +4,57 @@ import com.commercetools.sync.commons.helpers.BaseBatchValidator;
 import com.commercetools.sync.commons.helpers.BaseSyncStatistics;
 import com.commercetools.sync.customers.CustomerSyncOptions;
 import io.sphere.sdk.customers.CustomerDraft;
+import io.sphere.sdk.models.Address;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toSet;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class CustomerBatchValidator
     extends BaseBatchValidator<CustomerDraft, CustomerSyncOptions, BaseSyncStatistics> {
 
-    static final String CUSTOMER_DRAFT_EMAIL_NOT_SET = "CustomerDraft with key: %s doesn't have an email. "
-        + "Please make sure all customer drafts have emails.";
+    static final String CUSTOMER_DRAFT_IS_NULL = "CustomerDraft is null.";
     static final String CUSTOMER_DRAFT_KEY_NOT_SET = "CustomerDraft with email: %s doesn't have a key. "
         + "Please make sure all customer drafts have keys.";
-    static final String CUSTOMER_DRAFT_IS_NULL = "CustomerDraft is null.";
+    static final String CUSTOMER_DRAFT_EMAIL_NOT_SET = "CustomerDraft with key: %s doesn't have an email. "
+        + "Please make sure all customer drafts have emails.";
+
+    static final String ADDRESSES_ARE_NULL = "CustomerDraft with key: '%s' has null addresses on indexes: '%s'. "
+        + "Please make sure each address is set in the addresses list.";
+    static final String ADDRESSES_THAT_KEYS_NOT_SET = "CustomerDraft with key: '%s' has blank address keys on indexes: "
+        + "'%s'. Please make sure each address has a key in the addresses list";
+    static final String ADDRESSES_THAT_KEYS_NOT_UNIQUE = "CustomerDraft with key: '%s' has duplicate address keys on "
+        + "indexes: '%s'. Please make sure each address key is unique in the addresses list.";
+
+    static final String DEFAULT_BILLING_ADDRESS_IS_NOT_VALID = "CustomerDraft with key: '%s' does not contain an "
+        + "index: '%s' of the 'defaultBillingAddress' in the addresses array."
+        + "Please make sure all customer drafts has a valid index value for the 'defaultBillingAddress'."
+        + "Note: When The 'defaultBillingAddressId' of the customer will be set to the ID of that address.";
+
+    static final String BILLING_ADDRESSES_ARE_NOT_VALID = "CustomerDraft with key: '%s' does not contain indices: '%s' "
+        + "of the 'billingAddresses' in the addresses list. "
+        + "Please make sure all customer drafts have valid index values for the 'billingAddresses'."
+        + "Note: The 'billingAddressIds' of the customer will be set to the IDs of that addresses.";
+
+    static final String DEFAULT_SHIPPING_ADDRESS_IS_NOT_VALID = "CustomerDraft with key: '%s' does not contain an "
+        + "index: '%s' of the 'defaultShippingAddress' in the addresses array."
+        + "Please make sure all customer drafts has a valid index value for the 'defaultShippingAddress'."
+        + "Note: The 'defaultShippingAddressId' of the customer will be set to the ID of that address.";
+
+    static final String SHIPPING_ADDRESSES_ARE_NOT_VALID = "CustomerDraft with key: '%s' does not contain indices: '%s'"
+        + " of the 'shippingAddresses' in the addresses list. "
+        + "Please make sure all customer drafts have valid index values for the 'shippingAddresses'."
+        + "Note: The 'shippingAddressIds' of the customer will be set to the IDs of that addresses.";
 
     public CustomerBatchValidator(@Nonnull final CustomerSyncOptions syncOptions,
                                   @Nonnull final BaseSyncStatistics syncStatistics) {
@@ -41,6 +72,11 @@ public class CustomerBatchValidator
      * <li>It is not null</li>
      * <li>It has a key which is not blank (null/empty)</li>
      * <li>It has a email which is not blank (null/empty)</li>
+     * <li>It has not null addresses</li>
+     * <li>It has valid addresses with keys is not blank (null/empty)</li>
+     * <li>It has unique address keys</li>
+     * <li>It has valid valid 'defaultBillingAddress', 'billingAddresses',
+     * 'defaultShippingAddress', 'shippingAddresses'</li>
      * </ol>
      *
      * @param customerDrafts the customer drafts to validate and collect referenced keys.
@@ -57,13 +93,12 @@ public class CustomerBatchValidator
             .stream()
             .filter(this::isValidCustomerDraft)
             .peek(customerDraft -> collectReferencedKeys(referencedKeys, customerDraft))
-            .collect(Collectors.toSet());
+            .collect(toSet());
 
         return ImmutablePair.of(validDrafts, referencedKeys);
     }
 
-    private boolean isValidCustomerDraft(
-        @Nullable final CustomerDraft customerDraft) {
+    private boolean isValidCustomerDraft(@Nullable final CustomerDraft customerDraft) {
 
         if (customerDraft == null) {
             handleError(CUSTOMER_DRAFT_IS_NULL);
@@ -71,15 +106,121 @@ public class CustomerBatchValidator
             handleError(format(CUSTOMER_DRAFT_KEY_NOT_SET, customerDraft.getEmail()));
         } else if (isBlank(customerDraft.getEmail())) {
             handleError(format(CUSTOMER_DRAFT_EMAIL_NOT_SET, customerDraft.getKey()));
-        } else {
+        } else if (hasValidAddresses(customerDraft)) {
             // todo (ahmetoz)
-            // address: Sets the key of each address to be unique in the addresses list.
-            // check if billing and shipping addresses has the right index.
-            // check if the indexes are not negative.
             // what to do for password ?
-            return true;
+            return hasValidBillingAndShippingAddresses(customerDraft);
         }
         return false;
+    }
+
+    private boolean hasValidAddresses(@Nonnull final CustomerDraft customerDraft) {
+
+        final List<Address> addressList = customerDraft.getAddresses();
+        if (addressList == null || addressList.isEmpty()) {
+            return true;
+        }
+
+        final List<Integer> indexesOfNullAddresses = getIndexes(addressList, Objects::isNull);
+        if (!indexesOfNullAddresses.isEmpty()) {
+            handleError(format(ADDRESSES_ARE_NULL, customerDraft.getKey(), indexesOfNullAddresses));
+            return false;
+        }
+
+        final List<Integer> indexesOfBlankAddressKeys = getIndexes(addressList, address -> isBlank(address.getKey()));
+        if (!indexesOfBlankAddressKeys.isEmpty()) {
+            handleError(format(ADDRESSES_THAT_KEYS_NOT_SET, customerDraft.getKey(), indexesOfBlankAddressKeys));
+            return false;
+        }
+
+        final Predicate<Address> searchDuplicateKeys = (Address address) ->
+            addressList.stream().filter(a -> Objects.equals(a.getKey(), address.getKey())).count() > 1;
+
+        final List<Integer> indexesOfDuplicateAddressKeys = getIndexes(addressList, searchDuplicateKeys);
+        if (!indexesOfDuplicateAddressKeys.isEmpty()) {
+            handleError(format(ADDRESSES_THAT_KEYS_NOT_UNIQUE, customerDraft.getKey(), indexesOfDuplicateAddressKeys));
+            return false;
+        }
+
+        return true;
+    }
+
+    private List<Integer> getIndexes(@Nonnull final List<Address> list, @Nonnull final Predicate<Address> predicate) {
+
+        final List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (predicate.test(list.get(i))) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+
+    private boolean hasValidBillingAndShippingAddresses(@Nonnull final CustomerDraft customerDraft) {
+        /* An example error response from the API, when the indexes are not valid:
+           {
+                "statusCode": 400,
+                "message": "Customer does not contain an address at index '1'.",
+                "errors": [{
+                    "code": "InvalidOperation",
+                    "message": "Customer does not contain an address at index '-1'."
+                },
+                {
+                    "code": "InvalidOperation",
+                    "message": "Customer does not contain an address at index '1'."
+                }]
+            }
+         */
+
+        final List<Address> addressList = customerDraft.getAddresses();
+        final boolean hasAddresses = addressList != null && !addressList.isEmpty();
+
+        final Predicate<Integer> isMissingIndex = index -> index == null
+            || (hasAddresses && (index < 0 || index > addressList.size() - 1));
+
+        if (customerDraft.getDefaultBillingAddress() != null
+            && isMissingIndex.test(customerDraft.getDefaultBillingAddress())) {
+            handleError(format(DEFAULT_BILLING_ADDRESS_IS_NOT_VALID,
+                customerDraft.getKey(), customerDraft.getDefaultBillingAddress()));
+            return false;
+        }
+
+        if (customerDraft.getBillingAddresses() != null && !customerDraft.getBillingAddresses().isEmpty()) {
+            final List<Integer> inValidIndexes = getIndexValues(customerDraft.getBillingAddresses(), isMissingIndex);
+            if (!inValidIndexes.isEmpty()) {
+                handleError(format(BILLING_ADDRESSES_ARE_NOT_VALID, customerDraft.getKey(), inValidIndexes));
+                return false;
+            }
+        }
+
+        if (customerDraft.getDefaultShippingAddress() != null
+            && isMissingIndex.test(customerDraft.getDefaultShippingAddress())) {
+            handleError(format(DEFAULT_SHIPPING_ADDRESS_IS_NOT_VALID,
+                customerDraft.getKey(), customerDraft.getDefaultShippingAddress()));
+            return false;
+        }
+
+        if (customerDraft.getShippingAddresses() != null && !customerDraft.getShippingAddresses().isEmpty()) {
+            final List<Integer> inValidIndexes = getIndexValues(customerDraft.getBillingAddresses(), isMissingIndex);
+            if (!inValidIndexes.isEmpty()) {
+                handleError(format(SHIPPING_ADDRESSES_ARE_NOT_VALID, customerDraft.getKey(), inValidIndexes));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<Integer> getIndexValues(@Nonnull final List<Integer> list,
+        @Nonnull final Predicate<Integer> predicate) {
+
+        final List<Integer> indexes = new ArrayList<>();
+        for (Integer integer : list) {
+            if (predicate.test(integer)) {
+                indexes.add(integer);
+            }
+        }
+        return indexes;
     }
 
     private void collectReferencedKeys(
@@ -90,6 +231,7 @@ public class CustomerBatchValidator
             referencedKeys.customerGroupKeys::add);
         collectReferencedKeyFromCustomFieldsDraft(customerDraft.getCustom(),
             referencedKeys.typeKeys::add);
+        // todo (ahmetoz) stores.
     }
 
     public static class ReferencedKeys {
--- src/test/java/com/commercetools/sync/customers/helpers/CustomerBatchValidatorTest.java
+++ src/test/java/com/commercetools/sync/customers/helpers/CustomerBatchValidatorTest.java
@@ -2,8 +2,10 @@ package com.commercetools.sync.customers.helpers;
 
 import com.commercetools.sync.customers.CustomerSyncOptions;
 import com.commercetools.sync.customers.CustomerSyncOptionsBuilder;
+import com.neovisionaries.i18n.CountryCode;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.customers.CustomerDraft;
+import io.sphere.sdk.models.Address;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,10 +16,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.ADDRESSES_ARE_NULL;
+import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.ADDRESSES_THAT_KEYS_NOT_SET;
+import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.ADDRESSES_THAT_KEYS_NOT_UNIQUE;
+import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.BILLING_ADDRESSES_ARE_NOT_VALID;
 import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.CUSTOMER_DRAFT_EMAIL_NOT_SET;
 import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.CUSTOMER_DRAFT_IS_NULL;
 import static com.commercetools.sync.customers.helpers.CustomerBatchValidator.CUSTOMER_DRAFT_KEY_NOT_SET;
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -44,7 +52,7 @@ class CustomerBatchValidatorTest {
 
     @Test
     void validateAndCollectReferencedKeys_WithEmptyDraft_ShouldHaveEmptyResult() {
-        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.emptyList());
+        final Set<CustomerDraft> validDrafts = getValidDrafts(emptyList());
 
         assertThat(errorCallBackMessages).hasSize(0);
         assertThat(validDrafts).isEmpty();
@@ -107,6 +115,104 @@ class CustomerBatchValidatorTest {
         assertThat(validDrafts).isEmpty();
     }
 
+    @Test
+    void validateAndCollectReferencedKeys_WithNullAddressList_ShouldNotHaveValidationError() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(null);
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(0);
+        assertThat(validDrafts).containsExactly(customerDraft);
+    }
+
+    @Test
+    void validateAndCollectReferencedKeys_WithEmptyAddressList_ShouldNotHaveValidationError() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(emptyList());
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(0);
+        assertThat(validDrafts).containsExactly(customerDraft);
+    }
+
+    @Test
+    void validateAndCollectReferencedKeys_WithNullAddresses_ShouldHaveValidationErrorAndEmptyResult() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(asList(null, Address.of(CountryCode.DE), null));
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0))
+            .isEqualTo(format(ADDRESSES_ARE_NULL, "key", "[0, 2]"));
+        assertThat(validDrafts).isEmpty();
+    }
+
+    @Test
+    void validateAndCollectReferencedKeys_WithBlankKeysInAddresses_ShouldHaveValidationErrorAndEmptyResult() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(asList(
+            Address.of(CountryCode.DE).withKey("address-key1"),
+            Address.of(CountryCode.DE).withKey(null),
+            Address.of(CountryCode.US).withKey("address-key2"),
+            Address.of(CountryCode.AC).withKey("  ")));
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0))
+            .isEqualTo(format(ADDRESSES_THAT_KEYS_NOT_SET, "key", "[1, 3]"));
+        assertThat(validDrafts).isEmpty();
+    }
+
+    @Test
+    void validateAndCollectReferencedKeys_WithDuplicateKeysInAddresses_ShouldHaveValidationErrorAndEmptyResult() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(asList(
+            Address.of(CountryCode.DE).withKey("address-key1"),
+            Address.of(CountryCode.FR).withKey("address-key2"),
+            Address.of(CountryCode.DE).withKey("address-key3"),
+            Address.of(CountryCode.US).withKey("address-key1"),
+            Address.of(CountryCode.US).withKey("address-key3")));
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0))
+            .isEqualTo(format(ADDRESSES_THAT_KEYS_NOT_UNIQUE, "key", "[0, 2, 3, 4]"));
+        assertThat(validDrafts).isEmpty();
+    }
+
+    @Test
+    void validateAndCollectReferencedKeys_WithNullBillingIndexes_ShouldHaveValidationErrorAndEmptyResult() {
+        final CustomerDraft customerDraft = mock(CustomerDraft.class);
+        when(customerDraft.getKey()).thenReturn("key");
+        when(customerDraft.getEmail()).thenReturn("email");
+        when(customerDraft.getAddresses()).thenReturn(asList(
+            Address.of(CountryCode.DE).withKey("address-key1"),
+            Address.of(CountryCode.FR).withKey("address-key2"),
+            Address.of(CountryCode.US).withKey("address-key3")));
+        when(customerDraft.getBillingAddresses()).thenReturn(asList(null, -1,  1, 2));
+
+        final Set<CustomerDraft> validDrafts = getValidDrafts(Collections.singletonList(customerDraft));
+
+        assertThat(errorCallBackMessages).hasSize(1);
+        assertThat(errorCallBackMessages.get(0))
+            .isEqualTo(format(BILLING_ADDRESSES_ARE_NOT_VALID, "key", "[null, -1]"));
+        assertThat(validDrafts).isEmpty();
+    }
 
     @Nonnull
     private Set<CustomerDraft> getValidDrafts(@Nonnull final List<CustomerDraft> customerDrafts) {
