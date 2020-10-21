0c0b1ab28b80228953795cbe2331a5dde9d677f4
salander85 <70885646+salander85@users.noreply.github.com>
2020-10-21 09:46:18 +0200

    Update javadoc
    Co-authored-by: Ahmet Öz <bilmuhahmet@gmail.com>

--- src/main/java/com/commercetools/sync/shoppinglists/helpers/ShoppingListBatchValidator.java
+++ src/main/java/com/commercetools/sync/shoppinglists/helpers/ShoppingListBatchValidator.java
@@ -65,7 +65,7 @@ public class ShoppingListBatchValidator
      * </li>
      * </ol>
      *
-     * @param shoppingListDrafts the shopping list drafts to validate and collect referenced type keys.
+     * @param shoppingListDrafts the shopping list drafts to validate and collect referenced keys.
      * @return {@link ImmutablePair}&lt;{@link Set}&lt;{@link ShoppingListDraft}&gt;,
      * {@link ReferencedKeys}&gt; which contains the {@link Set} of valid drafts and
      *      referenced keys within a wrapper.
