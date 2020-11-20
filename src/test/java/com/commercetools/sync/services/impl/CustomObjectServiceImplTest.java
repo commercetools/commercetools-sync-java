cc8dcc6f4be9ee7bde56ebe30f0f0b6ae7c0f114
King-Hin Leung <kinghin.leung@gmail.com>
2020-11-20 17:17:53 +0100

    Resolve CustomObjectServiceImplTest

--- src/test/java/com/commercetools/sync/services/impl/CustomObjectServiceImplTest.java
+++ src/test/java/com/commercetools/sync/services/impl/CustomObjectServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.commercetools.sync.services.impl;
 
+import com.commercetools.sync.commons.FakeClient;
 import com.commercetools.sync.customobjects.CustomObjectSyncOptions;
 import com.commercetools.sync.customobjects.CustomObjectSyncOptionsBuilder;
 import com.commercetools.sync.customobjects.helpers.CustomObjectCompositeIdentifier;
@@ -10,15 +11,7 @@ import io.sphere.sdk.client.BadRequestException;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.customobjects.CustomObject;
 import io.sphere.sdk.customobjects.CustomObjectDraft;
-import io.sphere.sdk.customobjects.commands.CustomObjectUpsertCommand;
-import io.sphere.sdk.customobjects.queries.CustomObjectQuery;
 import io.sphere.sdk.queries.PagedQueryResult;
-import io.sphere.sdk.utils.CompletableFutureUtils;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,16 +20,22 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Nonnull;
 
 import static io.sphere.sdk.customobjects.CustomObjectUtils.getCustomObjectJavaTypeForValue;
 import static io.sphere.sdk.json.SphereJsonUtils.convertToJavaType;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class CustomObjectServiceImplTest {
@@ -48,6 +47,7 @@ class CustomObjectServiceImplTest {
     private String customObjectId;
     private String customObjectContainer;
     private String customObjectKey;
+    private CustomObjectSyncOptions customObjectSyncOptions;
 
     @BeforeEach
     void setup() {
@@ -55,9 +55,7 @@ class CustomObjectServiceImplTest {
         customObjectContainer = RandomStringUtils.random(15, true, true);
         customObjectKey = RandomStringUtils.random(15, true, true);
 
-        CustomObjectSyncOptions customObjectSyncOptions = CustomObjectSyncOptionsBuilder.of(client).build();
-
-        service = new CustomObjectServiceImpl(customObjectSyncOptions);
+        initMockService(client);
     }
 
     @AfterEach
@@ -82,15 +80,15 @@ class CustomObjectServiceImplTest {
         final CustomObjectPagedQueryResult result = mock(CustomObjectPagedQueryResult.class);
         when(result.getResults()).thenReturn(Collections.singletonList(mock));
 
-        when(client.execute(any())).thenReturn(CompletableFuture.completedFuture(result));
-
+        final FakeClient<CustomObject<JsonNode>> fakeCustomObjectClient = new FakeClient(result);
+        initMockService(fakeCustomObjectClient);
 
         final Optional<String> fetchedId = service
             .fetchCachedCustomObjectId(CustomObjectCompositeIdentifier.of(key, container))
             .toCompletableFuture().join();
 
         assertThat(fetchedId).contains(id);
-        verify(client).execute(any(CustomObjectQuery.class));
+        assertThat(fakeCustomObjectClient.isExecuted()).isTrue();
     }
 
     @Test
@@ -117,7 +115,8 @@ class CustomObjectServiceImplTest {
         final CustomObjectPagedQueryResult result = mock(CustomObjectPagedQueryResult.class);
         when(result.getResults()).thenReturn(Arrays.asList(mock1, mock2));
 
-        when(client.execute(any())).thenReturn(CompletableFuture.completedFuture(result));
+        final FakeClient<CustomObject<JsonNode>> fakeCustomObjectClient = new FakeClient(result);
+        initMockService(fakeCustomObjectClient);
 
         final Set<CustomObject<JsonNode>> customObjects = service
             .fetchMatchingCustomObjects(customObjectCompositeIdentifiers)
@@ -132,7 +131,7 @@ class CustomObjectServiceImplTest {
                 String.valueOf(customObjectCompositeIdlist.get(0)),
                 String.valueOf(customObjectCompositeIdlist.get(1)))
         );
-        verify(client).execute(any(CustomObjectQuery.class));
+        assertThat(fakeCustomObjectClient.isExecuted()).isTrue();
     }
 
     @Test
@@ -144,8 +143,8 @@ class CustomObjectServiceImplTest {
         final CustomObjectPagedQueryResult result = mock(CustomObjectPagedQueryResult.class);
         when(result.head()).thenReturn(Optional.of(mock));
 
-        when(client.execute(any())).thenReturn(CompletableFuture.completedFuture(result));
-
+        final FakeClient<CustomObject<JsonNode>> fakeCustomObjectClient = new FakeClient(result);
+        initMockService(fakeCustomObjectClient);
 
         final Optional<CustomObject<JsonNode>> customObjectOptional = service
             .fetchCustomObject(CustomObjectCompositeIdentifier.of(customObjectKey, customObjectContainer))
@@ -158,7 +157,7 @@ class CustomObjectServiceImplTest {
                     CustomObjectCompositeIdentifier.of(customObjectKey, customObjectContainer).toString())
             ).isEqualTo(customObjectId)
         );
-        verify(client).execute(any(CustomObjectQuery.class));
+        assertThat(fakeCustomObjectClient.isExecuted()).isTrue();
     }
 
     @Test
@@ -168,7 +167,9 @@ class CustomObjectServiceImplTest {
         when(mock.getKey()).thenReturn(customObjectKey);
         when(mock.getContainer()).thenReturn(customObjectContainer);
 
-        when(client.execute(any())).thenReturn(CompletableFuture.completedFuture(mock));
+        final FakeClient<CustomObject<JsonNode>> fakeCustomObjectClient =
+                new FakeClient<>(mock);
+        initMockService(fakeCustomObjectClient);
 
         final ObjectNode customObjectValue = JsonNodeFactory.instance.objectNode();
         customObjectValue.put("currentHash", "1234-5678-0912-3456");
@@ -182,7 +183,7 @@ class CustomObjectServiceImplTest {
             service.upsertCustomObject(draft).toCompletableFuture().join();
 
         assertThat(customObjectOptional).containsSame(mock);
-        verify(client).execute(eq(CustomObjectUpsertCommand.of(draft)));
+        assertThat(fakeCustomObjectClient.isExecuted()).isTrue();
     }
 
     @Test
@@ -190,19 +191,29 @@ class CustomObjectServiceImplTest {
         final CustomObject mock = mock(CustomObject.class);
         when(mock.getId()).thenReturn(customObjectId);
 
-        when(client.execute(any())).thenReturn(CompletableFutureUtils.failed(new BadRequestException("bad request")));
+        final FakeClient<CustomObject<JsonNode>> fakeCustomObjectClient =
+                new FakeClient<>(new BadRequestException("bad request"));
+        initMockService(fakeCustomObjectClient);
 
         final CustomObjectDraft<JsonNode> draftMock = mock(CustomObjectDraft.class);
         when(draftMock.getKey()).thenReturn(customObjectKey);
         when(draftMock.getContainer()).thenReturn(customObjectContainer);
         when(draftMock.getJavaType()).thenReturn(getCustomObjectJavaTypeForValue(convertToJavaType(JsonNode.class)));
 
-
         CompletableFuture future = service.upsertCustomObject(draftMock).toCompletableFuture();
 
         assertAll(
             () -> assertThat(future.isCompletedExceptionally()).isTrue(),
-            () -> assertThat(future).hasFailedWithThrowableThat().isExactlyInstanceOf(BadRequestException.class)
+            () -> assertThat(future).failsWithin(1, TimeUnit.SECONDS)
+                    .withThrowableOfType(ExecutionException.class)
+                    .withCauseExactlyInstanceOf(BadRequestException.class)
         );
+    }
+
+    private void initMockService(@Nonnull final SphereClient mockSphereClient) {
+        customObjectSyncOptions = CustomObjectSyncOptionsBuilder.of(mockSphereClient).build();
+
+        service = new CustomObjectServiceImpl(customObjectSyncOptions);
+
     }
 }
