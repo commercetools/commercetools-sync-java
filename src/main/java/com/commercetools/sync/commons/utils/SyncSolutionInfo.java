4fd5d5aab320b0bb78ef6405acdb4faf707bdf96
King-Hin Leung <kinghin.leung@gmail.com>
2020-12-04 17:56:46 +0100

    Revert "Resolve conflict"
    This reverts commit 11a4dad6e28715afb8da406ec019eb0e285df611.

--- src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
+++ src/main/java/com/commercetools/sync/commons/utils/SyncSolutionInfo.java
@@ -6,4 +6,5 @@ public final class SyncSolutionInfo {
      * This value is injected by the script at gradle-scripts/set-library-version.gradle.
      */
     public static final String LIB_VERSION = "#{LIB_VERSION}";
+    
 }
