1a8c7776ff9d3aff630417dc60964e1ff0a48202
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2021-03-19 16:46:40 +0100

    Refactor productservice.

--- src/main/java/com/commercetools/sync/services/impl/InventoryServiceImpl.java
+++ src/main/java/com/commercetools/sync/services/impl/InventoryServiceImpl.java
@@ -63,8 +63,6 @@ public final class InventoryServiceImpl
       @Nonnull final InventoryEntry inventoryEntry,
       @Nonnull final List<UpdateAction<InventoryEntry>> updateActions) {
 
-    return updateResource(inventoryEntry, InventoryEntryUpdateCommand.o
-
-            InventoryEntryUpdateCommand::of, updateActions);
+    return updateResource(inventoryEntry, InventoryEntryUpdateCommand::of, updateActions);
   }
 }
