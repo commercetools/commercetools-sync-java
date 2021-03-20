e1da9721a7351777f08fd851db4776a9e3f094ee
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2021-03-20 16:21:49 +0100

    Fix tests.

--- src/integration-test/java/com/commercetools/sync/integration/externalsource/products/ProductSyncWithNestedReferencedCustomObjectsIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/externalsource/products/ProductSyncWithNestedReferencedCustomObjectsIT.java
@@ -37,6 +37,7 @@ import io.sphere.sdk.customobjects.CustomObject;
 import io.sphere.sdk.products.Product;
 import io.sphere.sdk.products.ProductDraft;
 import io.sphere.sdk.products.ProductDraftBuilder;
+import io.sphere.sdk.products.ProductProjection;
 import io.sphere.sdk.products.ProductVariantDraft;
 import io.sphere.sdk.products.ProductVariantDraftBuilder;
 import io.sphere.sdk.products.attributes.Attribute;
@@ -145,7 +146,7 @@ class ProductSyncWithNestedReferencedCustomObjectsIT {
   }
 
   private ProductSyncOptions buildSyncOptions() {
-    final TriConsumer<SyncException, Optional<ProductDraft>, Optional<Product>> warningCallback =
+    final TriConsumer<SyncException, Optional<ProductDraft>, Optional<ProductProjection>> warningCallback =
         (syncException, productDraft, product) ->
             warningCallBackMessages.add(syncException.getMessage());
 
@@ -153,7 +154,7 @@ class ProductSyncWithNestedReferencedCustomObjectsIT {
         .errorCallback(
             (syncException, draft, product, updateActions) ->
                 collectErrors(syncException.getMessage(), syncException))
-        .beforeUpdateCallback(this::collectActions)
+        .beforeUpdateCallback((actions1, productDraft, product1) -> collectActions(actions1))
         .warningCallback(warningCallback)
         .build();
   }
@@ -164,9 +165,7 @@ class ProductSyncWithNestedReferencedCustomObjectsIT {
   }
 
   private List<UpdateAction<Product>> collectActions(
-      @Nonnull final List<UpdateAction<Product>> actions,
-      @Nonnull final ProductDraft productDraft,
-      @Nonnull final Product product) {
+          @Nonnull final List<UpdateAction<Product>> actions) {
     this.actions.addAll(actions);
     return actions;
   }
