4f8adfa81a9686d6398a44330978225962d49587
praveenkumarct <praveen.kumar@commercetools.com>
2021-03-20 10:22:56 +0100

    - spotless check fixes

--- src/main/java/com/commercetools/sync/products/utils/VariantReferenceResolutionUtils.java
+++ src/main/java/com/commercetools/sync/products/utils/VariantReferenceResolutionUtils.java
@@ -81,10 +81,10 @@ public final class VariantReferenceResolutionUtils {
    *   </tbody>
    * </table>
    *
-   * <p><b>Note:</b> The aforementioned references should be cached(idToKey value fetched and
-   * stored in a map). Any reference that is not cached will have its id in place and not
-   * replaced by the key will be considered as existing resources on the target commercetools
-   * project and the library will issues an update/create API request without reference resolution.
+   * <p><b>Note:</b> The aforementioned references should be cached(idToKey value fetched and stored
+   * in a map). Any reference that is not cached will have its id in place and not replaced by the
+   * key will be considered as existing resources on the target commercetools project and the
+   * library will issues an update/create API request without reference resolution.
    *
    * @param productVariants the product variants without expansion of references.
    * @param referenceIdToKeyMap the map(cache) contains reference Id to Key values.
@@ -132,9 +132,9 @@ public final class VariantReferenceResolutionUtils {
 
   /**
    * Takes a product variant that is supposed to have all its attribute product references and
-   * product set references id's cached(fetch and store key value for the reference id) in order
-   * to be able to replace the reference ids with the corresponding keys for the references.
-   * This method returns as a result a {@link List} of {@link AttributeDraft} that has all product
+   * product set references id's cached(fetch and store key value for the reference id) in order to
+   * be able to replace the reference ids with the corresponding keys for the references. This
+   * method returns as a result a {@link List} of {@link AttributeDraft} that has all product
    * references with keys replacing the ids.
    *
    * <p>Any product reference that is not cached(reference id is not present in the map) will have
