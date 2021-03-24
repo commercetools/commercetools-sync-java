f9cb6fb1f951d4b6aec7e1f243fe029df323db1c
praveenkumarct <praveen.kumar@commercetools.com>
2021-03-24 13:26:53 +0100

    - Implement reference resolution using cache for InventoryEntry resource.

--- src/main/java/com/commercetools/sync/inventories/utils/InventoryReferenceResolutionUtils.java
+++ src/main/java/com/commercetools/sync/inventories/utils/InventoryReferenceResolutionUtils.java
@@ -5,17 +5,16 @@ import static com.commercetools.sync.commons.utils.SyncUtils.getResourceIdentifi
 import static java.util.stream.Collectors.toList;
 
 import io.sphere.sdk.channels.Channel;
-import io.sphere.sdk.expansion.ExpansionPath;
 import io.sphere.sdk.inventory.InventoryEntry;
 import io.sphere.sdk.inventory.InventoryEntryDraft;
 import io.sphere.sdk.inventory.InventoryEntryDraftBuilder;
-import io.sphere.sdk.inventory.expansion.InventoryEntryExpansionModel;
 import io.sphere.sdk.inventory.queries.InventoryEntryQuery;
 import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.models.ResourceIdentifier;
 import io.sphere.sdk.queries.QueryExecutionUtils;
 import io.sphere.sdk.types.Type;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.Nonnull;
 
 /**
@@ -51,55 +50,45 @@ public final class InventoryReferenceResolutionUtils {
    *   </tbody>
    * </table>
    *
-   * <p><b>Note:</b> The {@link Channel} and {@link Type} references should be expanded with a key.
-   * Any reference that is not expanded will have its id in place and not replaced by the key will
-   * be considered as existing resources on the target commercetools project and the library will
-   * issues an update/create API request without reference resolution.
+   * <p><b>Note:</b> The {@link Channel} and {@link Type} reference should contain Id in the
+   * map(cache) with a key value. Any reference that is not available in the map will have its id in
+   * place and not replaced by the key will be considered as existing resources on the target
+   * commercetools project and the library will issues an update/create API request without
+   * reference resolution.
    *
-   * @param inventoryEntries the inventory entries with expanded references.
+   * @param inventoryEntries the inventory entries without expansion of references.
    * @return a {@link List} of {@link InventoryEntryDraft} built from the supplied {@link List} of
    *     {@link InventoryEntry}.
    */
   @Nonnull
   public static List<InventoryEntryDraft> mapToInventoryEntryDrafts(
-      @Nonnull final List<InventoryEntry> inventoryEntries) {
+      @Nonnull final List<InventoryEntry> inventoryEntries,
+      @Nonnull final Map<String, String> referenceIdToKeyMap) {
 
     return inventoryEntries.stream()
-        .map(InventoryReferenceResolutionUtils::mapToInventoryEntryDraft)
+        .map(inventoryEntry -> mapToInventoryEntryDraft(inventoryEntry, referenceIdToKeyMap))
         .collect(toList());
   }
 
   @Nonnull
   private static InventoryEntryDraft mapToInventoryEntryDraft(
-      @Nonnull final InventoryEntry inventoryEntry) {
+      @Nonnull final InventoryEntry inventoryEntry,
+      @Nonnull final Map<String, String> referenceIdToKeyMap) {
     return InventoryEntryDraftBuilder.of(inventoryEntry)
-        .custom(mapToCustomFieldsDraft(inventoryEntry))
-        .supplyChannel(getResourceIdentifierWithKey(inventoryEntry.getSupplyChannel()))
+        .custom(mapToCustomFieldsDraft(inventoryEntry, referenceIdToKeyMap))
+        .supplyChannel(
+            getResourceIdentifierWithKey(inventoryEntry.getSupplyChannel(), referenceIdToKeyMap))
         .build();
   }
 
   /**
-   * Builds a {@link InventoryEntryQuery} for fetching inventories from a source CTP project with
-   * all the needed references expanded for the sync:
+   * Builds a {@link InventoryEntryQuery} for fetching inventories from a source CTP project.
    *
-   * <ul>
-   *   <li>Custom Type
-   *   <li>Supply Channel
-   * </ul>
-   *
-   * <p>Note: Please only use this util if you desire to sync all the aforementioned references from
-   * a source commercetools project. Otherwise, it is more efficient to build the query without
-   * expansions, if they are not needed, to avoid unnecessarily bigger payloads fetched from the
-   * source project.
-   *
-   * @return the query for fetching inventories from the source CTP project with all the
-   *     aforementioned references expanded.
+   * @return the query for fetching inventories from the source CTP project without any references
+   *     expanded.
    */
   public static InventoryEntryQuery buildInventoryQuery() {
-    return InventoryEntryQuery.of()
-        .withLimit(QueryExecutionUtils.DEFAULT_PAGE_SIZE)
-        .withExpansionPaths(ExpansionPath.of("custom.type"))
-        .plusExpansionPaths(InventoryEntryExpansionModel::supplyChannel);
+    return InventoryEntryQuery.of().withLimit(QueryExecutionUtils.DEFAULT_PAGE_SIZE);
   }
 
   private InventoryReferenceResolutionUtils() {}
