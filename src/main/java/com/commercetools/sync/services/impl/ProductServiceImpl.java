2f040a67fa20fad518449b042fa3ee8455b8c7de
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2021-03-24 14:39:12 +0100

    Update todo-comment.

--- src/main/java/com/commercetools/sync/services/impl/ProductServiceImpl.java
+++ src/main/java/com/commercetools/sync/services/impl/ProductServiceImpl.java
@@ -193,9 +193,10 @@ public final class ProductServiceImpl
 
     return updateResourceByKey(product, updateActions);
   }
-  // TODO (JVM-SDK), remove this replicated code, when the "update by id" method is provided by the
-  // JVM-SDK
-  // see: https://github.com/commercetools/commercetools-jvm-sdk/issues/2129
+  // TODO: (JVM-SDK), remove this replicated code, when the the JVM-SDK allows to use
+  // ProductProjections in a ProductUpdateCommand.
+
+  // see: https://github.com/commercetools/commercetools-sync-java/pull/699#discussion_r600457980
   @Nonnull
   CompletionStage<ProductProjection> updateResourceByKey(
       @Nonnull final ProductProjection resource,
