76b8d591a1008b9a1450ae0b4d02814b853e7741
salander85 <sarah.lander@commercetools.com>
2021-03-25 13:31:46 +0100

    Add service class and interface

---
+++ src/main/java/com/commercetools/sync/producttypes/service/ProductTypeReferenceTransformService.java
@@ -0,0 +1,32 @@
+package com.commercetools.sync.producttypes.service;
+
+import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.ProductTypeDraft;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface ProductTypeReferenceTransformService {
+
+  /**
+   * Transforms productTypes by resolving the references and map them to ProductTypeDrafts.
+   *
+   * <p>This method replaces the ids on attribute references with keys and resolves(fetch key values
+   * for the reference id's) non null and unexpanded references of the productType{@link ProductType} by
+   * using cache.
+   *
+   * <p>If the reference ids are already cached, key values are pulled from the cache, otherwise it
+   * executes the query to fetch the key value for the reference id's and store the idToKey value
+   * pair in the cache for reuse.
+   *
+   * <p>Then maps the ProductType to ProductTypeDraft by performing reference resolution considering idToKey
+   * value from the cache.
+   *
+   * @param productTypes the productTypes to replace the references and attributes id's with keys.
+   * @return a new list which contains productTypeDrafts which have all their references and attributes
+   *     references resolved and already replaced with keys.
+   */
+  @Nonnull
+  CompletableFuture<List<ProductTypeDraft>> transformProductTypeReferences(@Nonnull List<ProductType> productTypes);
+}
---
+++ src/main/java/com/commercetools/sync/producttypes/service/impl/ProductTypeReferenceTransformServiceImpl.java
@@ -0,0 +1,43 @@
+package com.commercetools.sync.producttypes.service.impl;
+
+import com.commercetools.sync.commons.exceptions.ReferenceReplacementException;
+import com.commercetools.sync.producttypes.service.ProductTypeReferenceTransformService;
+import com.commercetools.sync.producttypes.utils.ProductTypeReferenceResolutionUtils;
+import com.commercetools.sync.services.impl.BaseTransformServiceImpl;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.ProductTypeDraft;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class ProductTypeReferenceTransformServiceImpl extends BaseTransformServiceImpl
+    implements ProductTypeReferenceTransformService {
+
+  private static final String FAILED_TO_REPLACE_REFERENCES_ON_ATTRIBUTES =
+      "Failed to replace referenced resource ids with keys on the attributes of the productTypes in "
+          + "the current fetched page from the source project. This page will not be synced to the target "
+          + "project.";
+
+  public ProductTypeReferenceTransformServiceImpl(
+      @Nonnull final SphereClient ctpClient,
+      @Nonnull final Map<String, String> referenceIdToKeyCache) {
+    super(ctpClient, referenceIdToKeyCache);
+  }
+
+  @Nonnull
+  @Override
+  public CompletableFuture<List<ProductTypeDraft>> transformProductTypeReferences(
+      @Nonnull final List<ProductType> productTypes) throws ReferenceReplacementException {
+
+    // TODO: Collect all attributes with references (nestedAttributes, setAttributes with nested type) and
+    // check if the ids are already cached in referenceIdToKeyCache otherwise fetch the keys by ids of the references
+    // and store in cache. Then call the utility function to map productTypes to productTypeDrafts
+
+    return CompletableFuture.completedFuture(ProductTypeReferenceResolutionUtils.mapToProductTypeDrafts(productTypes,
+        referenceIdToKeyCache));
+  }
+
+}
