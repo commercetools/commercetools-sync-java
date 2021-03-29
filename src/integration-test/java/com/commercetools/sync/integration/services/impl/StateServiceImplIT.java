40a1359c55894abc96c98ec084a7b2f9b4268c9b
praveenkumarct <praveen.kumar@commercetools.com>
2021-03-29 12:17:23 +0200

    revert changes to delete State type.

--- src/integration-test/java/com/commercetools/sync/integration/services/impl/StateServiceImplIT.java
+++ src/integration-test/java/com/commercetools/sync/integration/services/impl/StateServiceImplIT.java
@@ -163,7 +163,6 @@ class StateServiceImplIT {
 
     clearTransitions(CTP_TARGET_CLIENT, fetchState);
     deleteStates(CTP_TARGET_CLIENT, Optional.of(TRANSITION_STATE_TYPE));
-    deleteStates(CTP_TARGET_CLIENT, Optional.of(STATE_TYPE));
   }
 
   @Test
