d9de5655a55abf9ba421fd885c554b8fce89a734
Jens Schulze <jens.schulze@commercetools.com>
2022-07-21 13:02:58 +0200

    remove http pool

--- src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
@@ -34,7 +34,7 @@ public final class ClientConfigurationUtils {
    */
   public static SphereClient createClient(@Nonnull final SphereClientConfig clientConfig) {
     ProjectApiRoot apiRoot =
-        ApiRootBuilder.of(new ForkJoinPool(8))
+        ApiRootBuilder.of()
             .defaultClient(
                 ClientCredentials.of()
                     .withClientSecret(clientConfig.getClientSecret())
