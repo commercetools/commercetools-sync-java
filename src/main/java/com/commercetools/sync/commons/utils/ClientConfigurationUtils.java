8c0bc98ce6022d64601bf7e77af854812b3fd12e
Jens Schulze <jens.schulze@commercetools.com>
2022-07-21 11:55:55 +0200

    try explicit pools

--- src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
+++ src/main/java/com/commercetools/sync/commons/utils/ClientConfigurationUtils.java
@@ -33,7 +33,7 @@ public final class ClientConfigurationUtils {
    */
   public static SphereClient createClient(@Nonnull final SphereClientConfig clientConfig) {
     ProjectApiRoot apiRoot =
-        ApiRootBuilder.of()
+        ApiRootBuilder.of(new ForkJoinPool())
             .defaultClient(
                 ClientCredentials.of()
                     .withClientSecret(clientConfig.getClientSecret())
@@ -41,6 +41,7 @@ public final class ClientConfigurationUtils {
                     .build(),
                 clientConfig.getAuthUrl() + "/oauth/token",
                 clientConfig.getApiUrl())
+            .withOAuthExecutorService(new ForkJoinPool())
             .withInternalLoggerFactory(
                 ApiInternalLoggerFactory::get,
                 Level.INFO,
@@ -50,6 +51,7 @@ public final class ClientConfigurationUtils {
             .withErrorMiddleware(ErrorMiddleware.ExceptionMode.UNWRAP_COMPLETION_EXCEPTION)
             .addNotFoundExceptionMiddleware(Collections.singleton(ApiHttpMethod.GET))
             .withRetryMiddleware(
+                new ForkJoinPool(),
                 5, 200, 60000, Arrays.asList(500, 502, 503, 504), null, options -> options)
             .build(clientConfig.getProjectKey());
     return CompatSphereClient.of(apiRoot);
