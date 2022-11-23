d24437bc004abbdc5057f6654c37d2ba30a4eb85
King-Hin Leung <>
2022-11-23 18:58:02 +0100

    Change chart.js for testing purpose

--- benchmarks/assets/js/chart.js
+++ benchmarks/assets/js/chart.js
@@ -7604,9 +7604,9 @@ defaults._set('polarArea', {
 						var fill = custom.backgroundColor ? custom.backgroundColor : valueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
 						var stroke = custom.borderColor ? custom.borderColor : valueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
 						var bw = custom.borderWidth ? custom.borderWidth : valueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
-
+                        console.log('label : '+ label)
 						return {
-							text: '<a href="">'+label+'_1'+'</a>',
+							text: label+'_1',
 							fillStyle: fill,
 							strokeStyle: stroke,
 							lineWidth: bw,
