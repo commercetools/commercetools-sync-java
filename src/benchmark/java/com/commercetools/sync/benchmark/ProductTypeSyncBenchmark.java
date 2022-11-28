889a4016a69858c728393be48b4ea277b5f43dc0
King-Hin Leung <>
2022-11-28 09:30:28 +0100

    Remove log level guarding

--- src/benchmark/java/com/commercetools/sync/benchmark/ProductTypeSyncBenchmark.java
+++ src/benchmark/java/com/commercetools/sync/benchmark/ProductTypeSyncBenchmark.java
@@ -131,16 +131,11 @@ class ProductTypeSyncBenchmark {
     assertThat(errorCallBackExceptions).isEmpty();
     assertThat(errorCallBackMessages).isEmpty();
     assertThat(warningCallBackMessages).isEmpty();
-    assertThat(logger.isErrorEnabled()).isTrue();
-    assertThat(logger.isWarnEnabled()).isTrue();
-    assertThat(logger.isInfoEnabled()).isTrue();
-    assertThat(logger.isDebugEnabled()).isTrue();
-    assertThat(logger.isTraceEnabled()).isTrue();
-    if (logger.isInfoEnabled()) {
-      logger.info("Resources Type : " + PRODUCT_TYPE_SYNC);
-      logger.info("Sync Type : " + CREATES_ONLY);
-      logger.info("Time elapsed : " + totalTime);
-    }
+
+    logger.info("Resources Type : " + PRODUCT_TYPE_SYNC);
+    logger.info("Sync Type : " + CREATES_ONLY);
+    logger.info("Time elapsed : " + totalTime);
+
     saveNewResult(PRODUCT_TYPE_SYNC, CREATES_ONLY, totalTime);
   }
 
