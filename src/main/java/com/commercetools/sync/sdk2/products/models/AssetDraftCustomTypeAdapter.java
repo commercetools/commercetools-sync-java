e583ab5c365404082850a80c3db8a779178e8588
King-Hin Leung <>
2022-12-04 12:22:11 +0100

    Add AssetCustomTypeAdapter and AssetDraftCustomTypeAdapter

---
+++ src/main/java/com/commercetools/sync/sdk2/products/models/AssetDraftCustomTypeAdapter.java
@@ -0,0 +1,42 @@
+package com.commercetools.sync.sdk2.products.models;
+
+import com.commercetools.api.models.common.AssetDraft;
+import com.commercetools.api.models.customer.CustomerDraft;
+import com.commercetools.api.models.type.CustomFieldsDraft;
+import com.commercetools.sync.sdk2.commons.models.CustomDraft;
+import com.commercetools.sync.sdk2.commons.utils.CustomUpdateActionUtils;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Adapt Customer with {@link CustomDraft} interface to be used on {@link CustomUpdateActionUtils}
+ */
+public final class AssetDraftCustomTypeAdapter implements CustomDraft {
+
+  private final AssetDraft assetDraft;
+
+  private AssetDraftCustomTypeAdapter(AssetDraft assetDraft) {
+    this.assetDraft = assetDraft;
+  }
+
+  /**
+   * Get custom fields of the {@link CustomerDraft}
+   *
+   * @return the {@link CustomFieldsDraft}
+   */
+  @Nullable
+  @Override
+  public CustomFieldsDraft getCustom() {
+    return this.assetDraft.getCustom();
+  }
+
+  /**
+   * Build an adapter to be used for preparing custom type actions of with the given {@link
+   * CustomerDraft}
+   *
+   * @param assetDraft the {@link AssetDraft}
+   * @return the {@link AssetDraftCustomTypeAdapter}
+   */
+  public static AssetDraftCustomTypeAdapter of(AssetDraft assetDraft) {
+    return new AssetDraftCustomTypeAdapter(assetDraft);
+  }
+}
