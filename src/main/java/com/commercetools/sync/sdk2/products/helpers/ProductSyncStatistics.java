79e20be910a1a0f116a85983cd4b46e84f15d710
praveenkumarct <praveen.kumar@commercetools.com>
2022-12-15 11:36:21 +0100

    Add todo to not to forget to implement this class.

--- src/main/java/com/commercetools/sync/sdk2/products/helpers/ProductSyncStatistics.java
+++ src/main/java/com/commercetools/sync/sdk2/products/helpers/ProductSyncStatistics.java
@@ -2,6 +2,7 @@ package com.commercetools.sync.sdk2.products.helpers;
 
 import com.commercetools.sync.sdk2.commons.helpers.BaseSyncStatistics;
 
+// TODO: Yet to implement
 public class ProductSyncStatistics extends BaseSyncStatistics {
 
   @Override
