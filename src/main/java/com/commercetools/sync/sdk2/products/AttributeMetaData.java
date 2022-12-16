73fb559a027432f830b40dd2c71aae133b747861
King-Hin Leung <>
2022-12-16 11:13:18 +0100

    Clone AttributeMetaData to sdk-v2 package

---
+++ src/main/java/com/commercetools/sync/sdk2/products/AttributeMetaData.java
@@ -0,0 +1,52 @@
+package com.commercetools.sync.sdk2.products;
+
+import com.commercetools.api.models.product_type.AttributeDefinition;
+import com.commercetools.api.models.product_type.AttributeConstraintEnum;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Custom container for product variant attribute information: its name and whether it has the
+ * constraint "SameForAll" or not.
+ */
+public final class AttributeMetaData {
+  private String name;
+  private boolean isSameForAll;
+
+  private AttributeMetaData(@Nonnull final String name, final boolean isSameForAll) {
+    this.name = name;
+    this.isSameForAll = isSameForAll;
+  }
+
+  /**
+   * Uses the supplied {@link AttributeDefinition} instance to infer the name and whether it has the
+   * constraint "SameForAll" or not, to instantiate a new {@link AttributeMetaData} containing the
+   * aforementioned information.
+   *
+   * @param attributeDefinition the instance for which the needed information is used.
+   * @return a new instance of {@link AttributeMetaData}.
+   */
+  public static AttributeMetaData of(@Nonnull final AttributeDefinition attributeDefinition) {
+    boolean isSameForAll =
+        attributeDefinition.getAttributeConstraint().equals(AttributeConstraintEnum.SAME_FOR_ALL);
+    return new AttributeMetaData(attributeDefinition.getName(), isSameForAll);
+  }
+
+  /**
+   * Gets the name of the attribute.
+   *
+   * @return the name of the attribute.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Does the attribute have a "SameForAll" constraint or not.
+   *
+   * @return boolean flag specifying whether the attribute has a "SameForAll" constraint or not.
+   */
+  public boolean isSameForAll() {
+    return isSameForAll;
+  }
+}
