465b308ffa204330f17b25624147051f88d3be88
praveenkumarct <praveen.kumar@commercetools.com>
2022-12-16 13:33:30 +0100

    Clone productType, Category and channel service classes for comparison.

---
+++ src/main/java/com/commercetools/sync/sdk2/services/impl/ChannelServiceImpl.java
@@ -0,0 +1,86 @@
+package com.commercetools.sync.sdk2.services.impl;
+
+import com.commercetools.sync.commons.BaseSyncOptions;
+import com.commercetools.sync.commons.helpers.ResourceKeyIdGraphQlRequest;
+import com.commercetools.sync.commons.models.GraphQlQueryResources;
+import com.commercetools.sync.services.ChannelService;
+import com.commercetools.sync.services.impl.BaseServiceWithKey;
+import io.sphere.sdk.channels.Channel;
+import io.sphere.sdk.channels.ChannelDraft;
+import io.sphere.sdk.channels.ChannelDraftBuilder;
+import io.sphere.sdk.channels.ChannelRole;
+import io.sphere.sdk.channels.commands.ChannelCreateCommand;
+import io.sphere.sdk.channels.expansion.ChannelExpansionModel;
+import io.sphere.sdk.channels.queries.ChannelQuery;
+import io.sphere.sdk.channels.queries.ChannelQueryBuilder;
+import io.sphere.sdk.channels.queries.ChannelQueryModel;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+
+public final class ChannelServiceImpl
+    extends BaseServiceWithKey<
+            ChannelDraft,
+            Channel,
+            Channel,
+            BaseSyncOptions,
+            ChannelQuery,
+            ChannelQueryModel,
+            ChannelExpansionModel<Channel>>
+    implements ChannelService {
+
+  private final Set<ChannelRole> channelRoles;
+
+  public ChannelServiceImpl(
+      @Nonnull final BaseSyncOptions syncOptions, @Nonnull final Set<ChannelRole> channelRoles) {
+    super(syncOptions);
+    this.channelRoles = channelRoles;
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Map<String, String>> cacheKeysToIds(
+      @Nonnull final Set<String> channelKeys) {
+
+    return cacheKeysToIds(
+        channelKeys,
+        keysNotCached ->
+            new ResourceKeyIdGraphQlRequest(keysNotCached, GraphQlQueryResources.CHANNELS));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<String>> fetchCachedChannelId(@Nonnull final String key) {
+
+    return fetchCachedResourceId(
+        key,
+        () ->
+            ChannelQueryBuilder.of()
+                .plusPredicates(queryModel -> queryModel.key().is(key))
+                .build());
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<Channel>> createChannel(@Nonnull final String key) {
+
+    final ChannelDraft draft = ChannelDraftBuilder.of(key).roles(channelRoles).build();
+
+    return createResource(draft, ChannelCreateCommand::of);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<Channel>> createAndCacheChannel(@Nonnull final String key) {
+
+    return createChannel(key)
+        .thenApply(
+            channelOptional -> {
+              channelOptional.ifPresent(channel -> keyToIdCache.put(key, channel.getId()));
+              return channelOptional;
+            });
+  }
+}
---
+++ src/main/java/com/commercetools/sync/sdk2/services/impl/ProductTypeServiceImpl.java
@@ -0,0 +1,150 @@
+package com.commercetools.sync.sdk2.services.impl;
+
+import com.commercetools.sync.commons.BaseSyncOptions;
+import com.commercetools.sync.commons.helpers.ResourceKeyIdGraphQlRequest;
+import com.commercetools.sync.commons.models.GraphQlQueryResources;
+import com.commercetools.sync.commons.utils.CtpQueryUtils;
+import com.commercetools.sync.products.AttributeMetaData;
+import com.commercetools.sync.services.ProductTypeService;
+import io.sphere.sdk.commands.UpdateAction;
+import io.sphere.sdk.producttypes.ProductType;
+import io.sphere.sdk.producttypes.ProductTypeDraft;
+import io.sphere.sdk.producttypes.commands.ProductTypeCreateCommand;
+import io.sphere.sdk.producttypes.commands.ProductTypeUpdateCommand;
+import io.sphere.sdk.producttypes.expansion.ProductTypeExpansionModel;
+import io.sphere.sdk.producttypes.queries.ProductTypeQuery;
+import io.sphere.sdk.producttypes.queries.ProductTypeQueryBuilder;
+import io.sphere.sdk.producttypes.queries.ProductTypeQueryModel;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.singleton;
+
+public final class ProductTypeServiceImpl
+    extends BaseServiceWithKey<
+                ProductTypeDraft,
+                ProductType,
+                ProductType,
+                BaseSyncOptions,
+                ProductTypeQuery,
+                ProductTypeQueryModel,
+                ProductTypeExpansionModel<ProductType>>
+    implements ProductTypeService {
+
+  private final Map<String, Map<String, AttributeMetaData>> productsAttributesMetaData =
+      new ConcurrentHashMap<>();
+
+  public ProductTypeServiceImpl(@Nonnull final BaseSyncOptions syncOptions) {
+    super(syncOptions);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Map<String, String>> cacheKeysToIds(@Nonnull final Set<String> keys) {
+
+    return cacheKeysToIds(
+        keys,
+        keysNotCached ->
+            new ResourceKeyIdGraphQlRequest(keysNotCached, GraphQlQueryResources.PRODUCT_TYPES));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<String>> fetchCachedProductTypeId(@Nonnull final String key) {
+
+    return fetchCachedResourceId(
+        key,
+        () ->
+            ProductTypeQueryBuilder.of()
+                .plusPredicates(queryModel -> queryModel.key().isIn(singleton(key)))
+                .build());
+  }
+
+  @Nonnull
+  private static Map<String, AttributeMetaData> getAttributeMetaDataMap(
+      @Nonnull final ProductType productType) {
+    return productType.getAttributes().stream()
+        .map(AttributeMetaData::of)
+        .collect(
+            Collectors.toMap(AttributeMetaData::getName, attributeMetaData -> attributeMetaData));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<Map<String, AttributeMetaData>>>
+      fetchCachedProductAttributeMetaDataMap(@Nonnull final String productTypeId) {
+
+    if (productsAttributesMetaData.isEmpty()) {
+      return fetchAndCacheProductMetaData(productTypeId);
+    }
+    return CompletableFuture.completedFuture(
+        Optional.ofNullable(productsAttributesMetaData.get(productTypeId)));
+  }
+
+  @Nonnull
+  private CompletionStage<Optional<Map<String, AttributeMetaData>>> fetchAndCacheProductMetaData(
+      @Nonnull final String productTypeId) {
+
+    final Consumer<List<ProductType>> productTypePageConsumer =
+        productTypePage ->
+            productTypePage.forEach(
+                type -> {
+                  final String id = type.getId();
+                  productsAttributesMetaData.put(id, getAttributeMetaDataMap(type));
+                });
+
+    return CtpQueryUtils.queryAll(
+            syncOptions.getCtpClient(), ProductTypeQuery.of(), productTypePageConsumer)
+        .thenApply(result -> Optional.ofNullable(productsAttributesMetaData.get(productTypeId)));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Set<ProductType>> fetchMatchingProductTypesByKeys(
+      @Nonnull final Set<String> keys) {
+    return fetchMatchingResources(
+        keys,
+        (keysNotCached) ->
+            ProductTypeQueryBuilder.of()
+                .plusPredicates(queryModel -> queryModel.key().isIn(keysNotCached))
+                .build());
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<ProductType>> createProductType(
+      @Nonnull final ProductTypeDraft productTypeDraft) {
+    return createResource(productTypeDraft, ProductTypeCreateCommand::of);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<ProductType> updateProductType(
+      @Nonnull final ProductType productType,
+      @Nonnull final List<UpdateAction<ProductType>> updateActions) {
+
+    return updateResource(productType, ProductTypeUpdateCommand::of, updateActions);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<ProductType>> fetchProductType(@Nullable final String key) {
+
+    return fetchResource(
+        key,
+        () ->
+            ProductTypeQueryBuilder.of()
+                .plusPredicates(queryModel -> queryModel.key().is(key))
+                .build());
+  }
+}
