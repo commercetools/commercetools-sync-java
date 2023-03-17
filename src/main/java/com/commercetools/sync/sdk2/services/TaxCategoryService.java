1b7f940656c3175299d8798ebb2f5d56885c3319
lojzatran <lam.tran@commercetools.com>
2023-03-17 15:56:34 +0000

    Add tax category service

---
+++ src/main/java/com/commercetools/sync/sdk2/services/TaxCategoryService.java
@@ -0,0 +1,132 @@
+package com.commercetools.sync.sdk2.services;
+
+import com.commercetools.api.models.tax_category.TaxCategory;
+import com.commercetools.api.models.tax_category.TaxCategoryDraft;
+import com.commercetools.api.models.tax_category.TaxCategoryUpdateAction;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public interface TaxCategoryService {
+
+  /**
+   * Filters out the keys which are already cached and fetches only the not-cached tax category keys
+   * from the CTP project defined in an injected {@link io.sphere.sdk.client.SphereClient} and
+   * stores a mapping for every tax category to id in the cached map of keys -&gt; ids and returns
+   * this cached map.
+   *
+   * @param taxCategoryKeys - a set of tax category keys to fetch and cache the ids for
+   * @return {@link java.util.concurrent.CompletionStage}&lt;{@link java.util.Map}&gt; in which the
+   *     result of it's completion contains a map of requested tax category keys -&gt; ids
+   */
+  @Nonnull
+  CompletionStage<Map<String, String>> cacheKeysToIds(@Nonnull final Set<String> taxCategoryKeys);
+
+  /**
+   * Given a {@code key}, this method first checks if a cached map of TaxCategory keys -&gt; ids is
+   * not empty. If not, it returns a completed future that contains an optional that contains what
+   * this key maps to in the cache. If the cache is empty, the method populates the cache with the
+   * mapping of all TaxCategory keys to ids in the CTP project, by querying the CTP project for all
+   * Tax categories.
+   *
+   * <p>After that, the method returns a {@link java.util.concurrent.CompletionStage}&lt;{@link
+   * java.util.Optional}&lt;{@link String}&gt;&gt; in which the result of it's completion could
+   * contain an {@link java.util.Optional} with the id inside of it or an empty {@link
+   * java.util.Optional} if no {@link io.sphere.sdk.taxcategories.TaxCategory} was found in the CTP
+   * project with this key.
+   *
+   * @param key the key by which a {@link io.sphere.sdk.taxcategories.TaxCategory} id should be
+   *     fetched from the CTP project.
+   * @return {@link java.util.concurrent.CompletionStage}&lt;{@link java.util.Optional}&lt;{@link
+   *     String}&gt;&gt; in which the result of its completion could contain an {@link
+   *     java.util.Optional} with the id inside of it or an empty {@link java.util.Optional} if no
+   *     {@link io.sphere.sdk.taxcategories.TaxCategory} was found in the CTP project with this key.
+   */
+  @Nonnull
+  CompletionStage<Optional<String>> fetchCachedTaxCategoryId(@Nullable final String key);
+
+  /**
+   * Given a {@link java.util.Set} of tax category keys, this method fetches a set of all the
+   * taxCategories, matching given set of keys in the CTP project, defined in an injected {@link
+   * io.sphere.sdk.client.SphereClient}. A mapping of the key to the id of the fetched taxCategories
+   * is persisted in an in-memory map. <br>
+   * One must remember key is not required to create TaxCategory but is required to synchronize tax
+   * categories.
+   *
+   * @param keys set of tax category keys to fetch matching taxCategories by.
+   * @return {@link java.util.concurrent.CompletionStage}&lt;{@link java.util.Map}&gt; in which the
+   *     result of it's completion contains a {@link java.util.Set} of all matching taxCategories.
+   */
+  @Nonnull
+  CompletionStage<Set<TaxCategory>> fetchMatchingTaxCategoriesByKeys(
+      @Nonnull final Set<String> keys);
+
+  /**
+   * Given a tax category key, this method fetches a tax category that matches given key in the CTP
+   * project defined in a potentially injected {@link io.sphere.sdk.client.SphereClient}. If there
+   * is no matching tax category an empty {@link java.util.Optional} will be returned in the
+   * returned future. A mapping of the key to the id of the fetched category is persisted in an
+   * in-memory map.
+   *
+   * @param key the key of the tax category to fetch.
+   * @return {@link java.util.concurrent.CompletionStage}&lt;{@link java.util.Optional}&gt; in which
+   *     the result of it's completion contains an {@link java.util.Optional} that contains the
+   *     matching {@link io.sphere.sdk.taxcategories.TaxCategory} if exists, otherwise empty.
+   */
+  @Nonnull
+  CompletionStage<Optional<TaxCategory>> fetchTaxCategory(@Nullable final String key);
+
+  /**
+   * Given a resource draft of {@link io.sphere.sdk.taxcategories.TaxCategoryDraft}, this method
+   * attempts to create a resource {@link io.sphere.sdk.taxcategories.TaxCategory} based on it in
+   * the CTP project defined by the sync options.
+   *
+   * <p>A completion stage containing an empty option and the error callback will be triggered in
+   * those cases:
+   *
+   * <ul>
+   *   <li>the draft has a blank key
+   *   <li>the create request fails on CTP
+   * </ul>
+   *
+   * <p>On the other hand, if the resource gets created successfully on CTP, then the created
+   * resource's id and key are cached and the method returns a {@link
+   * java.util.concurrent.CompletionStage} in which the result of it's completion contains an
+   * instance {@link java.util.Optional} of the resource which was created.
+   *
+   * @param taxCategoryDraft the resource draft to create a resource based off of.
+   * @return a {@link java.util.concurrent.CompletionStage} containing an optional with the created
+   *     resource if successful otherwise an empty optional.
+   */
+  @Nonnull
+  CompletionStage<Optional<TaxCategory>> createTaxCategory(
+      @Nonnull final TaxCategoryDraft taxCategoryDraft);
+
+  /**
+   * Given a {@link io.sphere.sdk.taxcategories.TaxCategory} and a {@link java.util.List}&lt;{@link
+   * io.sphere.sdk.commands.UpdateAction}&lt;{@link
+   * io.sphere.sdk.taxcategories.TaxCategory}&gt;&gt;, this method issues an update request with
+   * these update actions on this {@link io.sphere.sdk.taxcategories.TaxCategory} in the CTP project
+   * defined in a potentially injected {@link io.sphere.sdk.client.SphereClient}. This method
+   * returns {@link java.util.concurrent.CompletionStage}&lt;{@link
+   * io.sphere.sdk.taxcategories.TaxCategory}&gt; in which the result of it's completion contains an
+   * instance of the {@link io.sphere.sdk.taxcategories.TaxCategory} which was updated in the CTP
+   * project.
+   *
+   * @param taxCategory the {@link io.sphere.sdk.taxcategories.TaxCategory} to update.
+   * @param updateActions the update actions to update the {@link
+   *     io.sphere.sdk.taxcategories.TaxCategory} with.
+   * @return {@link java.util.concurrent.CompletionStage}&lt;{@link
+   *     io.sphere.sdk.taxcategories.TaxCategory}&gt; containing as a result of it's completion an
+   *     instance of the {@link io.sphere.sdk.taxcategories.TaxCategory} which was updated in the
+   *     CTP project or a {@link io.sphere.sdk.models.SphereException}.
+   */
+  @Nonnull
+  CompletionStage<TaxCategory> updateTaxCategory(
+      @Nonnull final TaxCategory taxCategory,
+      @Nonnull final List<TaxCategoryUpdateAction> updateActions);
+}
---
+++ src/main/java/com/commercetools/sync/sdk2/services/impl/TaxCategoryServiceImpl.java
@@ -0,0 +1,131 @@
+package com.commercetools.sync.sdk2.services.impl;
+
+import static com.commercetools.sync.commons.utils.SyncUtils.batchElements;
+
+import com.commercetools.api.client.ByProjectKeyTaxCategoriesGet;
+import com.commercetools.api.client.ByProjectKeyTaxCategoriesKeyByKeyGet;
+import com.commercetools.api.client.ByProjectKeyTaxCategoriesPost;
+import com.commercetools.api.models.tax_category.TaxCategory;
+import com.commercetools.api.models.tax_category.TaxCategoryDraft;
+import com.commercetools.api.models.tax_category.TaxCategoryPagedQueryResponse;
+import com.commercetools.api.models.tax_category.TaxCategoryUpdateAction;
+import com.commercetools.api.models.tax_category.TaxCategoryUpdateBuilder;
+import com.commercetools.sync.sdk2.commons.models.GraphQlQueryResource;
+import com.commercetools.sync.sdk2.services.TaxCategoryService;
+import com.commercetools.sync.sdk2.taxcategories.TaxCategorySyncOptions;
+import io.vrap.rmf.base.client.ApiHttpResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public final class TaxCategoryServiceImpl
+    extends BaseServiceWithKey<
+        TaxCategorySyncOptions,
+        TaxCategory,
+        TaxCategoryDraft,
+        ByProjectKeyTaxCategoriesGet,
+        TaxCategoryPagedQueryResponse,
+        ByProjectKeyTaxCategoriesKeyByKeyGet,
+        TaxCategory,
+        ByProjectKeyTaxCategoriesPost>
+    implements TaxCategoryService {
+
+  public TaxCategoryServiceImpl(@Nonnull final TaxCategorySyncOptions syncOptions) {
+    super(syncOptions);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Map<String, String>> cacheKeysToIds(
+      @Nonnull final Set<String> taxCategoryKeys) {
+    return super.cacheKeysToIdsUsingGraphQl(taxCategoryKeys, GraphQlQueryResource.TAX_CATEGORIES);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<String>> fetchCachedTaxCategoryId(@Nullable final String key) {
+    final ByProjectKeyTaxCategoriesGet query =
+        syncOptions
+            .getCtpClient()
+            .taxCategories()
+            .get()
+            .withWhere("key in :keys")
+            .withPredicateVar("keys", Collections.singletonList(key));
+
+    return fetchCachedResourceId(key, query);
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Set<TaxCategory>> fetchMatchingTaxCategoriesByKeys(
+      @Nonnull final Set<String> keys) {
+    return fetchMatchingResources(
+        keys,
+        TaxCategory::getKey,
+        (keysNotCached) ->
+            syncOptions
+                .getCtpClient()
+                .taxCategories()
+                .get()
+                .withWhere("key in :keys")
+                .withPredicateVar("keys", keysNotCached));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<TaxCategory>> fetchTaxCategory(@Nullable final String key) {
+    return super.fetchResource(key, syncOptions.getCtpClient().taxCategories().withKey(key).get());
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<Optional<TaxCategory>> createTaxCategory(
+      @Nonnull final TaxCategoryDraft taxCategoryDraft) {
+    return super.createResource(
+        taxCategoryDraft,
+        TaxCategoryDraft::getKey,
+        TaxCategory::getId,
+        Function.identity(),
+        syncOptions.getCtpClient().taxCategories().post(taxCategoryDraft));
+  }
+
+  @Nonnull
+  @Override
+  public CompletionStage<TaxCategory> updateTaxCategory(
+      @Nonnull final TaxCategory taxCategory,
+      @Nonnull final List<TaxCategoryUpdateAction> updateActions) {
+
+    final List<List<TaxCategoryUpdateAction>> actionBatches =
+        batchElements(updateActions, MAXIMUM_ALLOWED_UPDATE_ACTIONS);
+
+    CompletionStage<ApiHttpResponse<TaxCategory>> resultStage =
+        CompletableFuture.completedFuture(new ApiHttpResponse<>(200, null, taxCategory));
+
+    for (final List<TaxCategoryUpdateAction> batch : actionBatches) {
+      resultStage =
+          resultStage
+              .thenApply(ApiHttpResponse::getBody)
+              .thenCompose(
+                  updatedTaxCategory ->
+                      syncOptions
+                          .getCtpClient()
+                          .taxCategories()
+                          .withId(updatedTaxCategory.getId())
+                          .post(
+                              TaxCategoryUpdateBuilder.of()
+                                  .actions(batch)
+                                  .version(updatedTaxCategory.getVersion())
+                                  .build())
+                          .execute());
+    }
+
+    return resultStage.thenApply(ApiHttpResponse::getBody);
+  }
+}
