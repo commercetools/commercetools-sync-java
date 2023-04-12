a97ab7e2dc854bdd2977cb149c8d83f7b206477d
lojzatran <lam.tran@commercetools.com>
2023-04-12 12:07:33 +0200

    Update comments

--- src/main/java/com/commercetools/sync/sdk2/commons/utils/ResourceIdentifierUtils.java
+++ src/main/java/com/commercetools/sync/sdk2/commons/utils/ResourceIdentifierUtils.java
@@ -15,22 +15,20 @@ public final class ResourceIdentifierUtils {
   public static final String REFERENCE_ID_FIELD = "id";
 
   /**
-   * Given a {@link io.sphere.sdk.models.Referenceable} {@code resource} of the type {@code T}, if
+   * Given a {@link ResourceIdentifiable} {@code resource}, if
    * it is not null, this method applies the {@link
-   * io.sphere.sdk.models.Referenceable#toResourceIdentifier()} method to return it as a {@link
-   * io.sphere.sdk.models.ResourceIdentifier} of the type {@code T}. If it is {@code null}, this
+   * ResourceIdentifiable#toResourceIdentifier()} method to return it as a {@link
+   * ResourceIdentifiable}. If it is {@code null}, this
    * method returns {@code null}.
    *
    * @param resource represents the resource to return as a {@link
-   *     io.sphere.sdk.models.ResourceIdentifier} if not {@code null}.
-   * @param <T> type of the resource supplied.
-   * @param <S> represents the type of the {@link io.sphere.sdk.models.ResourceIdentifier} returned.
-   * @return the supplied resource in the as a {@link io.sphere.sdk.models.ResourceIdentifier} if
+   *     ResourceIdentifier} if not {@code null}.
+   * @return the supplied resource in the as a {@link ResourceIdentifier} if
    *     not {@code null}. If it is {@code null}, this method returns {@code null}.
    */
   @Nullable
-  public static <T extends ResourceIdentifiable, S>
-      ResourceIdentifier toResourceIdentifierIfNotNull(@Nullable final T resource) {
+  public static <ResourceIdentifiableT extends ResourceIdentifiable>
+      ResourceIdentifier toResourceIdentifierIfNotNull(@Nullable final ResourceIdentifiableT resource) {
     return ofNullable(resource).map(ResourceIdentifiable::toResourceIdentifier).orElse(null);
   }
 
