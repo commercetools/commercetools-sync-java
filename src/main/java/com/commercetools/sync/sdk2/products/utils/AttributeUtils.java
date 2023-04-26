28c314c783654be1fd8986b9eec8df1dce25aa69
salander85 <sarah.lander@commercetools.com>
2023-04-26 12:12:14 +0200

    Add javadoc

--- src/main/java/com/commercetools/sync/sdk2/products/utils/AttributeUtils.java
+++ src/main/java/com/commercetools/sync/sdk2/products/utils/AttributeUtils.java
@@ -13,7 +13,15 @@ import javax.annotation.Nonnull;
 
 public final class AttributeUtils {
 
-  public static List<Reference> getAttributeReference(@Nonnull final Attribute attribute) {
+  /**
+   * Given an attribute this method checks if its value is of type "reference" or "set" of
+   * "reference" and transforms it's value into a {@link List} of {@link Reference}.
+   *
+   * @param attribute - Attribute to extract the References if exists.
+   * @return - a {@link List} of {@link Reference} extracted from the given attribute or empty list
+   *     if the attribute * doesn't contain reference types.
+   */
+  private static List<Reference> getAttributeReference(@Nonnull final Attribute attribute) {
     final Object attrValue = attribute.getValue();
     if (attrValue instanceof Reference) {
       return singletonList(AttributeAccessor.asReference(attribute));
@@ -25,11 +33,20 @@ public final class AttributeUtils {
     return Collections.emptyList();
   }
 
+  /**
+   * Given an attribute this method first checks if its of type "nested" or "set" of "nested" and
+   * transforms it's value into a {@link List} of {@link Attribute}. The resulting list is then
+   * mapped to type "reference" or "set" of "reference" depending on attributes' values types.
+   *
+   * @param attribute - Attribute to extract the Reference if exists
+   * @return - a {@link List} of {@link Reference} extracted from the given attribute or empty list
+   *     if the attribute doesn't contain reference types.
+   */
   public static List<Reference> getAttributeReferences(@Nonnull final Attribute attribute) {
     List<Attribute> attributes;
-    Object attrValue = attribute.getValue();
+    final Object attrValue = attribute.getValue();
     if (attrValue instanceof List && ((List) attrValue).stream().anyMatch(v -> v instanceof List)) {
-      List<List<Attribute>> nestedSet = AttributeAccessor.asSetNested(attribute);
+      final List<List<Attribute>> nestedSet = AttributeAccessor.asSetNested(attribute);
       attributes = nestedSet.stream().flatMap(Collection::stream).collect(Collectors.toList());
     } else if (attribute.getValue() instanceof List
         && ((List) attribute.getValue()).stream().anyMatch(v -> v instanceof Attribute)) {
