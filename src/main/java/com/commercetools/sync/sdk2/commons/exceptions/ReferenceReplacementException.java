2398902eecf702885882f14bea91b6ce7d5fa227
lojzatran <lam.tran@commercetools.com>
2023-05-18 21:48:36 +0200

    DEVX-87 Migrate ProductTypeReferenceResolutionUtils

---
+++ src/main/java/com/commercetools/sync/sdk2/commons/exceptions/ReferenceReplacementException.java
@@ -0,0 +1,20 @@
+package com.commercetools.sync.sdk2.commons.exceptions;
+
+import static com.commercetools.sync.sdk2.commons.exceptions.ExceptionUtils.buildMessage;
+
+import java.util.Set;
+import javax.annotation.Nonnull;
+
+public class ReferenceReplacementException extends RuntimeException {
+  private final Set<Throwable> causes;
+
+  public ReferenceReplacementException(
+      @Nonnull final String message, @Nonnull final Set<Throwable> causes) {
+    super(buildMessage(message, causes, 1));
+    this.causes = causes;
+  }
+
+  public Set<Throwable> getCauses() {
+    return causes;
+  }
+}
