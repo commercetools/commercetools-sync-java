9b971f4e39065717d4179fb28971b7ef47c17636
salander85 <sarah.lander@commercetools.com>
2023-07-18 11:43:46 +0200

    Migrate taxcategory sync utils

---
+++ src/main/java/com/commercetools/sync/sdk2/taxcategories/utils/TaxCategorySyncUtils.java
@@ -0,0 +1,41 @@
+package com.commercetools.sync.sdk2.taxcategories.utils;
+
+import static com.commercetools.sync.sdk2.commons.utils.OptionalUtils.filterEmptyOptionals;
+import static com.commercetools.sync.sdk2.taxcategories.utils.TaxCategoryUpdateActionUtils.*;
+
+import com.commercetools.api.models.tax_category.TaxCategory;
+import com.commercetools.api.models.tax_category.TaxCategoryDraft;
+import com.commercetools.api.models.tax_category.TaxCategoryUpdateAction;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+public final class TaxCategorySyncUtils {
+
+  private TaxCategorySyncUtils() {}
+
+  /**
+   * Compares all the fields of a {@link TaxCategory} and a {@link TaxCategoryDraft}. It returns a
+   * {@link List} of {@link TaxCategoryUpdateAction} as a result. If no update action is needed, for
+   * example in case where both the {@link TaxCategory} and the {@link TaxCategoryDraft} have the
+   * same fields, an empty {@link List} is returned.
+   *
+   * @param oldTaxCategory the {@link TaxCategory} which should be updated.
+   * @param newTaxCategory the {@link TaxCategoryDraft} where we get the new data.
+   * @return A list of tax category-specific update actions.
+   */
+  @Nonnull
+  public static List<TaxCategoryUpdateAction> buildActions(
+      @Nonnull final TaxCategory oldTaxCategory, @Nonnull final TaxCategoryDraft newTaxCategory) {
+
+    final List<TaxCategoryUpdateAction> updateActions =
+        new ArrayList<>(
+            filterEmptyOptionals(
+                buildChangeNameAction(oldTaxCategory, newTaxCategory),
+                buildSetDescriptionAction(oldTaxCategory, newTaxCategory)));
+
+    updateActions.addAll(buildTaxRateUpdateActions(oldTaxCategory, newTaxCategory));
+
+    return updateActions;
+  }
+}
