3e0e3ba382eed9f7ae41473ebf2885e3de7daa27
salander85 <sarah.lander@commercetools.com>
2023-07-25 15:54:32 +0200

    Formatting

--- src/test/java/com/commercetools/sync/sdk2/commons/utils/GenericUpdateActionUtilsTest.java
+++ src/test/java/com/commercetools/sync/sdk2/commons/utils/GenericUpdateActionUtilsTest.java
@@ -1,26 +1,25 @@
 package com.commercetools.sync.sdk2.commons.utils;
 
-import com.commercetools.api.client.ProjectApiRoot;
-import com.commercetools.api.models.type.ResourceTypeId;
-import com.commercetools.sync.sdk2.categories.CategorySyncOptions;
-import com.commercetools.sync.sdk2.categories.helpers.AssetCustomActionBuilder;
-import com.commercetools.sync.sdk2.commons.exceptions.SyncException;
-import com.commercetools.api.models.category.Category;
-import com.commercetools.api.models.category.CategoryDraft;
-import com.commercetools.api.models.category.CategoryUpdateAction;
-import com.commercetools.sync.sdk2.commons.models.AssetCustomTypeAdapter;
-import com.commercetools.api.models.common.Asset;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
 import static com.commercetools.sync.sdk2.categories.CategorySyncOptionsBuilder.of;
 import static com.commercetools.sync.sdk2.commons.utils.GenericUpdateActionUtils.buildTypedSetCustomTypeUpdateAction;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+
+import com.commercetools.api.client.ProjectApiRoot;
+import com.commercetools.api.models.category.Category;
+import com.commercetools.api.models.category.CategoryDraft;
+import com.commercetools.api.models.category.CategoryUpdateAction;
+import com.commercetools.api.models.common.Asset;
+import com.commercetools.api.models.type.ResourceTypeId;
+import com.commercetools.sync.sdk2.categories.CategorySyncOptions;
+import com.commercetools.sync.sdk2.categories.helpers.AssetCustomActionBuilder;
+import com.commercetools.sync.sdk2.commons.exceptions.SyncException;
+import com.commercetools.sync.sdk2.commons.models.AssetCustomTypeAdapter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
 
 class GenericUpdateActionUtilsTest {
 
@@ -29,10 +28,7 @@ class GenericUpdateActionUtilsTest {
       buildTypedSetCustomTypeUpdateAction_WithNullNewIdCategoryAsset_ShouldNotBuildCategoryUpdateAction() {
     final ArrayList<String> errorMessages = new ArrayList<>();
     final QuadConsumer<
-                SyncException,
-                Optional<CategoryDraft>,
-                Optional<Category>,
-                List<CategoryUpdateAction>>
+            SyncException, Optional<CategoryDraft>, Optional<Category>, List<CategoryUpdateAction>>
         errorCallback =
             (exception, oldResource, newResource, updateActions) ->
                 errorMessages.add(exception.getMessage());
